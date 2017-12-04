96ca6012d8f400165fd75ab7f653e10851a4d0cf
Heather Lent <hclent@email.arizona.edu>
2017-12-04 15:53:21 -0700

    updates to get AgroSystem working with new grammar

--- src/main/scala/org/clulab/wm/AgroSystem.scala
+++ src/main/scala/org/clulab/wm/AgroSystem.scala
@@ -19,10 +19,9 @@ import org.clulab.wm.wmutils.FileUtils.{findFilesFromResources, loadDomainParams
   * Handles text processing and information extraction for Agro domain.
   */
 class AgroSystem(
-  masterRulesPath: String = "/org/clulab/wm/grammars/agro/master.yml",
-  eventRulesPath: String = "/org/clulab/wm/grammars/agro/events_master.yml",
+  masterRulesPath: String = "/org/clulab/wm/grammars/master.yml",
   quantifierKBPath:String = "/org/clulab/wm/quantifierKB/gradable_adj_fullmodel.kb",
-  domainParamKBPath:String = "/org/clulab/wm/quantifierKB/domain_parameters.kb",
+  //domainParamKBPath:String = "/org/clulab/wm/quantifierKB/domain_parameters.kb",
   //agrovocLexiconsPath:String = "org/clulab/wm/agrovoc/lexicons",
   processor: Option[Processor] = None,
   debug: Boolean = false
@@ -38,7 +37,7 @@ class AgroSystem(
   val entityFinder = AgroEntityFinder(maxHops = 2)
 
   // Load the domain parameters (if param == 'all', apply the same values to all the parameters) //TODO: Change this appropriately
-  val domainParamValues = loadDomainParams(domainParamKBPath)
+  //val domainParamValues = loadDomainParams(domainParamKBPath)
 
   // Load the gradable adj grounding KB file
   val gradableAdjGroundingModel = loadGradableAdjGroundingFile(quantifierKBPath)
@@ -60,8 +59,10 @@ class AgroSystem(
 //    Seq("org/clulab/wm/lexicons/Quantifier.tsv", "org/clulab/wm/lexicons/IncDec.tsv") ++ agrovocLexicons,
 //    caseInsensitiveMatching = true
 //  )
+
+  //TODO: keep Quantifier...
   val ner = LexiconNER(
-    Seq("org/clulab/wm/lexicons/Quantifier.tsv", "org/clulab/wm/lexicons/IncDec.tsv"),
+    Seq("org/clulab/wm/lexicons/Quantifier.tsv"), //, "org/clulab/wm/lexicons/IncDec.tsv"),
     caseInsensitiveMatching = true
   )
 
@@ -76,7 +77,7 @@ class AgroSystem(
     extractFrom(doc)
   }
 
-//TODO: BECKY PICK UP HERE :)
+//TODO: starting here :)
 
   def extractEntitiesFrom(doc: Document, state: State = new State()): Vector[Mention] = {
     println ("Extracting from: " + doc.sentences.map(_.getSentenceText()).mkString(" "))
@@ -87,27 +88,28 @@ class AgroSystem(
       debugPrint("keptEntities:")
       debugMentions(keptEntities)
 
-      val locallyExtractedEntities = entityEngine.extractFrom(doc, state)
-      val quantifiers = locallyExtractedEntities.filter(m => m.labels.contains("Quantifier"))
-      val params = locallyExtractedEntities.filter(m => m.labels.contains("Param"))
-      val agrovoc = locallyExtractedEntities.filter(m => m.labels.contains("AGROVOC"))
 
-      debugPrint("Params:")
-      debugMentions(params)
-      debugPrint("Quantifiers:")
-      debugMentions(quantifiers)
-      debugPrint("AGROVOC:")
-      debugMentions(agrovoc)
-
-      val expandedParams = params.map(p => expandParams(keptEntities, p)).toVector
-      debugPrint("ExpandedParams:")
-      debugMentions(expandedParams)
-
-      val relabeled = actions.copyLabel(expandedParams, State(keptEntities ++ params ++ expandedParams))
-      debugPrint("Relabeled:")
-      debugMentions(relabeled)
-      //    val longestEntities = actions.keepLongestMentions(res, State(res)).toVector
-      return keptEntities ++ relabeled ++ quantifiers ++ agrovoc
+//      val locallyExtractedEntities = entityEngine.extractFrom(doc, state)
+//      val quantifiers = locallyExtractedEntities.filter(m => m.labels.contains("Quantifier"))
+//      //val params = locallyExtractedEntities.filter(m => m.labels.contains("Param"))
+//      //val agrovoc = locallyExtractedEntities.filter(m => m.labels.contains("AGROVOC"))
+//
+//      debugPrint("Params:")
+//      debugMentions(params)
+//      debugPrint("Quantifiers:")
+//      debugMentions(quantifiers)
+//      debugPrint("AGROVOC:")
+//      debugMentions(agrovoc)
+//
+//      val expandedParams = params.map(p => expandParams(keptEntities, p)).toVector
+//      debugPrint("ExpandedParams:")
+//      debugMentions(expandedParams)
+//
+//      val relabeled = actions.copyLabel(expandedParams, State(keptEntities ++ params ++ expandedParams))
+//      debugPrint("Relabeled:")
+//      debugMentions(relabeled)
+//      //    val longestEntities = actions.keepLongestMentions(res, State(res)).toVector
+      return keptEntities // ++ relabeled ++ quantifiers ++ agrovoc
 
 
   }
@@ -142,11 +144,12 @@ class AgroSystem(
 
 
   def extractEventsFrom(doc: Document, state: State): Vector[Mention] = {
-    val res = eventEngine.extractFrom(doc, state).toVector
+    val res = engine.extractFrom(doc, state).toVector
+//    res
     val cleanMentions = actions.keepMostCompleteEvents(res, State(res)).toVector
-    val longest = actions.keepLongestMentions(cleanMentions, State(cleanMentions)).toVector
-   longest
-//    cleanMentions
+//    val longest = actions.keepLongestMentions(cleanMentions, State(cleanMentions)).toVector
+//   longest
+    cleanMentions
   }
 
   def extractFrom(doc: Document): Vector[Mention] = {
