055d1b10a54ea30042aa0f0e4e3839c693bdd6e4
Becky Sharp <bsharp@Beckys-MacBook-Pro.local>
2017-12-18 13:41:52 -0700

    added file for writing causal tests

--- src/test/scala/org/clulab/wm/TestCausal.scala
+++ src/test/scala/org/clulab/wm/TestCausal.scala
@@ -1,5 +1,17 @@
 package org.clulab.wm
 
-class TestCausal {
+import org.scalatest._
+import TestUtils._
+import ReaderUtils._
 
+class TestCausal extends FlatSpec with Matchers {
+  val sent3 = "Limited financial capacities and low education levels further restrict farmers’ ability for higher benefits from increased agricultural production."
+  // (DEC-limited) financial capacities --> (DEC) farmer's ability for higher benefits
+  // (QUANT-low) education levels
+  // (INC-inc) agricultural production
+  val mentions3 = extractMentions(sent3)
+
+  sent3 should "have three modified entities" in {
+    val entities = mentions3.count(_.attachments.nonEmpty) should be (3)
+  }
 }
