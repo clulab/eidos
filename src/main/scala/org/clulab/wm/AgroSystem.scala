2a4ad48e53e786ad17bc6a17dfdb3fa44c8c5753
Becky Sharp <bsharp@email.arizona.edu>
2017-12-18 14:31:58 -0700

    cleaning up agrosystem

--- src/main/scala/org/clulab/wm/AgroSystem.scala
+++ src/main/scala/org/clulab/wm/AgroSystem.scala
@@ -6,11 +6,8 @@ import org.clulab.processors.{Document, Processor}
 import org.clulab.processors.fastnlp.FastNLPProcessor
 import org.clulab.sequences.LexiconNER
 
-import scala.collection.mutable
 import org.clulab.wm.entities.AgroEntityFinder
 import org.clulab.wm.wmutils.FileUtils.{findFilesFromResources, loadDomainParams, loadGradableAdjGroundingFile, readRules}
-
-
 
 
 
@@ -23,11 +20,11 @@ class AgroSystem(
   //domainParamKBPath:String = "/org/clulab/wm/quantifierKB/domain_parameters.kb",
   //agrovocLexiconsPath:String = "org/clulab/wm/agrovoc/lexicons",
   processor: Option[Processor] = None,
-  debug: Boolean = false
+  debug: Boolean = true
 ) {
 
   // defaults to FastNLPProcessor if no processor is given
-  val proc = if (processor.nonEmpty) processor.get else new FastNLPProcessor()
+  val proc:Processor = if (processor.nonEmpty) processor.get else new FastNLPProcessor()
 
   // ODIN Rules Files
   val rules: String = readRules(masterRulesPath)
@@ -48,20 +45,12 @@ class AgroSystem(
   // LexiconNER for labeling domain entities
   //TODO: agrovoc lexicons aren't in this project yet
   // todo: the order matters, we should be taking order into account
-//  println("agrovoc path: " + agrovocLexiconsPath)
-//
-//  val agrovocLexicons = findFilesFromResources(agrovocLexiconsPath, "tsv")
-//  agrovocLexicons.foreach(println)
+  //val agrovocLexicons = findFilesFromResources(agrovocLexiconsPath, "tsv")
 
-  // todo: handle adverbial quantifiers
-//  val ner = LexiconNER(
-//    Seq("org/clulab/wm/lexicons/Quantifier.tsv", "org/clulab/wm/lexicons/IncDec.tsv") ++ agrovocLexicons,
-//    caseInsensitiveMatching = true
-//  )
 
   //TODO: keep Quantifier...
   val ner = LexiconNER(
-    Seq("org/clulab/wm/lexicons/Quantifier.tsv"), //, "org/clulab/wm/lexicons/IncDec.tsv"),
+    Seq("org/clulab/wm/lexicons/Quantifier.tsv"),
     caseInsensitiveMatching = true
   )
 
@@ -78,73 +67,9 @@ class AgroSystem(
 
 //TODO: starting here :)
 
-  def extractEntitiesFrom(doc: Document, state: State = new State()): Vector[Mention] = {
-    println ("Extracting from: " + doc.sentences.map(_.getSentenceText()).mkString(" "))
-      val foundEntities = entityFinder.extractAndFilter(doc).toVector
-      debugPrint("Found (filtered) entities:")
-      debugMentions(foundEntities)
-      val keptEntities = foundEntities.filter(!_.matches("Avoid"))
-      debugPrint("keptEntities:")
-      debugMentions(keptEntities)
-
-
-//      val locallyExtractedEntities = entityEngine.extractFrom(doc, state)
-//      val quantifiers = locallyExtractedEntities.filter(m => m.labels.contains("Quantifier"))
-//      //val params = locallyExtractedEntities.filter(m => m.labels.contains("Param"))
-//      //val agrovoc = locallyExtractedEntities.filter(m => m.labels.contains("AGROVOC"))
-//
-//      debugPrint("Params:")
-//      debugMentions(params)
-//      debugPrint("Quantifiers:")
-//      debugMentions(quantifiers)
-//      debugPrint("AGROVOC:")
-//      debugMentions(agrovoc)
-//
-//      val expandedParams = params.map(p => expandParams(keptEntities, p)).toVector
-//      debugPrint("ExpandedParams:")
-//      debugMentions(expandedParams)
-//
-//      val relabeled = actions.copyLabel(expandedParams, State(keptEntities ++ params ++ expandedParams))
-//      debugPrint("Relabeled:")
-//      debugMentions(relabeled)
-//      //    val longestEntities = actions.keepLongestMentions(res, State(res)).toVector
-      return keptEntities // ++ relabeled ++ quantifiers ++ agrovoc
-
-
-  }
-
-  def expandParams(nounPhrases: Seq[Mention], paramMention: Mention): Mention = {
-    //val paramTB = paramMention.asInstanceOf[TextBoundMention]
-    val nounPhrasesContainingParam = nounPhrases.filter(np => np.label != "Avoid" && np.tokenInterval.contains(paramMention.tokenInterval))
-    if (nounPhrasesContainingParam.nonEmpty) {
-      val longest = nounPhrasesContainingParam.sortBy(-_.tokenInterval.length).head
-      // Save this original text for the grounding model lookup
-      val baseParamText = paramMention.text
-      val tempParamMap = new mutable.HashMap[String, Seq[Mention]]
-      tempParamMap.put("baseParam", Seq(paramMention))
-      val newArgsMap = longest.arguments ++ tempParamMap
-      val expandedAsRelationMention = new RelationMention(
-        labels = paramMention.labels,
-        tokenInterval = longest.tokenInterval,
-        paths = longest.paths,
-        arguments = newArgsMap,
-        sentence = paramMention.sentence,
-        document = paramMention.document,
-        keep = paramMention.keep,
-        foundBy = s"${paramMention.foundBy}-${AgroSystem.EXPAND_SUFFIX}"
-      )
-      return expandedAsRelationMention
-//      return longest.asInstanceOf[TextBoundMention].copy(
-//        labels = paramTB.labels,
-//        foundBy = s"${paramTB.foundBy}-${AgroSystem.EXPAND_SUFFIX}")
-    }
-    paramMention
-  }
-
 
   def extractEventsFrom(doc: Document, state: State): Vector[Mention] = {
     val res = engine.extractFrom(doc, state).toVector
-//    res
     val cleanMentions = actions.keepMostCompleteEvents(res, State(res)).toVector
 //    val longest = actions.keepLongestMentions(cleanMentions, State(cleanMentions)).toVector
 //   longest
@@ -153,7 +78,7 @@ class AgroSystem(
 
   def extractFrom(doc: Document): Vector[Mention] = {
     // get entities
-    val entities = extractEntitiesFrom(doc)
+    val entities = entityFinder.extractAndFilter(doc).toVector
     // get events
     val res = extractEventsFrom(doc, State(entities)).distinct
     res
@@ -181,9 +106,7 @@ object AgroSystem {
   val INTERCEPT: String = "intercept"
   val PARAM_MEAN: String = "mean"
   val PARAM_STDEV: String = "stdev"
-
-
-
+  
 }
 
 
