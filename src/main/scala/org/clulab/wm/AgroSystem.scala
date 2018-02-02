ffd7719f740b9a1b0b8d1ff9f7c4f8bed6a65e9f
Keith Alcock <github@keithalcock.com>
2018-02-02 16:13:21 -0700

    Add reload functionality to shell

--- src/main/scala/org/clulab/wm/AgroSystem.scala
+++ src/main/scala/org/clulab/wm/AgroSystem.scala
@@ -15,46 +15,54 @@ import Aliases._
   * Handles text processing and information extraction for Agro domain.
   */
 class AgroSystem(
-  masterRulesPath: String = "/org/clulab/wm/grammars/master.yml",
-  quantifierKBPath:String = "/org/clulab/wm/quantifierKB/gradable_adj_fullmodel.kb",
-  domainParamKBPath:String = "/org/clulab/wm/quantifierKB/domain_parameters.kb",
-  //agrovocLexiconsPath:String = "org/clulab/wm/agrovoc/lexicons",
+  // In order to reload, these values must be saved.
+  // The first three are loaded as resources from URLs, thus the leading /
+  val   masterRulesPath:   String = "/org/clulab/wm/grammars/master.yml",
+  val  quantifierKBPath:   String = "/org/clulab/wm/quantifierKB/gradable_adj_fullmodel.kb",
+  val domainParamKBPath:   String = "/org/clulab/wm/quantifierKB/domain_parameters.kb",
+  // The last two are loaded as resources from files and have no leading /
+  val    quantifierPath:   String = "org/clulab/wm/lexicons/Quantifier.tsv",
+//  val agrovocLexiconsPath: String = "org/clulab/wm/agrovoc/lexicons",
   processor: Option[Processor] = None,
   debug: Boolean = true
 ) {
   def this(x:Object) = this()
 
-  // defaults to FastNLPProcessor if no processor is given
-  val proc:Processor = if (processor.nonEmpty) processor.get else new FastNLPProcessor()
-
-  // ODIN Rules Files
-  val rules: String = readRules(masterRulesPath)
-
-
-  val entityFinder: AgroEntityFinder = AgroEntityFinder(maxHops = 5)
-
-  // Load the domain parameters (if param == 'all', apply the same values to all the parameters) //TODO: Change this appropriately
-  val domainParamValues: Map[Param, Map[String, Double]] = loadDomainParams(domainParamKBPath)
-
-  // Load the gradable adj grounding KB file
-  val grounder: Map[Quantifier, Map[String, Double]] = loadGradableAdjGroundingFile(quantifierKBPath)
-
-  // ODIN components
-  val actions: AgroActions = new AgroActions
-  val engine: ExtractorEngine = ExtractorEngine(rules, actions)
-
-  // LexiconNER for labeling domain entities
-  //TODO: agrovoc lexicons aren't in this project yet
-  // todo: the order matters, we should be taking order into account
-  //val agrovocLexicons = findFilesFromResources(agrovocLexiconsPath, "tsv")
-
-
-  //TODO: keep Quantifier...
-  val ner = LexiconNER(
-    Seq("org/clulab/wm/lexicons/Quantifier.tsv"),
-    caseInsensitiveMatching = true
-  )
-
+  // Defaults to FastNLPProcessor if no processor is given.  This is the expensive object
+  // that should not be lost on a restart.  The "rules" that the processor follows are
+  // not expected to change, or if they do, the processor would be restarted.
+  val proc: Processor = if (processor.nonEmpty) processor.get else new FastNLPProcessor()
+  
+  protected var entityFinder: AgroEntityFinder = _
+  var domainParamValues: Map[Param, Map[String, Double]] = _
+  var grounder: Map[Quantifier, Map[String, Double]] = _
+  protected var actions: AgroActions = _
+  var engine: ExtractorEngine = _
+  var ner: LexiconNER = _
+  
+  init()
+  
+  protected def init(): Unit = {
+    // Both of these involve internal configuration files which may change.
+    entityFinder = AgroEntityFinder(maxHops = 5)    
+    // Load the domain parameters (if param == 'all', apply the same values to all the parameters) //TODO: Change this appropriately
+    domainParamValues = loadDomainParams(domainParamKBPath)
+    // Load the gradable adj grounding KB file
+    grounder = loadGradableAdjGroundingFile(quantifierKBPath)
+    
+    val rules = readRules(masterRulesPath)
+    actions = new AgroActions
+    engine = ExtractorEngine(rules, actions) // ODIN component
+    
+    // LexiconNER for labeling domain entities
+    //TODO: agrovoc lexicons aren't in this project yet
+    // todo: the order matters, we should be taking order into account
+    //val agrovocLexicons = findFilesFromResources(agrovocLexiconsPath, "tsv")
+    ner = LexiconNER(Seq(quantifierPath), caseInsensitiveMatching = true) //TODO: keep Quantifier...
+  }
+  
+  def reload() = init
+  
   def annotate(text: String): Document = {
     val doc = proc.annotate(text)
     doc.sentences.foreach(s => s.entities = Some(ner.find(s)))
@@ -67,7 +75,6 @@ class AgroSystem(
   }
 
 //TODO: starting here :)
-
 
   def extractEventsFrom(doc: Document, state: State): Vector[Mention] = {
     val res = engine.extractFrom(doc, state).toVector
@@ -94,7 +101,6 @@ class AgroSystem(
   def debugMentions(mentions: Seq[Mention]): Unit = {
     if (debug) mentions.foreach(m => println(s" * ${m.text} [${m.label}, ${m.tokenInterval}]"))
   }
-
 }
 
 object AgroSystem {
@@ -111,8 +117,4 @@ object AgroSystem {
   val INC_LABEL_AFFIX = "-Inc"
   val DEC_LABEL_AFFIX = "-Dec"
   val QUANT_LABEL_AFFIX = "-Quant"
-
-
 }
-
-
