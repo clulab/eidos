3324ca17fe336f2fcfede4e0a90e2ceb62eb32a2
Becky Sharp <bsharp@email.arizona.edu>
2018-02-06 16:17:47 -0700

    added comment

--- src/test/scala/org/clulab/wm/TestModifications.scala
+++ src/test/scala/org/clulab/wm/TestModifications.scala
@@ -9,6 +9,7 @@ class TestModifications extends FlatSpec with Matchers {
   //ONE Increase Event
   val sent1 = "Better and well-functioning agricultural credit and market services for both established and emerging farmers."
 
+  // This test is being ignored because it is designed for an old framework, now we actually attach better to both entities
   ignore should "have 1 Increase attachment for 'agricultural credit' " in {
 
     val mentions = extractMentions(sent1)
@@ -23,6 +24,7 @@ class TestModifications extends FlatSpec with Matchers {
     "developing and disseminating climate change adaptation agricultural technologies to the farmers."
   // Note: parse of sentence makes it "impossible" to extract increase for education and extension programs --> maybe
   // a reason to switch to cluprocessor
+  // This test is being ignored because it is designed for an old framework
   ignore should "have 1 Increase attachment" in {
     val mentions = extractMentions(sent2)
     val entities = mentions.filter(m => m.attachments.exists(a => a.isInstanceOf[Increase]))
