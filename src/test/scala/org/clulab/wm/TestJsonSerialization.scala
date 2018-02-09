aa6268df160dd804c774e34fad44811d6060c2f9
Keith Alcock <github@keithalcock.com>
2018-02-08 23:20:07 -0700

    Checkpoint rough draft

--- src/test/scala/org/clulab/wm/TestJsonSerialization.scala
+++ src/test/scala/org/clulab/wm/TestJsonSerialization.scala
@@ -3,11 +3,18 @@ package org.clulab.wm
 import CAG._
 import TestUtils._
 
+import org.clulab.odin.Attachment
 import org.clulab.odin.Mention
+import org.clulab.odin.TextBoundMention
+import org.clulab.odin.RelationMention
+import org.clulab.odin.EventMention
+
 import org.clulab.processors.Document
 import org.clulab.processors.Sentence
 import org.clulab.serialization.json.JSONSerialization
 import org.clulab.struct.DirectedGraph
+import org.clulab.struct.Interval
+import org.clulab.wm.Aliases.Quantifier
 
 import org.json4s._
 import org.json4s.JsonDSL._
@@ -18,27 +25,115 @@ class TestJsonSerialization extends Test {
   behavior of "Document"
   
   it should "serialize correctly" in {
+
     abstract class JSONObjectProvider extends JSONSerialization {
+      
       def toJObject: JObject
       
       def jsonAST: JValue = render(toJObject)
-    }
-    
-    // Need to pay attention to specific kind of mention
-    
-    class JSONMention(mention: Mention) extends JSONObjectProvider {
-      def toJObject: JObject = null
+
+      // TODO: Make these nicer by handing out ids from somewhere
+      def id(any: Any): String = System.identityHashCode(any).toString
       
+      // TODO: Documents may already have some kind of ID.  Should it be used?
+      def id(document: Document): String = document.id.getOrElse(id(document.asInstanceOf[Any]))
+      
+      def newJSONMention(mention: Mention): JSONMention = mention match {
+        case mention: TextBoundMention => new JSONTextBoundMention(mention)
+        case mention: RelationMention => new JSONRelationMention(mention)
+        case mention: EventMention => new JSONEventMention(mention)
+      }      
+
+      def newJSONAttachment(attachment: Attachment): JSONAttachment = attachment match {
+        // These attachments are the first class in the matching package and could be changed
+        case attachment: Quantification => new JSONAttachment("QUANT", attachment.quantifier, None)
+        case attachment: Increase => new JSONAttachment("INC", attachment.trigger, attachment.quantifier)
+        case attachment: Decrease => new JSONAttachment("DEC", attachment.trigger, attachment.quantifier)
+      }
     }
     
-    class JSONGraphMapPair(key: String, value: DirectedGraph[String]) extends JSONObjectProvider {
+    class JSONNamedArgument(name: String, attachments: Seq[Mention]) extends JSONObjectProvider {
+      def toJObject: JObject = 
+        ("name" -> name)
+        ("attachments" -> attachments.map(newJSONMention(_).toJObject)) // TODO: Why not id?
+    }
+    
+    class JSONAttachment(kind: String, text: String, modifiers: Option[Seq[Quantifier]]) extends JSONObjectProvider {
+      
       def toJObject: JObject =
-        ("graph" ->
+        ("type", kind) ~
+        ("text", text) ~
+        ("modifiers", modifiers) // TODO: This is as far as it goes
+    }
+    
+    class JSONInterval(interval: Interval) extends JSONObjectProvider {
+    
+      def toJObject: JObject =
+        ("start", interval.start) ~
+        ("end", interval.end)
+    }
+    
+    class JSONProvenance(mention: Mention) extends JSONObjectProvider {
+      def toJObject: JObject =
+        ("provenance" ->
+          ("document" -> id(mention.document)) ~
+          ("sentence" -> id(mention.sentenceObj)) ~
+          ("positions" -> Seq(mention.tokenInterval).map(new JSONInterval(_).toJObject)) // TODO: We only have one
+        )
+    }
+
+    class JSONMention(mention: Mention) extends JSONObjectProvider {
+     
+      def toJObject: JObject =
+         ("@id" -> id(mention)) ~
+         ("labels" -> mention.labels) ~
+         ("text" -> mention.text) ~
+         new JSONProvenance(mention).toJObject ~
+         ("rule" -> mention.foundBy)
+    }
+    
+    class JSONTextBoundMention(mention: TextBoundMention) extends JSONMention(mention) {
+      
+      override def toJObject: JObject =
+         ("@type" -> "Entity") ~
+         super.toJObject ~
+         ("states" -> mention.attachments.map(newJSONAttachment(_).toJObject).toList)
+    }
+
+    class JSONRelationMention(mention: RelationMention) extends JSONMention(mention) {
+      
+      override def toJObject: JObject = 
+        ("@type" -> "Undirected Relation") ~ // TODO: Is this true, see no trigger
+        super.toJObject
+        // TODO: No trigger
+        // TODO: The picture is vague
+        ("arguments", mention.arguments.map(item => new JSONNamedArgument(item._1, item._2).toJObject).toList)
+    }
+
+    class JSONEventMention(mention: EventMention) extends JSONMention(mention) {
+      
+      override def toJObject: JObject = {
+        val sources = mention.arguments.getOrElse("cause", Nil) // TODO: are these correct names?
+        val targets = mention.arguments.getOrElse("effect", Nil)
+          
+        ("@type" -> "Directed Relation") ~ // TODO: Is this true?
+        super.toJObject ~
+        ("trigger" -> newJSONMention(mention.trigger).toJObject) ~ // TODO: Why not just id?
+        ("sources" -> sources.map(newJSONMention(_).toJObject)) ~ // TODO: Why not just id?
+        ("destinations" -> targets.map(newJSONMention(_).toJObject)) // TODO: Why not just id?
+      }
+    }
+    
+    class JSONGraphMapPair(key: String, value: Option[DirectedGraph[String]]) extends JSONObjectProvider {
+      
+      def toJObject: JObject =
+        ("dependencies" ->
           ("key" -> key) // TODO: How are these described?
         )
     }
     
     class JSONWord(sentence: Sentence, index: Int, text: Option[String]) extends JSONObjectProvider {
+      
       def getOrNone(optionArray: Option[Array[String]]): Option[String] =
           if (optionArray.isDefined) Option(optionArray.get(index))
           else None
@@ -53,28 +148,31 @@ class TestJsonSerialization extends Test {
         ("startOffset" -> startOffset) ~
         ("endOffset" -> endOffset) ~
         ("lemma" -> getOrNone(sentence.lemmas)) ~
-        ("chunk" -> getOrNone(sentence.chunks)) ~
-        ("dependencies" -> sentence.graphs.map((graph) => new JSONGraphMapPair(graph._1, graph._2).toJObject))
+        ("chunk" -> getOrNone(sentence.chunks))
       }
     }
     
     class JSONSentence(sentence: Sentence, text: Option[String]) extends JSONObjectProvider {
-      def toJObject: JObject =
-        ("sentence" -> // TODO: Make this prettier
-          ("@id" -> sentence.equivalenceHash) ~
-          ("words" -> sentence.words.indices.map(index => new JSONWord(sentence, index, text).toJObject).toList) ~
-          ("text" -> sentence.getSentenceText())
-        )
+      
+      def toJObject: JObject = {
+        val key = "universal-enhanced"
+        val dependencies = sentence.graphs.get(key)
+        
+        ("@id" -> id(sentence)) ~
+        ("words" -> sentence.words.indices.map(new JSONWord(sentence, _, text).toJObject).toList) ~
+        ("text" -> sentence.getSentenceText()) ~
+        new JSONGraphMapPair(key, dependencies).toJObject
+      }
     }
     
     class JSONDocument(document: Document, mentions: Seq[Mention]) extends JSONObjectProvider {
+      
       def toJObject: JObject =
         ("document" ->
-          ("@id" -> document.id.getOrElse(document.equivalenceHash.toString())) ~
-          ("sentences" -> document.sentences.map(_.equivalenceHash).toList)
+          ("@id" -> document.id.getOrElse(id(document))) ~
+          ("sentences" -> document.sentences.map(new JSONSentence(_, document.text).toJObject).toList)
         ) ~
-        ("sentences" -> document.sentences.map(sentence => new JSONSentence(sentence, document.text).toJObject).toList)
-        ("mentions" -> mentions.map(mention => new JSONMention(mention).toJObject).toList)
+        ("entities" -> mentions.map(newJSONMention(_).toJObject).toList)
     }
     
     val text = p1s1 + " " + p1s2
