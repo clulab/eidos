e19090c61cf2c376be5d5d04ecb279c5fcce63f3
Keith Alcock <github@keithalcock.com>
2018-02-08 17:32:43 -0700

    Checkpoint rough draft

--- src/test/scala/org/clulab/wm/TestJsonSerialization.scala
+++ src/test/scala/org/clulab/wm/TestJsonSerialization.scala
@@ -3,6 +3,7 @@ package org.clulab.wm
 import CAG._
 import TestUtils._
 
+import org.clulab.odin.Mention
 import org.clulab.processors.Document
 import org.clulab.processors.Sentence
 import org.clulab.serialization.json.JSONSerialization
@@ -23,8 +24,18 @@ class TestJsonSerialization extends Test {
       def jsonAST: JValue = render(toJObject)
     }
     
-    class JSONGraphMapPair(key: String, value: DirectedGraph[String]) extends JSONObjectProvider {
+    // Need to pay attention to specific kind of mention
+    
+    class JSONMention(mention: Mention) extends JSONObjectProvider {
       def toJObject: JObject = null
+      
+    }
+    
+    class JSONGraphMapPair(key: String, value: DirectedGraph[String]) extends JSONObjectProvider {
+      def toJObject: JObject =
+        ("graph" ->
+          ("key" -> key) // TODO: How are these described?
+        )
     }
     
     class JSONWord(sentence: Sentence, index: Int, text: Option[String]) extends JSONObjectProvider {
@@ -43,11 +54,7 @@ class TestJsonSerialization extends Test {
         ("endOffset" -> endOffset) ~
         ("lemma" -> getOrNone(sentence.lemmas)) ~
         ("chunk" -> getOrNone(sentence.chunks)) ~
-        ("dependencies" -> sentence.graphs.map { item =>
-          item match {
-            case (key: String, value: DirectedGraph[String]) => key
-          }
-        })
+        ("dependencies" -> sentence.graphs.map((graph) => new JSONGraphMapPair(graph._1, graph._2).toJObject))
       }
     }
     
@@ -60,13 +67,14 @@ class TestJsonSerialization extends Test {
         )
     }
     
-    class JSONDocument(document: Document) extends JSONObjectProvider {
+    class JSONDocument(document: Document, mentions: Seq[Mention]) extends JSONObjectProvider {
       def toJObject: JObject =
         ("document" ->
-          ("@id" -> document.id) ~
+          ("@id" -> document.id.getOrElse(document.equivalenceHash.toString())) ~
           ("sentences" -> document.sentences.map(_.equivalenceHash).toList)
         ) ~
         ("sentences" -> document.sentences.map(sentence => new JSONSentence(sentence, document.text).toJObject).toList)
+        ("mentions" -> mentions.map(mention => new JSONMention(mention).toJObject).toList)
     }
     
     val text = p1s1 + " " + p1s2
@@ -75,7 +83,7 @@ class TestJsonSerialization extends Test {
     val document = mentions(0).document // assuming there are any mentions
     document.text = Some(text)
     
-    val jsonDocument = new JSONDocument(document)
+    val jsonDocument = new JSONDocument(document, mentions)
     val json = jsonDocument.json(true)
     
     println(json)
