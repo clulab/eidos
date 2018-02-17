8a0156f9c1f5868fb9bc15ab3ad10cb4e9b87673
Keith Alcock <github@keithalcock.com>
2018-02-16 22:48:09 -0700

    And reenable other tests

--- src/test/scala/org/clulab/wm/TestJsonLdSerialization.scala
+++ src/test/scala/org/clulab/wm/TestJsonLdSerialization.scala
@@ -62,67 +62,67 @@ class TestJsonSerialization extends Test {
     json should not be empty
   }
   
-//  it should "be grounded" in {
-//    val json = serialize(Seq(
-//        newAnnotatedDocument("Rainfall significantly increases poverty."), 
-//    ))
-//    
-//    println(json)
-//    json.contains("intercept") should be (true)
-//    json.contains("mu") should be (true)
-//    json.contains("sigma") should be (true)
-//  }
-//  
-//  it should "serialize two simple documents" in {
-//    val json = serialize(Seq(
-//        newAnnotatedDocument("This is a test"), 
-//        newAnnotatedDocument("This is only a test")
-//    ))
-//    
-//    println(json)
-//    json should not be empty
-//  }
-//  
-//  it should "serialize one more complex document" in {
-//    val json = serialize(Seq(
-//        newAnnotatedDocument(p1s1, "p1s1"), 
-//    ))
-//    
-//    println(json)
-//    json should not be empty
-//  }
-//  
-//  it should "serialize two more complex documents" in {
-//    val json = serialize(Seq(
-//        newAnnotatedDocument(p1s2, "p1s2"), 
-//        newAnnotatedDocument(p2s2, "p2s2")
-//    ))
-//    
-//    println(json)
-//    json should not be empty
-//  }
-//  
-//  it should "serialize very complex documents" in {
-//    val json = serialize(Seq(
-//        newAnnotatedDocument(p1, "p1"), 
-//        newAnnotatedDocument(p2, "p2")
-//    ))
-//    
-//    println(json)
-//    json should not be empty
-//  }
-//  
-//  it should "serialize all CAGs in one pass" in {
-//    val json = serialize(Seq(
-//        newAnnotatedDocument(p1, "p1"), 
-//        newAnnotatedDocument(p2, "p2"),
-//        newAnnotatedDocument(p3, "p3"), 
-//        newAnnotatedDocument(p4, "p4"),
-//        newAnnotatedDocument(p5, "p5"), 
-//        newAnnotatedDocument(p6, "p6")
-//    ))
-//    
-//    println(json)
-//    json should not be empty    
-//  }
+  it should "be grounded" in {
+    val json = serialize(Seq(
+        newAnnotatedDocument("Rainfall significantly increases poverty."), 
+    ))
+    
+    println(json)
+    json.contains("intercept") should be (true)
+    json.contains("mu") should be (true)
+    json.contains("sigma") should be (true)
+  }
+  
+  it should "serialize two simple documents" in {
+    val json = serialize(Seq(
+        newAnnotatedDocument("This is a test"), 
+        newAnnotatedDocument("This is only a test")
+    ))
+    
+    println(json)
+    json should not be empty
+  }
+  
+  it should "serialize one more complex document" in {
+    val json = serialize(Seq(
+        newAnnotatedDocument(p1s1, "p1s1"), 
+    ))
+    
+    println(json)
+    json should not be empty
+  }
+  
+  it should "serialize two more complex documents" in {
+    val json = serialize(Seq(
+        newAnnotatedDocument(p1s2, "p1s2"), 
+        newAnnotatedDocument(p2s2, "p2s2")
+    ))
+    
+    println(json)
+    json should not be empty
+  }
+  
+  it should "serialize very complex documents" in {
+    val json = serialize(Seq(
+        newAnnotatedDocument(p1, "p1"), 
+        newAnnotatedDocument(p2, "p2")
+    ))
+    
+    println(json)
+    json should not be empty
+  }
+  
+  it should "serialize all CAGs in one pass" in {
+    val json = serialize(Seq(
+        newAnnotatedDocument(p1, "p1"), 
+        newAnnotatedDocument(p2, "p2"),
+        newAnnotatedDocument(p3, "p3"), 
+        newAnnotatedDocument(p4, "p4"),
+        newAnnotatedDocument(p5, "p5"), 
+        newAnnotatedDocument(p6, "p6")
+    ))
+    
+    println(json)
+    json should not be empty    
+  }
 }
