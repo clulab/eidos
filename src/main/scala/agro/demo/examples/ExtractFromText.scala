cb6bf4107d08399d3792ba7a5666a657577f698b
Becky Sharp <bsharp@email.arizona.edu>
2018-02-19 11:48:43 -0700

    added json to example

--- src/main/scala/agro/demo/examples/ExtractFromText.scala
+++ src/main/scala/agro/demo/examples/ExtractFromText.scala
@@ -1,7 +1,9 @@
 package agro.demo.examples
 
 import org.clulab.wm.EidosSystem
+import org.clulab.wm.serialization.json.{JLDCorpus, WMJSONSerializer}
 import utils.DisplayUtils.displayMention
+import org.clulab.serialization.json.stringify
 
 object ExtractFromText extends App {
 
@@ -11,8 +13,18 @@ object ExtractFromText extends App {
   val reader = new EidosSystem()
 
   // Extract the mentions
-  val mentions = reader.extractFrom(text)
+  val annotatedDocument = reader.extractFrom(text)
 
-  // Display
-  mentions.foreach(displayMention)
+  // If you want to have a pretty display:
+  annotatedDocument.mentions.foreach(displayMention)
+
+  // You can export to JSON-LD:
+  val corpus = new JLDCorpus(Seq(annotatedDocument), reader)
+  val mentionsJSONLD = corpus.serialize()
+  println(stringify(mentionsJSONLD, pretty = true))
+
+  // Or... optionally you can serialize to regular JSON (e.g., if you want to later reload the mentions for post-processing)
+  val mentionsJSON = WMJSONSerializer.jsonAST(annotatedDocument.mentions)
+  println(stringify(mentionsJSON, pretty = true))
+
 }
