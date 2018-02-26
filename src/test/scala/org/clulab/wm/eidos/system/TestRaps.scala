008d23b5825ba5f4a0048537996fe2af92054686
Heather Lent <hclent@email.arizona.edu>
2018-02-25 17:20:43 -0700

    added tests for s2 & 3

--- src/test/scala/org/clulab/wm/eidos/system/TestRaps.scala
+++ src/test/scala/org/clulab/wm/eidos/system/TestRaps.scala
@@ -1,9 +1,8 @@
 package org.clulab.wm.eidos.system
 
 import org.clulab.wm.eidos.test.TestUtils._
-import org.clulab.wm.eidos.text.NodeSpec
-import org.clulab.wm.eidos.text.Inc
-import org.scalactic.source.Position.apply
+import org.clulab.wm.eidos.text.{Inc, Dec, Quant, Causal, Origin, EdgeSpec, NodeSpec, EventSpec}
+
 
 class TestRaps extends Test {
 
@@ -16,11 +15,69 @@ class TestRaps extends Test {
 
     behavior of "Raps_sent1"
 
-    passingTest should "have the correct edge" taggedAs(Heather) in {
-
+    passingTest should "have the correct node" taggedAs(Heather) in {
       tester.test(credit) should be (successful)
 
     }
+
+  }
+
+  { //3 Increase events
+    val sent2 = "The support for agricultural research, education, and extension programs will also be increased for developing and disseminating climate change adaptation agricultural technologies to the farmers."
+    // Note: parse of sentence makes it "impossible" to extract increase for education and extension programs
+    // Maybe a reason to switch to cluprocessor
+    val tester = new Tester(sent2)
+
+    //ALL of these fail to be increase events
+    val research = NodeSpec("agricultural research", Inc("increased"))
+    val education = NodeSpec("education", Inc("increased"))
+    val programs = NodeSpec("extension programs", Inc("increased"))
+
+    behavior of "Raps_sent2"
+
+    failingTest should "have correct nodes 1" taggedAs(Heather) in {
+      tester.test(research) should be (successful)
+    }
+
+    failingTest should "have correct nodes 2" taggedAs(Heather) in {
+      tester.test(education) should be (successful)
+    }
+
+    failingTest should "have correct nodes 3" taggedAs(Heather) in {
+      tester.test(programs) should be (successful)
+    }
+
+  }
+
+  { //3 Decrease, 2 Increase Events; 2 Causal Edges, 1 Origin Edge
+    val sent3 = "Limited financial capacities and low education levels further restrict farmers' ability for higher benefits from increased agricultural production."
+    val tester = new Tester(sent3)
+
+    val financial = NodeSpec("Limited financial capacities", Dec("Limited"))
+    val ability = NodeSpec("\' ability", Dec("restrict"))
+    val education = NodeSpec("education levels", Dec("low"), Quant("low"))
+
+    val production = NodeSpec("agricultural production", Inc("increased"))
+    val benefits = NodeSpec("benefits", Inc("higher"))
+
+    behavior of "Raps_sent3"
+
+    passingTest should "have correct edges 1" taggedAs(Heather) in {
+      tester.test(EdgeSpec(financial, Causal, ability)) should be (successful)
+    }
+
+    passingTest should "have correct edges 2" taggedAs(Heather) in {
+      tester.test(EdgeSpec(education, Causal, ability)) should be (successful)
+    }
+
+    passingTest should "have correct node" taggedAs(Heather) in {
+      tester.test(benefits) should be (successful)
+    }
+
+    //Its not importing EventSpec boooooo
+//    passingTest should "have correct edges 3" taggedAs(Heather) in {
+//      tester.test(EventSpec(production, Origin, benefits)) should be (successful)
+//    }
 
   }
 
