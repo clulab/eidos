534006dfd911d9bba77893ed11ddd3349f4ccd47
Keith Alcock <github@keithalcock.com>
2018-03-05 10:31:03 -0700

    Test better

--- src/test/scala/org/clulab/wm/eidos/serialization/TestSerialization.scala
+++ src/test/scala/org/clulab/wm/eidos/serialization/TestSerialization.scala
@@ -2,7 +2,7 @@ package org.clulab.wm.eidos.serialization
 
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
 
-import org.clulab.odin.EventMention
+import org.clulab.odin.{EventMention, Mention, SynPath}
 import org.clulab.wm.eidos.Aliases.Quantifier
 import org.clulab.wm.eidos.EidosSystem
 import org.clulab.wm.eidos.test.TestUtils.Test
@@ -13,7 +13,7 @@ class TestSerialization extends Test {
 
   behavior of "Standard Serializer"
 
-  ignore should "serialize and deserialize again" in {
+  it should "serialize and deserialize again" in {
     val text = "Water trucking has decreased due to the cost of fuel."
     val annotatedDocument = reader.extractFrom(text)
     //    val mentionsOut = "testing"
@@ -22,27 +22,44 @@ class TestSerialization extends Test {
     //    val mentionsOut = ("testing", Option(Seq("1", "2", "3")))
     //    val mentionsOut = new DerivedSerial("testing", Option(Seq("1", "2", "3")))
     //      val mentionsOut = Quantification("testing", Option(Seq("1", "2", "3")))
-    val mentionsOut = annotatedDocument.mentions(2).asInstanceOf[EventMention].paths // 0, 1 serializes but 2 doesn't
+//    val prementionsOut = annotatedDocument.mentions(2).asInstanceOf[EventMention].paths // 0, 1 serializes but 2 doesn't
+//    val mentionsOut = prementionsOut("cause")
+    val mentionsOut = annotatedDocument.mentions
     // What is in here?
+
+   // val mentionsOut: TestSerialization.SynPath = Seq((5, 4, "Testing"), (6, 7, "1, 2, 3"))
+    //val mentionsOut: Map[String, Map[Mention, SynPath]] = Map()
 
     //    val mentionsOut: Scratch.TestType = Seq((1, 1, "help"), (2, 2, "me"))
 
     val streamOut = new ByteArrayOutputStream()
     val encoder = new ObjectOutputStream(streamOut)
-    //    try {
-    encoder.writeObject(mentionsOut)
-    //    }
-    //    catch {
-    //      case exception: Exception => Nil
-    //    }
+
+    mentionsOut.indices.foreach { index =>
+      val mention = mentionsOut(index)
+
+      encoder.reset()
+      try {
+        encoder.writeObject(mentionsOut)
+      }
+      catch {
+        case exception: Exception => println("Threw exception while writing index " + index)
+      }
+
+      val bytes = streamOut.toByteArray
+      val streamIn = new ByteArrayInputStream(bytes)
+      val decoder = new ObjectInputStream(streamIn)
+
+      try {
+        val mentionsIn = decoder.readObject()
+      }
+      catch {
+        case exception: Exception => println("Threw exception while reading index " + index)
+      }
+
+      decoder.close()
+    }
     encoder.close()
-
-    val bytes = streamOut.toByteArray
-
-    val streamIn = new ByteArrayInputStream(bytes)
-    val decoder = new ObjectInputStream(streamIn)
-    val mentionsIn = decoder.readObject()
-    decoder.close()
 
     //    val jValue1 = WMJSONSerializer.jsonAST(mentionsOut)
     //    val jValue2 = WMJSONSerializer.jsonAST(mentionsIn.asInstanceOf[Seq[Mention]])
@@ -54,33 +71,37 @@ class TestSerialization extends Test {
     //    println(json2)
 
     //    json1 should be (json2)
-    mentionsOut should be (mentionsIn)
+//    mentionsOut should be (mentionsIn)
   }
 }
 
-@SerialVersionUID(1L)
-abstract class BaseSerial extends Serializable {
-  //implicit val formats = org.json4s.DefaultFormats // This is the problem with serialization!
-}
+object TestSerialization {
+  type SynPath = Seq[(Int, Int, String)] // governor, dependent, label
 
-@SerialVersionUID(1L)
-case class DerivedSerial(quantifier: Quantifier, adverbs: Option[Seq[String]]) extends BaseSerial {
+  @SerialVersionUID(1L)
+  abstract class BaseSerial extends Serializable {
+    //implicit val formats = org.json4s.DefaultFormats // This is the problem with serialization!
+  }
 
-  // Serialization support
-  //  def this() = this("".asInstanceOf[Quantifier], None)
+  @SerialVersionUID(1L)
+  case class DerivedSerial(quantifier: Quantifier, adverbs: Option[Seq[String]]) extends BaseSerial {
 
-  //  override def equals(other: Any) = {
-  //    other match {
-  //      case other: TestQuantification => quantifier == other.quantifier && adverbs == other.adverbs
-  //      case _ => false
-  //    }
-  //  }
-}
+    // Serialization support
+    //  def this() = this("".asInstanceOf[Quantifier], None)
 
-object DerivedSerial {
-  def apply(whatever: String) = new DerivedSerial("hello", None)
-}
+    //  override def equals(other: Any) = {
+    //    other match {
+    //      case other: TestQuantification => quantifier == other.quantifier && adverbs == other.adverbs
+    //      case _ => false
+    //    }
+    //  }
+  }
 
-object Scratch {
-  type TestType = Seq[(Int, Int, String)] // governor, dependent, label
+  object DerivedSerial {
+    def apply(whatever: String) = new DerivedSerial("hello", None)
+  }
+
+  object Scratch {
+    type TestType = Seq[(Int, Int, String)] // governor, dependent, label
+  }
 }
