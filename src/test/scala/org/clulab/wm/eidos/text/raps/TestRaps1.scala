d8d66fd237a3fb149303c929be3fd23d189f56b2
Heather Lent <hclent@email.arizona.edu>
2018-03-04 18:46:34 -0700

    add sent21. awkward sentence.

--- src/test/scala/org/clulab/wm/eidos/text/raps/TestRaps1.scala
+++ src/test/scala/org/clulab/wm/eidos/text/raps/TestRaps1.scala
@@ -31,4 +31,29 @@ class TestRaps1 extends Test {
 
   }
 
+  {
+    //Unsure exactly what this sentences convey. Are there actually any causal events here?
+    val sent21 = "A combination of increasing population, government plans to invest in fertilizer factory, " +
+      "government subsidy on fertilizers, improved economic performance expected to cause a shift from agriculture to service industry, " +
+      "government plans for massive expansion of irrigation (irrigate 1 million ha.), newly devolved county governments etc. are some of the developments " +
+      "expected to change agriculture development in the country."
+    val tester = new Tester(sent21)
+
+    val population = NodeSpec("population", Inc("increasing"))
+    val econ = NodeSpec("economic performance", Inc("improved"))
+
+    behavior of "Raps_sent21"
+
+    passingTest should "have correct node 1" taggedAs(Heather) in {
+      tester.test(population) should be (successful)
+    }
+
+    //currently extracts "economic performance expected" as the entity text
+    failingTest should "have correct node 2" taggedAs(Heather) in {
+      tester.test(econ) should be (successful)
+    }
+
+  }
+
+
 }
