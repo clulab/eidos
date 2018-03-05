161f7c1e3bddeb561ed2f0b4781f797ce22d9b26
Ajay Nagesh <ajaynagesh@Ajays-MacBook-Pro.local>
2018-03-04 17:08:34 -0700

    resolved the conflict

--- src/main/scala/org/clulab/wm/eidos/apps/ExtractFromDirectory.scala
+++ src/main/scala/org/clulab/wm/eidos/apps/ExtractFromDirectory.scala
@@ -3,44 +3,36 @@ package org.clulab.wm.eidos.apps
 import java.io.PrintWriter
 import scala.collection.Seq
 import org.clulab.serialization.json.stringify
-import org.clulab.wm.eidos.EidosSystem
 import org.clulab.wm.eidos.serialization.json.JLDCorpus
 import org.clulab.wm.eidos.utils.FileUtils.findFiles
+import org.clulab.wm.eidos.EidosSystem
+
 
 object ExtractFromDirectory extends App {
+
   val reader = new EidosSystem()
   val inputDir = args(0)
   val outputDir = args(1)
   val files = findFiles(inputDir, "txt")
 
-  // For each file in the input directory:
-  files foreach { file =>
+  for {
+    // For each file in the input directory:
+    file <- files
     // 1. Open corresponding output file
-    val pw = new PrintWriter(s"$outputDir/${file.getName}.jsonld")
+    outputFile = file.getName
+    pw = new PrintWriter(s"$outputDir/$outputFile.jsonld")
     // 2. Get the input file contents
-<<<<<<< HEAD
     source = scala.io.Source.fromFile(file)
     text <- source.getLines()
     // 3. Extract causal mentions from the text
-    annotatedDocument = reader.extractFromText(text)
+    annotatedDocument = reader.extractFrom(text)
     // 4. Convert to JSON
     corpus = new JLDCorpus(Seq(annotatedDocument), reader)
     mentionsJSONLD = corpus.serialize()
   } {
     // 5. Write to output file and close
     pw.println(stringify(mentionsJSONLD, pretty = true))
-=======
-    val source = scala.io.Source.fromFile(file)
-    source.getLines() foreach { text =>
-      // 3. Extract causal mentions from the text
-      val annotatedDocument = reader.extractFrom(text)
-      // 4. Convert to JSON
-      val corpus = new JLDCorpus(Seq(annotatedDocument), reader)
-      val mentionsJSONLD = corpus.serialize()
-      // 5. Write to output file
-      pw.println(stringify(mentionsJSONLD, pretty = true))
-    }
->>>>>>> 24c3d14f96a321ad2a7817044580d377623f0a18
     pw.close()
   }
+
 }
