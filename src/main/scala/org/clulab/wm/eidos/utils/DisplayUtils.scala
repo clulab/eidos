73dae17e1195aa78037359f6a5673f46604ddc06
Becky Sharp <bsharp@email.arizona.edu>
2018-03-13 16:01:26 -0700

    consolidated more cleaned comments

--- src/main/scala/org/clulab/wm/eidos/utils/DisplayUtils.scala
+++ src/main/scala/org/clulab/wm/eidos/utils/DisplayUtils.scala
@@ -37,7 +37,7 @@ object DisplayUtils {
       events.foreach(e => sb.append(s"${mentionToDisplayString(e, newline, tab)} $newline"))
       sb.append(s"${"=" * 50} $newline")
     }
-    sb.toString()
+    sb.toString
   }
 
   def mentionToDisplayString(mention: Mention, newline: String, tab: String): String = {
@@ -68,7 +68,7 @@ object DisplayUtils {
       case _ => ()
     }
     sb.append(s"$boundary $newline")
-    sb.toString()
+    sb.toString
   }
 
   def argumentsToString(b: Mention, newline: String, tab: String): String = {
@@ -80,19 +80,10 @@ object DisplayUtils {
           if (v.attachments.nonEmpty) sb.append(s"$tab  * Attachments: ${attachmentsString(v.attachments)} $newline")
         }
     }
-    sb.toString()
+    sb.toString
   }
 
   def attachmentsString(mods: Set[Attachment]): String = s"${mods.mkString(", ")}"
-
-
-  def displayMentions(mentions: Seq[Mention], doc: Document, printDeps: Boolean = false): Unit = {
-    println(mentionsToDisplayString(mentions, doc, printDeps, "\n", "\t"))
-  }
-
-  def printMentions(mentions: Seq[Mention], doc: Document, pw: PrintWriter, printDeps: Boolean = false): Unit = {
-    pw.println(mentionsToDisplayString(mentions, doc, printDeps, "\n", "\t"))
-  }
 
   def syntacticDependenciesToString(s:Sentence): String = {
     if (s.dependencies.isDefined) {
@@ -101,58 +92,23 @@ object DisplayUtils {
   }
 
 
-  def displayMention(mention: Mention) = println(mentionToDisplayString(mention, "\n", "\t"))
-
-
-  def webAppMention(mention: Mention): String = {
-    mentionToDisplayString(mention, "<br>", htmltab)
-
-//    val sb = new StringBuilder
-//    val boundary = s"${tab}${"-" * 30}<br>"
-//    sb.append(s"${mention.labels} => ${mention.text}<br>")
-//    sb.append(boundary)
-//    sb.append(s"${tab}Rule => ${mention.foundBy}<br>")
-//    val mentionType = mention.getClass.toString.split("""\.""").last
-//    sb.append(s"${tab}Type => $mentionType<br>")
-//    sb.append(boundary)
-//    mention match {
-//      case tb: TextBoundMention =>
-//        sb.append(s"${tab}${tb.labels.mkString(", ")} => ${tb.text}<br>")
-//        if (tb.attachments.nonEmpty) sb.append(s"${tab}  * Attachments: ${attachmentsString(tb.attachments)}<br>")
-//      case em: EventMention =>
-//        sb.append(s"${tab}trigger => ${em.trigger.text}<br>")
-//        if (em.trigger.attachments.nonEmpty) sb.append(s"${tab}  * Attachments: ${attachmentsString(em.trigger.attachments)}<br>")
-//        sb.append(webAppArguments(em))
-//        if (em.attachments.nonEmpty) {
-//          sb.append(s"${tab}Event Attachments: ${attachmentsString(em.attachments)}<br>")
-//        }
-//      case rel: RelationMention =>
-//        sb.append(webAppArguments(rel))
-//        if (rel.attachments.nonEmpty) {
-//          sb.append(s"${tab}Relation Attachments: ${attachmentsString(rel.attachments)}<br>")
-//        }
-//      case _ => ()
-//    }
-//    sb.append(s"$boundary<br>")
-//
-//    sb.toString()
+  /* Wrappers for displaying the mention string */
+  def displayMentions(mentions: Seq[Mention], doc: Document, printDeps: Boolean = false): Unit = {
+    println(mentionsToDisplayString(mentions, doc, printDeps, "\n", "\t"))
   }
-
-  def printMention(mention: Mention, pw: PrintWriter) = pw.println(mentionToDisplayString(mention, "\n", "\t"))
-
-//  def webAppArguments(b: Mention): String = {
-//    val sb = new StringBuilder
-//    b.arguments foreach {
-//      case (argName, ms) =>
-//        ms foreach { v =>
-//          sb.append(s"${tab}$argName ${v.labels.mkString("(", ", ", ")")} => ${v.text}<br>")
-//          if (v.attachments.nonEmpty) sb.append(s"${tab}  * Attachments: ${attachmentsString(v.attachments)}<br>")
-//        }
-//    }
-//    sb.toString()
-//  }
+  def displayMention(mention: Mention): Unit = println(mentionToDisplayString(mention, "\n", "\t"))
 
 
-  // html tab
-  val htmltab:String = "&nbsp;&nbsp;&nbsp;&nbsp;"
+  /* Wrappers for printing the mention string to a file */
+  def printMentions(mentions: Seq[Mention], doc: Document, pw: PrintWriter, printDeps: Boolean = false): Unit = {
+    pw.println(mentionsToDisplayString(mentions, doc, printDeps, "\n", "\t"))
+  }
+  def printMention(mention: Mention, pw: PrintWriter): Unit = pw.println(mentionToDisplayString(mention, "\n", "\t"))
+
+
+  /* Wrapper for getting html version of the mention string for use in the webapp */
+  protected val htmltab:String = "&nbsp;&nbsp;&nbsp;&nbsp;"
+  def webAppMention(mention: Mention): String = mentionToDisplayString(mention, "<br>", htmltab)
+
+
 }
