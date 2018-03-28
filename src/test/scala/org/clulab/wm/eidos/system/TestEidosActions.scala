160d2d1acb65eeffb1649b691216a94e96e49833
Zheng Tang <oscartang1120@gmail.com>
2018-03-28 16:43:05 -0700

    ...

--- src/test/scala/org/clulab/wm/eidos/system/TestEidosActions.scala
+++ src/test/scala/org/clulab/wm/eidos/system/TestEidosActions.scala
@@ -11,22 +11,21 @@ import org.clulab.serialization.json.stringify
 import org.clulab.wm.eidos.EidosSystem
 import org.clulab.wm.eidos.serialization.json.WMJSONSerializer
 import org.clulab.wm.eidos.test.TestUtils._
-import org.clulab.wm.eidos.utils.DisplayUtils
 
 import scala.collection.JavaConverters.asScalaSet
 
 class TestEidosActions extends Test {
-  
+
   def addAllMentions(mentions: Seq[Mention], mapOfMentions: IdentityHashMap[Mention, Mention]): Unit = {
     def mentionsInPaths(paths: Map[String, Map[Mention, SynPath]]): Seq[Mention] =
-        Seq.empty
-//        paths.values.map(_.keys).flatten.toSeq
-    
+      Seq.empty
+    //        paths.values.map(_.keys).flatten.toSeq
+
     mentions.foreach { mention =>
       mapOfMentions.put(mention, mention)
-      
+
       val arguments = mention.arguments.values.flatten.toSeq
-       
+
       mention match {
         case mention: TextBoundMention => addAllMentions(arguments, mapOfMentions)
         // One trigger can result in multiple events, so triggers should not be involved in the search for duplicates.
@@ -37,85 +36,79 @@ class TestEidosActions extends Test {
       }
     }
   }
-    
+
   def findUniqueMentions(mentions: Seq[Mention]): Seq[Mention] = {
     val mapOfMentions = new IdentityHashMap[Mention, Mention]()
-    
+
     addAllMentions(mentions, mapOfMentions)
     asScalaSet(mapOfMentions.keySet()).toSeq
   }
-  
+
   def areMatching(left: Mention, right: Mention): Boolean = {
     if (left.eq(right))
       true
     else {
       left.getClass == right.getClass &&
-          left.document.eq(right.document) &&
-          left.sentence == right.sentence &&
-          left.labels == right.labels &&
-          left.tokenInterval == right.tokenInterval &&
-          left.arguments.keys == right.arguments.keys && {
-            // Get the key for which there is a mismatch
-            val mismatch = left.arguments.keys.find { key =>
-              val leftValues = left.arguments(key)
-              val rightValues = right.arguments(key)
+        left.document.eq(right.document) &&
+        left.sentence == right.sentence &&
+        left.labels == right.labels &&
+        left.tokenInterval == right.tokenInterval &&
+        left.arguments.keys == right.arguments.keys && {
+        // Get the key for which there is a mismatch
+        val mismatch = left.arguments.keys.find { key =>
+          val leftValues = left.arguments(key)
+          val rightValues = right.arguments(key)
 
-              if (leftValues.size != rightValues.size) true
-              // Expecting the same order is a little much
-              else leftValues.zip(rightValues).exists(pair => !areMatching(pair._1, pair._2))
-            }
-            mismatch.isEmpty
-          }
+          if (leftValues.size != rightValues.size) true
+          // Expecting the same order is a little much
+          else leftValues.zip(rightValues).exists(pair => !areMatching(pair._1, pair._2))
+        }
+        mismatch.isEmpty
+      }
     }
   }
-  
+
   def findMatchingPair(mention: Mention, mentions: Seq[Mention]): Option[(Mention, Mention)] =
-      mentions match {
-        case Seq() => None
-        case Seq(head) => if (areMatching(mention, head)) Some((mention, head)) else None
-        case Seq(head, tail @ _*) => if (areMatching(mention, head)) Some((mention, head)) else findMatchingPair(mention, tail)
-      }
+    mentions match {
+      case Seq() => None
+      case Seq(head) => if (areMatching(mention, head)) Some((mention, head)) else None
+      case Seq(head, tail @ _*) => if (areMatching(mention, head)) Some((mention, head)) else findMatchingPair(mention, tail)
+    }
 
   // Note that it is possible for this to recurse forever.
   // However, it is unlikely and this is only a test.
   def findMatchingPair(mentions: Seq[Mention]): Option[(Mention, Mention)] =
-      mentions match {
-        case Seq() => None
-        case Seq(head) => None
-        case Seq(head, tail @ _*) => {
-          val matchingPair = findMatchingPair(head, tail)
-          
-          if (matchingPair.isDefined) matchingPair
-          else findMatchingPair(tail)
-        }
+    mentions match {
+      case Seq() => None
+      case Seq(head) => None
+      case Seq(head, tail @ _*) => {
+        val matchingPair = findMatchingPair(head, tail)
+
+        if (matchingPair.isDefined) matchingPair
+        else findMatchingPair(tail)
       }
-  
+    }
+
   val reader = new EidosSystem()
-  
+
   protected def test(text: String, index: Int): Unit = {
     val annotatedDocument = reader.extractFromText(text)
     val someMentions = annotatedDocument.odinMentions
-    println("Some mentions:")
-    someMentions.foreach(m => DisplayUtils.displayMention(m))
     val uniqueMentions = findUniqueMentions(someMentions)
     val matchingPair = findMatchingPair(uniqueMentions)
 
     if (matchingPair.isDefined) {
-      println("***************************")
-      DisplayUtils.displayMention(matchingPair.get._1)
-      DisplayUtils.displayMention(matchingPair.get._2)
-
       val jValueLeft = WMJSONSerializer.jsonAST(Seq(matchingPair.get._1))
       val jValueRight = WMJSONSerializer.jsonAST(Seq(matchingPair.get._2))
       val jsonLeft = stringify(jValueLeft, pretty = true)
       val jsonRight = stringify(jValueRight, pretty = true)
 
-//      println(jsonLeft)
-//      println(jsonRight)
+      println(jsonLeft)
+      println(jsonRight)
 
       val jValueAll = WMJSONSerializer.jsonAST(someMentions)
       val jsonAll = stringify(jValueAll, pretty = true)
-//      println(jsonAll)
+      println(jsonAll)
     }
     it should "produce not just unique but also distinct mentions " + index in {
       matchingPair should be (None)
@@ -129,10 +122,10 @@ class TestEidosActions extends Test {
     "The government promotes improved cultivar to boost agricultural production for ensuring food security.",
     // This one is sent19 in the TestRaps
     "The governmental policy objective is to achieve food security, ensure adequate raw materials for the manufacturing sector, " +
-        "and increased export earnings through increased productivity, efficient input use, and better market access, " +
-        "infrastructure, and service development.",
+      "and increased export earnings through increased productivity, efficient input use, and better market access, " +
+      "infrastructure, and service development.",
     "This is a test."
   )
-  
+
   texts.zipWithIndex.foreach { case (text, index) => test(text, index) }
 }
