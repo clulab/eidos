31dc625dd2a12e023de6481449c6a70d72e3448b
Keith Alcock <github@keithalcock.com>
2018-03-30 10:13:49 -0700

    Forget the base and just be redundant

--- src/main/scala/org/clulab/wm/eidos/serialization/json/JLDSerializer.scala
+++ src/main/scala/org/clulab/wm/eidos/serialization/json/JLDSerializer.scala
@@ -112,13 +112,11 @@ class JLDSerializer(val adjectiveGrounder: Some[AdjectiveGrounder]) {
   def mkType(jldObject: JLDObject): (String, String) = mkType(jldObject.typename)
 
   def mkContext(): JObject = {
-    def mkContext(name: String): JField = new JField(name, "#" + name)
+    def mkContext(name: String): JField = new JField(name, JLDSerializer.base + "#" + name)
     
-    val base = new JField("@base", JLDSerializer.base)
-    val context = new JField("@context", base)
     val types = typenames.toList.sorted.map(mkContext)
     
-    new JObject(context +: types)
+    new JObject(types)
   }
   
   def mkRef(identity: Any): JObject = {
