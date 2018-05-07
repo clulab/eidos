963e6ef1403426261c39ff0f6a1375d721206fb5
Becky Sharp <bsharp@email.arizona.edu>
2018-05-06 22:31:24 -0700

    stripping metadata from grounding string, but this is a hack for now

--- src/main/scala/org/clulab/wm/eidos/utils/GroundingUtils.scala
+++ src/main/scala/org/clulab/wm/eidos/utils/GroundingUtils.scala
@@ -9,14 +9,17 @@ object GroundingUtils {
   def getBaseGrounding(mention: EidosMention): String = {
     val allGroundings = mention.grounding
     val baseSet: OntologyGrounding = allGroundings(EidosOntologyGrounder.UN_NAMESPACE)
-    baseSet.grounding.head._1
+    stripMetaDataType(baseSet.grounding.head._1)
   }
 
   // Gets the top k groundings from the desired ontology (identified by namespace: String), with scores
   def getGroundingsString(mention: EidosMention, namespace: String, topK: Int = 5): String = {
     val grounding = mention.grounding(namespace)
-    val topkGroundings = grounding.grounding.slice(0,topK)
+    val topkGroundings = grounding.grounding.slice(0,topK).map(grd => (stripMetaDataType(grd._1), grd._2))
     topkGroundings.mkString(", ")
   }
+
+  // temp method for stripping the unwanted metadat suffixes
+  def stripMetaDataType(s: String): String = s.stripSuffix("/examples").stripSuffix("/description")
 
 }
