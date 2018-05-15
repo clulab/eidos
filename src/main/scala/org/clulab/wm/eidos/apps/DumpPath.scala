a166c793d3df154c549d34260b8f3f022ab7c287
Becky Sharp <bsharp@email.arizona.edu>
2018-05-15 11:53:43 -0700

    fixed most bugs, some bad sentences still ~3%

--- src/main/scala/org/clulab/wm/eidos/apps/DumpPath.scala
+++ src/main/scala/org/clulab/wm/eidos/apps/DumpPath.scala
@@ -49,58 +49,96 @@ object DumpPath extends App {
     // if you want to use this instead!
     val interval = if (e1Int < e2Int) Interval(e1Int, e2Int + 1) else Interval(e2Int, e1Int + 1)
     // Odin uses lastOption for getting mention.synHead, so that's what we're doing here
-    val govHead = DependencyUtils.findHeads(interval, sentence.dependencies.get)
+    //println(s"finding head")
+    var govHead = DependencyUtils.findHeads(interval, sentence.dependencies.get)
       .lastOption
       .getOrElse(sentence.dependencies.get.roots.head)
+    if (govHead == -1) {
+      // If there is no head -- backoff to the shortest path between e1 and e2
+      //println("WARNING: no head found, using e1 as head!")
+//      println(s"e1Int: $e1Int")
+      govHead = e1Int
+    }
+
+    //println(s"finding shortest paths to head (${sentence.words(govHead)})")
     val shortestE1ToHead = sentence.dependencies.get.shortestPathEdges(e1Int, govHead, ignoreDirection = true)
     val shortestHeadToE2 = sentence.dependencies.get.shortestPathEdges(govHead, e2Int, ignoreDirection = true)
 
 
-
+    //println(s"finding lexicalized paths to head (${sentence.words(govHead)})")
     val headLexicalizedPaths = for {
       i <- shortestE1ToHead
       j <- shortestHeadToE2
     } yield lexicalizedPathGovHead(sentence.words, govHead, i, j)
+    //println("--> done.")
 
     for (path <- headLexicalizedPaths) {
       val toPrint = (fields.slice(0, 5) ++ Seq(path)).mkString("\t")
-      println(s"${sentence.getSentenceText()}")
-      println(s"\t${sentence.words.zipWithIndex.mkString(", ")}")
-      println(s"\te1: ${e1} ($e1Int)\te2: ${e2} ($e2Int) --> \n\t\theadlexPath: \t${path}")
+//      println(s"${sentence.getSentenceText()}")
+//      println(s"\t${sentence.words.zipWithIndex.mkString(", ")}")
+//      println(s"\te1: ${e1} ($e1Int)\te2: ${e2} ($e2Int) --> \n\t\theadlexPath: \t${path}")
 
       pw.println(toPrint)
     }
+
   }
 
   def loadInstances(filename: String): Unit = {
     //"m.0ccvx    m.05gf08    queens    belle_harbor    /location/location/contains    sen. charles e. schumer called on federal safety officials yesterday to reopen their investigation into the fatal crash of a passenger jet in belle_harbor , queens , because equipment failure , not pilot error , might have been the cause . ###END###"
     val pw = new PrintWriter(filename + ".nonlex_dep_path")
 
+    var badSentence: Int = 0
+
     val source = scala.io.Source.fromFile(filename)
     // todo: make a buffered reader??? (for memory issues)
-    val lines = source.getLines().toSeq
-    for ((line, lineIdx) <- lines.zipWithIndex) {
-      if (lineIdx % 10 == 0) {
-        println(s"Processing line ${lineIdx} of ${lines.length}")
+    val lines = source.getLines()
+    var lineCounter = 0
+    while (lines.hasNext) {
+      val line = lines.next()
+      if (lineCounter % 1000 == 0) {
+        println(s"Processing line ${lineCounter}")
+        println(s"CURRENT BAD SENTENCE: $badSentence")
       }
-      val fields = line.split("\t")
-      val e1 = fields(2)//.split("_")
-      val e2 = fields(3)//.split("_")
-      val sentText = fields(5).split("###END###").head
+      lineCounter += 1
+      //println(s"**LINE: $line")
+
+      var fields = line.split("\t")
+      //println(s"**FIELDS: ${fields.mkString(", ")}")
+
+      var sentText = fields(5).split("###END###").head
+      // Store the end punctuation
+      val endPunct = sentText.trim.last
+      //println(endPunct)
+
+      sentText = sentText.replaceAll("\\.", "") + endPunct
+      fields = fields.map(s => s.replaceAll("\\.", ""))
+      var e1 = fields(2)//.split("_")
+      var e2 = fields(3)//.split("_")
+
       val sentence: Sentence = mkPartialAnnotation(sentText).sentences.head
       //println(sentence.getSentenceText())
+      //println(s"**WORDS: ${sentence.words.mkString(";")}")
+
 
       // Entity tokens
       val e1Int: Int = sentence.words.indexOf(e1)
-      //println(s"\te1: $e1 $e1Int  [${sentence.words(e1Int)}]")
+      //println(s"\te1: $e1 $e1Int  ")//[${sentence.words(e1Int)}]")
       val e2Int = sentence.words.indexOf(e2)
-      //println(s"\te2: $e2 $e2Int  [${sentence.words(e2Int)}]")
+      //println(s"\te2: $e2 $e2Int ")// [${sentence.words(e2Int)}]")
 
-      // Fully lexicalized
-//      writeFullyLexicalized(sentence, fields, e1Int, e2Int, e1, e2, pw)
+      if (e1Int == -1 || e2Int == -1) {
+        badSentence += 1
+      }  else {
+        // Fully lexicalized
+        //      writeFullyLexicalized(sentence, fields, e1Int, e2Int, e1, e2, pw)
 
-      // Lexicalize only gove head
-      writeLexicalizeOnlyGovHead(sentence, fields, e1Int, e2Int, e1, e2, pw)
+        // Lexicalize only gove head
+        writeLexicalizeOnlyGovHead(sentence, fields, e1Int, e2Int, e1, e2, pw)
+
+      }
+
+
+
 
 
     }
@@ -198,7 +236,7 @@ object DumpPath extends App {
 
   def pathToText(path: Seq[(Int, Int, String, String)]): String = path.map(edge => edge._4 + edge._3).mkString(" ")
 
-  val fn = "/Users/bsharp/relationExtraction/RE/train.txt"
+  val fn = "/Users/bsharp/relationExtraction/RE/test.txt"
   loadInstances(fn)
 
 
