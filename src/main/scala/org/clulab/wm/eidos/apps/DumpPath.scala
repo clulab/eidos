2fed9234683e2eb73d69d3d97da3f16318af07fa
Becky Sharp <bsharp@email.arizona.edu>
2018-05-15 13:59:18 -0700

    changes and bugfixes

--- src/main/scala/org/clulab/wm/eidos/apps/DumpPath.scala
+++ src/main/scala/org/clulab/wm/eidos/apps/DumpPath.scala
@@ -1,6 +1,6 @@
 package org.clulab.wm.eidos
 
-import java.io.PrintWriter
+import java.io.{File, FilenameFilter, PrintWriter}
 
 import org.clulab.processors.{Document, Sentence}
 import org.clulab.odin.impl.TokenPattern
@@ -8,15 +8,88 @@ import org.clulab.processors.fastnlp.FastNLPProcessor
 import org.clulab.struct.Interval
 import org.clulab.utils.DependencyUtils
 import org.clulab.utils.DependencyUtils._
+import org.clulab.wm.eidos.utils.{Sourcer}
 
+import scala.collection.parallel.ForkJoinTaskSupport
 import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.forkjoin.ForkJoinPool
 
 object DumpPath extends App {
 
   val proc = new FastNLPProcessor()
 
+  def loadInstances(file: File): Unit = {
+    //"m.0ccvx    m.05gf08    queens    belle_harbor    /location/location/contains    sen. charles e. schumer called on federal safety officials yesterday to reopen their investigation into the fatal crash of a passenger jet in belle_harbor , queens , because equipment failure , not pilot error , might have been the cause . ###END###"
+    val filename = file.getAbsolutePath
+    val pw = new PrintWriter(filename + ".nonlex_dep_path")
+
+    var badSentence: Int = 0
+
+    val source = scala.io.Source.fromFile(filename)
+    // todo: make a buffered reader??? (for memory issues)
+    val lines = source.getLines()
+    //var lineCounter = 0
+    while (lines.hasNext) {
+      val line = lines.next()
+//      if (lineCounter % 1000 == 0) {
+//        println(s"Processing line ${lineCounter}")
+//        println(s"CURRENT BAD SENTENCE: $badSentence")
+//      }
+//      lineCounter += 1
+      //println(s"**LINE: $line")
+
+      var fields = line.split("\t")
+      //println(s"**FIELDS: ${fields.mkString(", ")}")
+
+      var sentText = fields(5).split("###END###").head
+      // Store the end punctuation
+      val endPunct = sentText.trim.last
+      //println(endPunct)
+
+//      sentText = sentText.replaceAll("\\.", "") + endPunct
+//      fields = fields.map(s => s.replaceAll("\\.", ""))
+      var e1 = fields(2)//.split("_")
+      var e2 = fields(3)//.split("_")
+
+      val sentence: Sentence = mkPartialAnnotation(sentText).sentences.head
+      //println(sentence.getSentenceText())
+     // println(s"**WORDS: ${sentence.words.mkString(";")}")
+
+      if (!sentence.words.contains(e1) || !sentence.words.contains(e2)) {
+        badSentence += 1
+        println(s"WARNING: bad sentence -- one of the entities not found! \n doc sentence text: ${sentence.getSentenceText()}")
+        //println(s"CURRENT BAD SENTENCE: $badSentence")
+      }  else {
+        // Entity tokens
+        val e1Ints: Seq[Int] = sentence.words.zipWithIndex.filter(_._1 == e1).unzip._2
+        //println(s"\te1: $e1 $e1Int  ")//[${sentence.words(e1Int)}]")
+
+        val e2Ints: Seq[Int] = sentence.words.zipWithIndex.filter(_._1 == e2).unzip._2
+        //println(s"\te2: $e2 $e2Int ")// [${sentence.words(e2Int)}]")
+        val pairs = for {
+          a <- e1Ints
+          b <- e2Ints
+        } yield (a,b)
+        val (e1Int, e2Int) = pairs.map(p => (p, math.abs(p._1 - p._2))).minBy(_._2)._1
+
+        // Fully lexicalized
+        //      writeFullyLexicalized(sentence, fields, e1Int, e2Int, e1, e2, pw)
+
+        // Lexicalize only gove head
+        writeLexicalizeOnlyGovHead(sentence, fields, e1Int, e2Int, e1, e2, pw)
+      }
+
+    }
+
+    println(s"TOTAL BAD SENTENCE for file ${filename} = $badSentence")
+    source.close()
+    pw.close()
+  }
+
+
   def mkPartialAnnotation(text: String): Document = {
-    val doc = proc.mkDocument(text)
+//    val doc = proc.mkDocument(text)
+    val doc = proc.mkDocumentFromTokens(Seq(text.split(" ")))
     proc.tagPartsOfSpeech(doc)
     proc.lemmatize(doc)
     proc.parse(doc)
@@ -35,9 +108,9 @@ object DumpPath extends App {
     val fullyLexicalizedPaths = shortestE1E2.map(path => lexicalizedPath(sentence.words, path))
     for (path <- fullyLexicalizedPaths) {
       val toPrint = (fields.slice(0, 5) ++ Seq(path)).mkString("\t")
-      println(s"${sentence.getSentenceText()}")
-      println(s"\t${sentence.words.zipWithIndex.mkString(", ")}")
-      println(s"\te1: ${e1} ($e1Int)\te2: ${e2} ($e2Int) --> lexPath ${path}")
+      //println(s"${sentence.getSentenceText()}")
+      //println(s"\t${sentence.words.zipWithIndex.mkString(", ")}")
+      //println(s"\te1: ${e1} ($e1Int)\te2: ${e2} ($e2Int) --> lexPath ${path}")
 
       pw.println(toPrint)
     }
@@ -55,15 +128,15 @@ object DumpPath extends App {
       .getOrElse(sentence.dependencies.get.roots.head)
     if (govHead == -1) {
       // If there is no head -- backoff to the shortest path between e1 and e2
-      //println("WARNING: no head found, using e1 as head!")
-//      println(s"e1Int: $e1Int")
+      println("WARNING: no head found, using e1 as head!")
       govHead = e1Int
     }
 
     //println(s"finding shortest paths to head (${sentence.words(govHead)})")
     val shortestE1ToHead = sentence.dependencies.get.shortestPathEdges(e1Int, govHead, ignoreDirection = true)
     val shortestHeadToE2 = sentence.dependencies.get.shortestPathEdges(govHead, e2Int, ignoreDirection = true)
-
+    //println(s"len shortestE1ToHead: ${shortestE1ToHead.length}")
+    //println(s"len shortestHeadToE2: ${shortestHeadToE2.length}")
 
     //println(s"finding lexicalized paths to head (${sentence.words(govHead)})")
     val headLexicalizedPaths = for {
@@ -74,80 +147,15 @@ object DumpPath extends App {
 
     for (path <- headLexicalizedPaths) {
       val toPrint = (fields.slice(0, 5) ++ Seq(path)).mkString("\t")
-//      println(s"${sentence.getSentenceText()}")
-//      println(s"\t${sentence.words.zipWithIndex.mkString(", ")}")
-//      println(s"\te1: ${e1} ($e1Int)\te2: ${e2} ($e2Int) --> \n\t\theadlexPath: \t${path}")
+      //println(s"${sentence.getSentenceText()}")
+      //println(s"\t${sentence.words.zipWithIndex.mkString(", ")}")
+      //println(s"\te1: ${e1} ($e1Int)\te2: ${e2} ($e2Int) --> \n\t\theadlexPath: \t${path}")
 
       pw.println(toPrint)
     }
 
   }
 
-  def loadInstances(filename: String): Unit = {
-    //"m.0ccvx    m.05gf08    queens    belle_harbor    /location/location/contains    sen. charles e. schumer called on federal safety officials yesterday to reopen their investigation into the fatal crash of a passenger jet in belle_harbor , queens , because equipment failure , not pilot error , might have been the cause . ###END###"
-    val pw = new PrintWriter(filename + ".nonlex_dep_path")
-
-    var badSentence: Int = 0
-
-    val source = scala.io.Source.fromFile(filename)
-    // todo: make a buffered reader??? (for memory issues)
-    val lines = source.getLines()
-    var lineCounter = 0
-    while (lines.hasNext) {
-      val line = lines.next()
-      if (lineCounter % 1000 == 0) {
-        println(s"Processing line ${lineCounter}")
-        println(s"CURRENT BAD SENTENCE: $badSentence")
-      }
-      lineCounter += 1
-      //println(s"**LINE: $line")
-
-      var fields = line.split("\t")
-      //println(s"**FIELDS: ${fields.mkString(", ")}")
-
-      var sentText = fields(5).split("###END###").head
-      // Store the end punctuation
-      val endPunct = sentText.trim.last
-      //println(endPunct)
-
-      sentText = sentText.replaceAll("\\.", "") + endPunct
-      fields = fields.map(s => s.replaceAll("\\.", ""))
-      var e1 = fields(2)//.split("_")
-      var e2 = fields(3)//.split("_")
-
-      val sentence: Sentence = mkPartialAnnotation(sentText).sentences.head
-      //println(sentence.getSentenceText())
-      //println(s"**WORDS: ${sentence.words.mkString(";")}")
-
-
-      // Entity tokens
-      val e1Int: Int = sentence.words.indexOf(e1)
-      //println(s"\te1: $e1 $e1Int  ")//[${sentence.words(e1Int)}]")
-      val e2Int = sentence.words.indexOf(e2)
-      //println(s"\te2: $e2 $e2Int ")// [${sentence.words(e2Int)}]")
-
-      if (e1Int == -1 || e2Int == -1) {
-        badSentence += 1
-      }  else {
-        // Fully lexicalized
-        //      writeFullyLexicalized(sentence, fields, e1Int, e2Int, e1, e2, pw)
-
-        // Lexicalize only gove head
-        writeLexicalizeOnlyGovHead(sentence, fields, e1Int, e2Int, e1, e2, pw)
-
-      }
-
-
-
-
-
-    }
-
-    source.close()
-    pw.close()
-
-
-  }
 
 
   // from, to, relation, direction
@@ -164,7 +172,7 @@ object DumpPath extends App {
       out.append(s"${edge._4}${edge._3} $rightWord")
       }
     out.mkString(" ")
-    }
+  }
 
   // from, to, relation, direction
   def lexicalizedPathGovHead(words: Seq[String],
@@ -182,18 +190,25 @@ object DumpPath extends App {
 
     // Check to make sure e1 isn't the head (i.e. there is a path)
     if (pathe1ToHead.nonEmpty) {
+      //println(s"PATH E1 to HEAD: ${pathe1ToHead.mkString(";")}")
       val start = pathe1ToHead.head
       val leftWord = if (start._4 == ">") words(start._1) else words(start._2)
+      //println(s"leftWord: $leftWord")
       val rightWord = if (start._4 == ">") words(start._2) else words(start._1)
+      //println(s"rightWord: $rightWord")
       val startRel = start._4 + start._3
       out.append(s"$leftWord $startRel")
 
       val restOfPath = pathe1ToHead.slice(1, pathe1ToHead.length)
       if (restOfPath.nonEmpty) {
+        //println("** rest of the path:")
         var lastWord = ""
         for (edge <- restOfPath) {
           val rightWord = if (edge._4 == ">") words(edge._2) else words(edge._1)
-          out.append(s"${edge._4}${edge._3}")
+          //println(s"rightWord: $rightWord")
+          val pathPiece = s"${edge._4}${edge._3}"
+          //println(s"pathPiece: $pathPiece")
+          out.append(pathPiece)
           //println(s"appended: ( ${edge._4}${edge._3} )")
           lastWord = rightWord
         }
@@ -210,20 +225,19 @@ object DumpPath extends App {
 
     // Check to make sure e2 isn't the head (i.e., there is a path)
     if (pathHeadToE2.nonEmpty) {
+      //println(s"PATH HEAD to E2: ${pathHeadToE2.mkString(";")}")
       // I think that the head will be the first thing here, and the path will follow to e2, but check
       val starte2 = pathHeadToE2.head
       assert(starte2._1 == headInt || starte2._2 == headInt)
+      //println(s"starte2: $starte2")
 
-      val restOfThePath2 = pathHeadToE2.slice(1, pathHeadToE2.length)
-      if (restOfThePath2.nonEmpty) {
-        var lastWord = ""
-        for (edge <- restOfThePath2) {
-          val rightWord = if (edge._4 == ">") words(edge._2) else words(edge._1)
-          out.append(s"${edge._4}${edge._3}")
-          lastWord = rightWord
-        }
-        out.append(lastWord)
+      var lastWord = ""
+      for (edge <- pathHeadToE2) {
+        val rightWord = if (edge._4 == ">") words(edge._2) else words(edge._1)
+        out.append(s"${edge._4}${edge._3}")
+        lastWord = rightWord
       }
+      out.append(lastWord)
     } else {
       // nothing, we're done
     }
@@ -236,8 +250,26 @@ object DumpPath extends App {
 
   def pathToText(path: Seq[(Int, Int, String, String)]): String = path.map(edge => edge._4 + edge._3).mkString(" ")
 
-  val fn = "/Users/bsharp/relationExtraction/RE/test.txt"
-  loadInstances(fn)
+  def findFilesPrefix(collectionDir: String, prefix: String): Seq[File] = {
+    val dir = new File(collectionDir)
+    val filter = new FilenameFilter {
+      def accept(dir: File, name: String): Boolean = name.startsWith(prefix)
+    }
+
+    val result = dir.listFiles(filter)
+    if (result == null)
+      throw Sourcer.newFileNotFoundException(collectionDir)
+    result
+  }
+
+  //val fn = "/Users/bsharp/relationExtraction/RE/test.txt"
+  val nCores = 3
+  val dir = "/Users/bsharp/relationExtraction/RE/chunked"
+  val files = findFilesPrefix(dir, "x").par
+  files.tasksupport = new ForkJoinTaskSupport(new ForkJoinPool(nCores))
+  for {
+    file <- files
+  } loadInstances(file)
 
 
 
