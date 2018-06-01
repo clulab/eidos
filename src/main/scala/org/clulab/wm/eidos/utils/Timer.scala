12c9d41ea5daa1b051ce2905d3f68b0d7c531b06
Keith Alcock <github@keithalcock.com>
2018-06-01 17:20:13 +0200

    Make timer log instead of print

--- src/main/scala/org/clulab/wm/eidos/utils/Timer.scala
+++ src/main/scala/org/clulab/wm/eidos/utils/Timer.scala
@@ -1,14 +1,19 @@
 package org.clulab.wm.eidos.utils
 
+import org.slf4j.LoggerFactory
+
 object Timer {
+  lazy val logger = LoggerFactory.getLogger(this.getClass())
 
   // See http://biercoff.com/easily-measuring-code-execution-time-in-scala/
   def time[R](description: String)(block: => R): R = {
-    println(description)
+    logger.info(description)
+
     val t0 = System.currentTimeMillis()
     val result: R = block    // call-by-name
     val t1 = System.currentTimeMillis()
-    println(s"Elapsed time: " + (t1 - t0) / 1000 + " sec.")
+    logger.info(s"Elapsed time: " + (t1 - t0) / 1000 + " sec.")
+    
     result
   }
 }
