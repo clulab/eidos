8afb4a21feb325619d2a5bd66aa96c307115625c
Keith Alcock <github@keithalcock.com>
2018-06-11 09:27:32 +0200

    Include the quote in the matched text

--- src/test/scala/org/clulab/wm/eidos/text/eval6/TestDoc2.scala
+++ src/test/scala/org/clulab/wm/eidos/text/eval6/TestDoc2.scala
@@ -178,7 +178,7 @@ class TestDoc2 extends Test {
     val tester = new Tester(text)
 
     // removed 'truly' from this test and quantifiers.tsv bc it's not gradable
-    val malnutrition = NodeSpec("levels of malnutrition", Quant("alarming"))
+    val malnutrition = NodeSpec("\"The levels of malnutrition", Quant("alarming"))
 
     behavior of "TestDoc2 Paragraph 6"
 
