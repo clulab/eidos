a38b7fe1e9efec9f9481b7784b46141120f1b27f
Keith Alcock <github@keithalcock.com>
2018-07-28 18:55:02 -0700

    Clean up groundings

--- src/main/scala/org/clulab/wm/eidos/utils/GroundingUtils.scala
+++ src/main/scala/org/clulab/wm/eidos/utils/GroundingUtils.scala
@@ -9,25 +9,17 @@ object GroundingUtils {
   def getBaseGrounding(mention: EidosMention): String = {
     val namespace = EidosOntologyGrounder.UN_NAMESPACE
 
-    if (mention.grounding.contains(namespace)) {
-      val grounding = mention.grounding(namespace)
-
-      stripMetaDataType(grounding.grounding.head._1)
-    }
+    if (mention.grounding.contains(namespace) && mention.grounding(namespace).nonEmpty)
+      mention.grounding(namespace).head._1
     else
       "(unavailable)"
   }
 
   // Get the top k groundings from the desired ontology (identified by namespace: String), with scores.
   def getGroundingsString(mention: EidosMention, namespace: String, topK: Int = 5, delim: String = ", "): String = {
-    if (mention.grounding.contains(namespace)) {
-      val grounding = mention.grounding(namespace)
-      val topkGroundings = grounding.grounding.take(topK).map(grd => (stripMetaDataType(grd._1), grd._2))
-      topkGroundings.mkString(delim)
-    }
-    else "(namespace unavailable)"
+    if (mention.grounding.contains(namespace))
+      mention.grounding(namespace).take(topK).mkString(delim)
+    else
+      "(namespace unavailable)"
   }
-
-  // Strip the unwanted metadata suffixes.
-  protected def stripMetaDataType(s: String): String = s.stripSuffix("/examples").stripSuffix("/description")
 }
