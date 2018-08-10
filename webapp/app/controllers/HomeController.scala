8eba628a40f175a2472032621c700f213db4295c
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2018-08-10 13:20:22 -0700

    don't normalize here

--- webapp/app/controllers/HomeController.scala
+++ webapp/app/controllers/HomeController.scala
@@ -53,10 +53,9 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
 
   // Entry method
   def parseSentence(text: String, cagRelevantOnly: Boolean) = Action {
-    val normalizedText = Normalizer.normalize(text, Normalizer.Form.NFD)
-    val (doc, eidosMentions, groundedEntities, causalEvents) = processPlaySentence(ieSystem, normalizedText, cagRelevantOnly)
+    val (doc, eidosMentions, groundedEntities, causalEvents) = processPlaySentence(ieSystem, text, cagRelevantOnly)
     println(s"Sentence returned from processPlaySentence : ${doc.sentences.head.getSentenceText}")
-    val json = mkJson(normalizedText, doc, eidosMentions, groundedEntities, causalEvents) // we only handle a single sentence
+    val json = mkJson(text, doc, eidosMentions, groundedEntities, causalEvents) // we only handle a single sentence
     Ok(json)
   }
 
