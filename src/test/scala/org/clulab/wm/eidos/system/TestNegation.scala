c6f3544b3923bd509b8de7a63a9aeba3c94b7b05
Becky Sharp <bsharp@email.arizona.edu>
2018-09-27 22:23:29 -0700

    Added negation handling... not curr exported
    ... to JSONLD

--- src/test/scala/org/clulab/wm/eidos/system/TestNegation.scala
+++ src/test/scala/org/clulab/wm/eidos/system/TestNegation.scala
@@ -1,5 +1,34 @@
 package org.clulab.wm.eidos.system
 
-class TestNegation {
+import org.clulab.wm.eidos.mentions.EidosEventMention
+import org.clulab.wm.eidos.test.TestUtils._
 
+class TestNegation extends Test {
+  it should "find no negation when not negated" in {
+    val text1 = "Rainfall causes flooding."
+    val mentions1 = extractEidosMentions(text1)
+    mentions1.filter(eem => eem.isInstanceOf[EidosEventMention])
+      .exists(m => m.asInstanceOf[EidosEventMention].negations.nonEmpty) should be (false)
+  }
+
+  it should "find no negation when there is a single negation" in {
+    val text1 = "Rainfall does not cause flooding."
+    val mentions1 = extractEidosMentions(text1)
+    mentions1.filter(eem => eem.isInstanceOf[EidosEventMention])
+      .exists(m => m.asInstanceOf[EidosEventMention].negations.size == 1) should be (true)
+  }
+
+  it should "find no negation when double negated" in {
+    val text1 = "Rainfall does not not cause flooding."
+    val mentions1 = extractEidosMentions(text1)
+    mentions1.filter(eem => eem.isInstanceOf[EidosEventMention])
+      .exists(m => m.asInstanceOf[EidosEventMention].negations.nonEmpty) should be (false)
+  }
+
+  it should "find negation when ridiculously (and of odd cardinality) negated" in {
+    val text1 = "Rainfall does not not not cause flooding."
+    val mentions1 = extractEidosMentions(text1)
+    mentions1.filter(eem => eem.isInstanceOf[EidosEventMention])
+      .exists(m => m.asInstanceOf[EidosEventMention].negations.size == 1) should be (true)
+  }
 }
