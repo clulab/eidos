5b93faebdc975477f0717141ebf51aad2386aacc
Becky Sharp <bsharp@email.arizona.edu>
2018-09-27 20:48:05 -0700

    Converted hedging to a TriggeredAttachment
    ... to help it go through to the jsonld.  Format is now trigger is the
    num of hedging words (as a string) and the quantifiers holds the list of
    the hedging terms

--- src/main/scala/org/clulab/wm/eidos/attachments/EidosAttachment.scala
+++ src/main/scala/org/clulab/wm/eidos/attachments/EidosAttachment.scala
@@ -315,21 +315,23 @@ object Time {
 }
 
 
-class Hedging(hedgingTerms: Seq[String]) extends ContextAttachment {
-  val text = hedgingTerms.mkString(", ")
-  val value = hedgingTerms
+class Hedging(trigger: String, quantifiers: Option[Seq[String]], triggerMention: Option[TextBoundMention] = None,
+  quantifierMentions: Option[Seq[Mention]] = None) extends TriggeredAttachment(trigger, quantifiers, triggerMention, quantifierMentions) {
 
-  override def newJLDAttachment(serializer: JLDEidosSerializer): JLDEidosAttachment =
-    newJLDContextAttachment(serializer, Hedging.kind)
+  override def canEqual(other: Any): Boolean = other.isInstanceOf[Hedging]
 
-  override def toJson(): JValue = toJson(Hedging.label)
+  override def newJLDAttachment(serializer: JLDEidosSerializer): JLDEidosAttachment = newJLDTriggeredAttachment(serializer, Hedging.kind)
+
+  override def toJson(): JValue = toJson(trigger)
 }
+
 
 object Hedging {
   val label = "Hedging"
   val kind = "HEDGE"
 
-  def apply(hedgingTerms: Seq[String]): Hedging = new Hedging(hedgingTerms)
+  def apply(hedgingTerms: Seq[String]): Hedging = Hedging(hedgingTerms.length.toString, Some(hedgingTerms))
+  def apply(num: String, hts: Option[Seq[String]]) = new Hedging(num, hts)
 }
 
 case class Score(score: Double) extends EidosAttachment {
