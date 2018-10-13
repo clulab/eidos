0237d2e2ebf72ef936d0b370658f05609d352561
Keith Alcock <github@keithalcock.com>
2018-10-12 22:45:49 -0700

    Don't forget to include already existing attachments

--- src/main/scala/org/clulab/wm/eidos/utils/MentionUtils.scala
+++ src/main/scala/org/clulab/wm/eidos/utils/MentionUtils.scala
@@ -7,14 +7,14 @@ object MentionUtils {
   def newCrossSentenceMention(m: CrossSentenceMention, attachments: Set[Attachment]) =
       new CrossSentenceMention(m.labels, m.anchor, m.neighbor, m.arguments, m.document, m.keep, m.foundBy, attachments)
 
-  def withAttachments(mention: Mention, seqOfAttachments: Seq[Attachment]): Mention = {
-    val setOfAttachments = seqOfAttachments.toSet
+  def withAttachments(mention: Mention, attachments: Seq[Attachment]): Mention = {
+    val allAttachments = mention.attachments ++ attachments
 
     mention match {
-      case m: TextBoundMention => m.copy(attachments = setOfAttachments)
-      case m: RelationMention => m.copy(attachments = setOfAttachments)
-      case m: EventMention => m.copy(attachments = setOfAttachments)
-      case m: CrossSentenceMention => newCrossSentenceMention(m, setOfAttachments)
+      case m: TextBoundMention => m.copy(attachments = allAttachments)
+      case m: RelationMention => m.copy(attachments = allAttachments)
+      case m: EventMention => m.copy(attachments = allAttachments)
+      case m: CrossSentenceMention => newCrossSentenceMention(m, allAttachments)
     }
   }
 }
