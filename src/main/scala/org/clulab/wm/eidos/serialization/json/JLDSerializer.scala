45bb67943d8cf3620183e1f004e27985d2d922a7
Keith Alcock <github@keithalcock.com>
2018-11-09 15:08:24 -0700

    Fix the jsonld context values

--- src/main/scala/org/clulab/wm/eidos/serialization/json/JLDSerializer.scala
+++ src/main/scala/org/clulab/wm/eidos/serialization/json/JLDSerializer.scala
@@ -105,7 +105,9 @@ class JLDSerializer(val adjectiveGrounder: Some[AdjectiveGrounder]) {
   def mkType(jldObject: JLDObject): (String, String) = mkType(jldObject.typename)
 
   def mkContext(): JObject = {
-    def mkContext(name: String): JField = new JField(name, JLDSerializer.base + "#" + name)
+    // The wiki turns <a id="Document"> into <a id="user-content-document">
+    // but w3id.org is not set up to lowercase the document, so it is done here in code.
+    def mkContext(name: String): JField = new JField(name, JLDSerializer.base + name.toLowerCase())
 
     val types = typenames.toList.sorted.map(mkContext)
 
@@ -137,7 +139,7 @@ class JLDSerializer(val adjectiveGrounder: Some[AdjectiveGrounder]) {
 }
 
 object JLDSerializer {
-  val base = "https://github.com/clulab/eidos/wiki/JSON-LD"
+  val base = "https://w3id.org/wm/cag/"
 }
 
 class JLDArgument(serializer: JLDSerializer, typeString: String, mention: EidosMention)
