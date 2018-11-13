30333f3f121e566eb45f44ccdb2bc67931e45c79
Gus Hahn-Powell <gushahnpowell@gmail.com>
2018-11-13 15:18:15 -0700

    Simplify grammar
    We're not doing anything with quantifiers at the moment

--- src/main/scala/org/clulab/wm/eidos/EidosActions.scala
+++ src/main/scala/org/clulab/wm/eidos/EidosActions.scala
@@ -25,7 +25,6 @@ import scala.collection.mutable.{ArrayBuffer, Set => MutableSet}
 
 class EidosActions(val taxonomy: Taxonomy, val expansionHandler: ExpansionHandler) extends EidosBaseActions with LazyLogging {
 
-
   /*
       Global Action -- performed after each round in Odin
   */
@@ -158,16 +157,6 @@ class EidosActions(val taxonomy: Taxonomy, val expansionHandler: ExpansionHandle
   /*
       Filtering Methods
    */
-
-  // This was essentially .head before, but that is dependent on order.
-  protected def tieBreaker(mentions: Seq[Mention]): Mention = {
-    val oldBest = mentions.head
-    val newBest = mentions.minBy(_.foundBy)
-
-//    if (!oldBest.eq(newBest))
-//      println("Changed answer")
-    newBest
-  }
 
   /**
     * @author Gus Hahn-Powell
@@ -381,31 +370,8 @@ class EidosActions(val taxonomy: Taxonomy, val expansionHandler: ExpansionHandle
   }
 
   /*
-      Mention State / Attachment Methods
-   */
-
-  //Rule to apply quantifiers directly to the state of an Entity (e.g. "small puppies") and
-  //Rule to add Increase/Decrease to the state of an entity
-  //TODO: perhaps keep token interval of the EVENT because it will be longer?
-  // todo BECKY: if trigger is also a quant (i.e. has a quant attachment), add as quant in inc/dec
-  def applyAttachment(ms: Seq[Mention], state: State): Seq[Mention] = {
-    for {
-      m <- ms
-      //if m matches "EntityModifier"
-      attachment = getAttachment(m)
-
-      copyWithMod = m match {
-        case tb: TextBoundMention => tb.copy(attachments = tb.attachments ++ Set(attachment), foundBy = s"${tb.foundBy}++mod")
-        // Here, we want to keep the theme that is being modified, not the modification event itself
-        case rm: RelationMention =>
-          val theme = tieBreaker(rm.arguments("theme")).asInstanceOf[TextBoundMention]
-          theme.copy(attachments = theme.attachments ++ Set(attachment), foundBy = s"${theme.foundBy}++${rm.foundBy}")
-        case em: EventMention =>
-          val theme = tieBreaker(em.arguments("theme")).asInstanceOf[TextBoundMention]
-          theme.copy(attachments = theme.attachments ++ Set(attachment), foundBy = s"${theme.foundBy}++${em.foundBy}")
-      }
-    } yield copyWithMod
-  }
+    Mention State / Attachment Methods
+  */
 
   def applyTimeAttachment(ms: Seq[Mention], state: State): Seq[Mention] = {
     for {
@@ -441,9 +407,6 @@ class EidosActions(val taxonomy: Taxonomy, val expansionHandler: ExpansionHandle
     println("DEBUG ACTION")
     ms
   }
-
-  def getAttachment(mention: Mention): EidosAttachment = EidosAttachment.newEidosAttachment(mention)
-
 
   def pass(mentions: Seq[Mention], state: State): Seq[Mention] = mentions
 
--- src/main/scala/org/clulab/wm/eidos/actions/EidosBaseActions.scala
+++ src/main/scala/org/clulab/wm/eidos/actions/EidosBaseActions.scala
@@ -1,6 +1,7 @@
 package org.clulab.wm.eidos.actions
 
-import org.clulab.odin.{Actions, Mention, State}
+import org.clulab.odin._
+import org.clulab.wm.eidos.attachments.EidosAttachment
 
 /** All *event* actions in Eidos must implement this trait */
 trait EidosBaseActions extends Actions {
@@ -9,4 +10,42 @@ trait EidosBaseActions extends Actions {
 
   def keepMostCompleteEvents(mentions: Seq[Mention], state: State): Seq[Mention]
 
+
+
+
+
+  // This was essentially .head before, but that is dependent on order.
+  def tieBreaker(mentions: Seq[Mention]): Mention = {
+    val oldBest = mentions.head
+    val newBest = mentions.minBy(_.foundBy)
+
+    //    if (!oldBest.eq(newBest))
+    //      println("Changed answer")
+    newBest
+  }
+
+  //Rule to apply quantifiers directly to the state of an Entity (e.g. "small puppies") and
+  //Rule to add Increase/Decrease to the state of an entity
+  //TODO: perhaps keep token interval of the EVENT because it will be longer?
+  // todo BECKY: if trigger is also a quant (i.e. has a quant attachment), add as quant in inc/dec
+  def applyAttachment(ms: Seq[Mention], state: State): Seq[Mention] = {
+    for {
+      m <- ms
+      //if m matches "EntityModifier"
+      attachment = getAttachment(m)
+
+      copyWithMod = m match {
+        case tb: TextBoundMention => tb.copy(attachments = tb.attachments ++ Set(attachment), foundBy = s"${tb.foundBy}++mod")
+        // Here, we want to keep the theme that is being modified, not the modification event itself
+        case rm: RelationMention =>
+          val theme = tieBreaker(rm.arguments("theme")).asInstanceOf[TextBoundMention]
+          theme.copy(attachments = theme.attachments ++ Set(attachment), foundBy = s"${theme.foundBy}++${rm.foundBy}")
+        case em: EventMention =>
+          val theme = tieBreaker(em.arguments("theme")).asInstanceOf[TextBoundMention]
+          theme.copy(attachments = theme.attachments ++ Set(attachment), foundBy = s"${theme.foundBy}++${em.foundBy}")
+      }
+    } yield copyWithMod
+  }
+
+  def getAttachment(mention: Mention): EidosAttachment = EidosAttachment.newEidosAttachment(mention)
 }
