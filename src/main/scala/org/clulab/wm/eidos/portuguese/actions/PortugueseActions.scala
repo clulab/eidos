affb5af1373087711400135455c2837b5f1a8326
gcgbarbosa <gcgbarbosa@gmail.com>
2018-11-27 13:02:48 -0700

    cleaning

--- src/main/scala/org/clulab/wm/eidos/portuguese/actions/PortugueseActions.scala
+++ src/main/scala/org/clulab/wm/eidos/portuguese/actions/PortugueseActions.scala
@@ -3,16 +3,10 @@ package org.clulab.wm.eidos.portuguese.actions
 import org.clulab.odin.{Mention, State, TextBoundMention}
 import org.clulab.struct.Interval
 import org.clulab.wm.eidos.actions.{AttachmentHandler, EidosBaseActions}
-
 import scala.collection.mutable.ArrayBuffer
 
 /** Actions for Portuguese **/
 class PortugueseActions extends EidosBaseActions {
-
-//  /** ??? */
-//  def filterTriggers(mentions: Seq[Mention], state: State): Seq[Mention] = mentions.filterNot{ m =>
-//    m.text.endsWith("ção")
-//  }
 
   def checkOverlapWithAvoid(mentions: Seq[Mention], state: State): Seq[Mention] = {
     val (entities, other) = mentions.partition(_.matches("Entity"))
@@ -27,7 +21,6 @@ class PortugueseActions extends EidosBaseActions {
         case discard if overlapping.exists{om => om.matches("Avoid.*".r) && ! om.matches("PossibleTrigger")} =>
           Nil
         // trim me.  I overlap with a possible trigger.
-        // FIXME: Implement this split!
         case trimEntity if triggers.nonEmpty =>
           var finalMentions : Seq[TextBoundMention] = Seq()
           // get set of trigger indexes
@@ -46,18 +39,19 @@ class PortugueseActions extends EidosBaseActions {
           // [0,3] and [4,5]
           var i=0
           while(i < mentionSize) {
+            // find start of the sequence
             if(mentionMask(i) == 1){
               val start=i+m.start
+              // find end of the sequence
               while(i < mentionSize && mentionMask(i) == 1){
                 i=i+1
               }
               val end=i+m.start+1
-              val newInterval = Interval(start, end)
-              finalMentions = finalMentions ++ Seq(tbm.copy(tokenInterval = newInterval))
+              // add result to final mentions
+              finalMentions = finalMentions ++ Seq(tbm.copy(tokenInterval = Interval(start, end)))
             }
             i=i+1
           }
-          finalMentions.foreach(fm => println(fm.text))
           finalMentions
         case _ => Seq(m)
       }
@@ -65,7 +59,6 @@ class PortugueseActions extends EidosBaseActions {
     }
     (validEntities ++ other).distinct
   }
-
   /** identity action */
   def globalAction(mentions: Seq[Mention], state: State): Seq[Mention] = mentions
 
