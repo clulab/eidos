b8f1b8aff53596cd730a29db33cd286764ca1400
Andrew Zupon <zupon@email.arizona.edu>
2019-06-06 13:56:02 -0700

    add stub for migration attachments, prepare to merge tests

--- src/main/scala/org/clulab/wm/eidos/actions/MigrationUtils.scala
+++ src/main/scala/org/clulab/wm/eidos/actions/MigrationUtils.scala
@@ -1,7 +1,10 @@
 package org.clulab.wm.eidos.actions
 
-import org.clulab.odin.Mention
+import org.clulab.odin.{EventMention, Mention, State, TextBoundMention}
 import org.clulab.wm.eidos.EidosSystem
+import org.clulab.wm.eidos.attachments.Location
+import org.clulab.wm.eidos.context.GeoPhraseID
+import org.clulab.wm.eidos.document.EidosDocument
 
 object MigrationUtils {
   def processMigrationEvents(mentions: Seq[Mention]): Seq[Mention] = {
@@ -9,8 +12,34 @@ object MigrationUtils {
     val (migrationEvents, other) = mentions.partition(_ matches EidosSystem.MIGRATION_LABEL)
 
     // TODO: do
-    val handled = migrationEvents
+
+//    val handled = migrationEvents
     // todo: add attachments for Time and Location (start with overlapping already found NN ones, see action)
+//     def applyLocationAttachmentMigration(ms: Seq[Mention]): Seq[Mention] = {
+//       for {
+//         m <- ms
+// //        trigger = m.asInstanceOf[EventMention].trigger
+// //        locations = Set("moveTo", "moveFrom", "moveThrough")
+//         locationMention = m.arguments("moveTo").head
+// //        theme = tieBreaker(m.arguments("theme")).asInstanceOf[TextBoundMention]
+//         geolocs = m.document.asInstanceOf[EidosDocument].geolocs
+//         location: Option[GeoPhraseID] = if (geolocs.isDefined) geolocs.get(m.sentence).find(_.startOffset == locationMention.startOffset) else None
+//         locationAttachment = if (location.isEmpty) locationMention else locationMention.withAttachment(new Location(location.head))
+//         } yield m.
+// //        })
+// //      } yield m.arguments ++ (location match {
+// //        case None => locationMention
+// //        case Some(l) => locationMention.withAttachment(new Location(l))
+// //      })
+//     }
+    // w/ locationMention, we get correct attachment to Location but lose humanMigration event
+    // copy copyWithnewArgs from ArgumentExpander.scala in Eidos
+
+    // val handled = applyLocationAttachmentMigration(migrationEvents)
+
+    val handled = migrationEvents
+
+
     // todo: backoff times and locations -- use the normalization apis
     // todo: combine times (timeStart/timeEnd)????
     // todo: aggregation of cross-sentence stuff?????????????
