06f48e8aa4be8eda2e81574f75d679bda6f39469
Maria <maxaalexeeva@gmail.com>
2019-06-11 14:56:36 -0700

    prototype for sent assembly (within same sent?)

--- src/main/scala/org/clulab/wm/eidos/actions/MigrationUtils.scala
+++ src/main/scala/org/clulab/wm/eidos/actions/MigrationUtils.scala
@@ -43,21 +43,76 @@ object MigrationUtils {
     // todo: aggregation of cross-sentence stuff?????????????
 
     // return all
-    handled ++ other
+//    handled ++ other
+    noArgOverlap(assemble(handled)) ++ other
   }
 
-//  def assemble(mentions: Seq[Mention]): Seq[Mention] = {
-//    val groupedMentions = mentions.groupBy(_.sentence)
-//    val afterAssembly = for  {
-//      group <- groupedMentions
-//      e <- group._2
-//      e1 <- group._2
-//      if (e != e1 && e.arguments.values.toList.intersect(e1.arguments.values.toList).nonEmpty)
+  def assemble(mentions: Seq[Mention]): Seq[Mention] = {
+    val groupedMentions = mentions.groupBy(_.sentence)
+    val afterAssembly = for  {
+      group <- groupedMentions
+      e <- group._2
+      e1 <- group._2
+      if (e != e1 && e.arguments.values.toList.intersect(e1.arguments.values.toList).nonEmpty)
+      //      argIntersectAll = e.arguments.keys.toList.intersect(e1.arguments.keys.toList)
+      //      diffValuesArg = for {
+      //        argName <- argIntersectAll
+      //        if (e.arguments(argName)!=e1.arguments(argName))
+      //      } yield argName
+      //      overlapArgAsSeq = Map(diffValuesArg.head -> Seq(e.arguments(diffValuesArg.head).head, e1.arguments(diffValuesArg.head).head))
+
+    } yield copyWithNewArgs(e, e1.arguments ++ e.arguments)
+
+    val handled = if (afterAssembly.nonEmpty) mostArgs(afterAssembly.toSeq) else mentions
+    handled
+  }
+
+
+
+  def mostArgs(mentions: Seq[Mention]): Seq[Mention] = {
+    val sorted = mentions.sortBy(_.arguments.toList.length).reverse
+    val mostArguments = sorted.head
+    Seq(mostArguments)
+  }
+
+  def noArgOverlap(mentions: Seq[Mention]): Seq[Mention] = {
+    val groupedMentions = mentions.groupBy(_.sentence)
+    val afterAssembly = for  {
+      group <- groupedMentions
+      e <- group._2
+      e1 <- group._2
+      if (e != e1 && e.arguments.values.toList.intersect(e1.arguments.values.toList).isEmpty)
+      //      argIntersectAll = e.arguments.keys.toList.intersect(e1.arguments.keys.toList)
+      //      diffValuesArg = for {
+      //        argName <- argIntersectAll
+      //        if (e.arguments(argName)!=e1.arguments(argName))
+      //      } yield argName
+      //      overlapArgAsSeq = Map(diffValuesArg.head -> Seq(e.arguments(diffValuesArg.head).head, e1.arguments(diffValuesArg.head).head))
+
+    } yield copyWithNewArgs(e, e1.arguments ++ e.arguments)
+
+    val handled = if (afterAssembly.nonEmpty) mostArgs(afterAssembly.toSeq) else mentions
+    handled
+  }
+
+
+
 //
-//    } yield copyWithNewArgs(e, e1.arguments ++ e.arguments)
-//
-//    afterAssembly.toSeq
+//  def mostArgs(mentions: Seq[Mention]): Seq[Mention] = {
+//    val sorted = mentions.sortBy(_.arguments.toList.length).reverse
+//    var handled = List(sorted.head)
+//    for (m <- sorted.tail) {
+//      val mArgs = m.arguments.values.toList
+//      for (m2 <- handled) {
+//        val m2Args = m2.arguments.values.toList
+//        if (m2Args contains mArgs) {
+//          handled :+ m
+//        }
+//      }
+//    }
+//    handled
 //  }
+
 
   def copyWithNewArgs(orig: Mention, expandedArgs: Map[String, Seq[Mention]], foundByAffix: Option[String] = None, mkNewInterval: Boolean = true): Mention = {
     // Helper method to get a token interval for the new event mention with expanded args
@@ -87,3 +142,27 @@ object MigrationUtils {
   }
 
 }
+
+//attempting to not return one migratio event over the whole paragraph. mainly failed.
+//def assemble(mentions: Seq[Mention]): Seq[Mention] = {
+//  val groupedMentions = mentions.groupBy(_.sentence)
+//  val afterAssembly = for {
+//  group <- groupedMentions
+//  allSentEvents = for {
+//  e <- group._2
+//  e1 <- group._2
+//  if (e != e1 && e.arguments.values.toList.intersect(e1.arguments.values.toList).nonEmpty)
+//  //      argIntersectAll = e.arguments.keys.toList.intersect(e1.arguments.keys.toList)
+//  //      diffValuesArg = for {
+//  //        argName <- argIntersectAll
+//  //        if (e.arguments(argName)!=e1.arguments(argName))
+//  //      } yield argName
+//  //      overlapArgAsSeq = Map(diffValuesArg.head -> Seq(e.arguments(diffValuesArg.head).head, e1.arguments(diffValuesArg.head).head))
+//
+//} yield copyWithNewArgs(e, e1.arguments ++ e.arguments)
+//
+//} yield mostArgs(allSentEvents).head
+//
+//  val handled = if (afterAssembly.nonEmpty) afterAssembly.toSeq else mentions
+//  handled
+//}
