a726630da0e2fd486719c74d9938c433a361b5d2
Keith Alcock <github@keithalcock.com>
2019-09-03 10:16:10 -0700

    Match Tokenizer to new version in processors

--- src/main/scala/org/clulab/wm/eidos/EidosProcessor.scala
+++ src/main/scala/org/clulab/wm/eidos/EidosProcessor.scala
@@ -2,8 +2,6 @@ package org.clulab.wm.eidos
 
 import java.util.regex.Pattern
 
-import org.antlr.v4.runtime.CommonTokenStream
-import org.antlr.v4.runtime.Token
 import org.clulab.processors.Processor
 import org.clulab.processors.Sentence
 import org.clulab.processors.clu.PortugueseCluProcessor
@@ -12,6 +10,7 @@ import org.clulab.processors.clu.tokenizer.RawToken
 import org.clulab.processors.clu.tokenizer.SentenceSplitter
 import org.clulab.processors.clu.tokenizer.Tokenizer
 import org.clulab.processors.fastnlp.FastNLPProcessor
+import org.clulab.wm.eidos.utils.Timer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -32,7 +31,7 @@ class EidosEnglishProcessor(val language: String, cutoff: Int) extends FastNLPPr
     with SentencesExtractor with LanguageSpecific {
   override lazy val tokenizer = new EidosTokenizer(localTokenizer, cutoff)
 
-  // TODO: Move this to processors.
+  // TODO: This should be checked with each update of processors.
   def extractSentences(text: String): Array[Sentence] = {
     // This mkDocument will now be subject to all of the EidosProcessor changes.
     val document = mkDocument(text, keepText = false)
@@ -50,7 +49,7 @@ class EidosSpanishProcessor(val language: String, cutoff: Int) extends SpanishCl
     with SentencesExtractor with LanguageSpecific {
   override lazy val tokenizer = new EidosTokenizer(localTokenizer, cutoff)
 
-  // TODO: Move this to processors.
+  // TODO: This should be checked with each update of processors.
   def extractSentences(text: String): Array[Sentence] = {
     // This mkDocument will now be subject to all of the EidosProcessor changes.
     val document = mkDocument(text, keepText = false)
@@ -68,7 +67,7 @@ class EidosPortugueseProcessor(val language: String, cutoff: Int) extends Portug
     with SentencesExtractor with LanguageSpecific {
   override lazy val tokenizer = new EidosTokenizer(localTokenizer, cutoff)
 
-  // TODO: Move this to processors.
+  // TODO: This should be checked with each update of processors.
   def extractSentences(text: String): Array[Sentence] = {
     // This mkDocument will now be subject to all of the EidosProcessor changes.
     val document = mkDocument(text, keepText = false)
@@ -88,8 +87,11 @@ class ParagraphSplitter {
   // Neither of the other tokens should be end of sentence (eosPattern) characters themselves.
   // At this stage periods have not been combined with their abbreviations, so they are separate.
   def split(text: String, tokens: Array[RawToken]): Array[RawToken] = {
-    val hasEoses = tokens.map { token => ParagraphSplitter.eosPattern.matcher(token.word).matches }
+    // See https://stackoverflow.com/questions/11391337/java-pattern-matcher-create-new-or-reset
+    val eosMatcher = ParagraphSplitter.eosPattern.matcher("")
+    val hasEoses = tokens.map { token => eosMatcher.reset(token.word).matches }
     val newTokens = new ArrayBuffer[RawToken]()
+    val eopMatcher = ParagraphSplitter.eopPattern.matcher("")
 
     tokens.indices.foreach { index =>
       val prevToken = tokens(index)
@@ -103,7 +105,7 @@ class ParagraphSplitter {
             if (nextTokenOpt.isDefined) nextTokenOpt.get.beginPosition
             else text.length
         val whitespace = text.slice(beginPosition, endPosition)
-        val hasEop = ParagraphSplitter.eopPattern.matcher(whitespace).matches
+        val hasEop = eopMatcher.reset(whitespace).matches
 
         if (hasEop) {
           // The sentenceSplitter could refrain from using this period in an abbreviation
@@ -127,43 +129,13 @@ class EidosTokenizer(tokenizer: Tokenizer, cutoff: Int) extends Tokenizer(
 ) {
   val paragraphSplitter = new ParagraphSplitter()
 
-  // TODO This should be inherited from a method in Tokenizer
-  protected def readTokens(text: String): Array[RawToken] = {
-
-    def newRawToken(token: Token): RawToken = {
-      val word = token.getText
-      val beginPosition = token.getStartIndex
-      val endPosition = token.getStopIndex + 1 // antlr is inclusive on end position, we are exclusive
-
-      assert(beginPosition + word.length == endPosition)
-      RawToken(word, beginPosition)
-    }
-
-    val tokens: CommonTokenStream = lexer.mkLexer(text)
-    val rawTokenBuffer = new ArrayBuffer[RawToken]()
-
-    def processToken(token: Token): Boolean = {
-      if (token.getType == -1)
-        false
-      else {
-        rawTokenBuffer += newRawToken(token)
-        true
-      }
-    }
-
-    while (processToken(tokens.LT(1)))
-      tokens.consume()
-    rawTokenBuffer.toArray
-  }
-
-  // See Tokenizer for this method's template.
   override def tokenize(text: String, sentenceSplit: Boolean = true): Array[Sentence] = {
-    val tokens = readTokens(text)
-    val rawTokens = steps.foldLeft(tokens) { (tokens, step) =>
-      step.process(tokens)
+    val rawTokens = readTokens(text)
+    val stepTokens = steps.foldLeft(rawTokens) { (rawTokens, step) =>
+      step.process(rawTokens)
     }
     // The first major change is with the added paragraphSplitter.
-    val paragraphTokens = paragraphSplitter.split(text, rawTokens)
+    val paragraphTokens = Timer.time("paragraph splitting") { paragraphSplitter.split(text, stepTokens) }
     val sentences = sentenceSplitter.split(paragraphTokens, sentenceSplit)
     // The second change is to filter by sentence length.
     val shortSentences = sentences.filter { sentence => sentence.words.length < cutoff }
