31ad4fb5d7461f4cde627f190aa0165b8a0d57d3
Keith Alcock <github@keithalcock.com>
2020-01-21 21:09:31 -0700

    Force text to single line.
    It will not be properly indented otherwise.

--- src/main/scala/org/clulab/wm/eidos/utils/OdinUtils.scala
+++ src/main/scala/org/clulab/wm/eidos/utils/OdinUtils.scala
@@ -11,10 +11,13 @@ object OdinUtils {
   // string. The escape character is the backslash (e.g., \). If the string is a valid Java identifier, the quotes
   // can be omitted. For example, word=dog matches the word “dog”.
   // So, to be on the safe side, escape escape, escape existing quotes, add surrounding quotes.
+  // Furthermore, text needs to stay on a single line, so also translate \n and \r.
   def escapeExactStringMatcher(string: String): String = {
     val escaped = string
         .replaceAllLiterally("\\", "\\\\")
         .replaceAllLiterally("\"", "\\\"")
+        .replaceAllLiterally("\n", "\\n")
+        .replaceAllLiterally("\r", "\\r")
 
     '"' + escaped + '"'
   }
