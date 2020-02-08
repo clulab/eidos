08e0a9248822f567f8f38f0bdd8798417bd80492
Keith Alcock <github@keithalcock.com>
2020-02-08 14:02:57 -0700

    Adjust query

--- sparql/src/main/scala/ai/lum/eidos/sparql/apps/DumpOntology.scala
+++ sparql/src/main/scala/ai/lum/eidos/sparql/apps/DumpOntology.scala
@@ -1,42 +1,56 @@
 package ai.lum.eidos.sparql.apps
 
 import java.io.File
+import java.io.PrintWriter
 
 import ai.lum.eidos.sparql.data.Dataset
 import ai.lum.eidos.sparql.data.Ontology
 import ai.lum.eidos.sparql.utils.Closer.AutoCloser
 import ai.lum.eidos.sparql.utils.Sinker
 import ai.lum.eidos.sparql.utils.StringUtils
-import org.apache.jena.query.QuerySolution
-import org.apache.jena.query.ResultSetFormatter
 import org.apache.jena.rdfconnection.RDFConnection
 import org.apache.jena.rdfconnection.RDFConnectionFuseki
 
 object DumpOntology extends App {
+  val host = "http://localhost:3030"
 
-  def mkConnection(dataset: String): RDFConnection = {
-    val host = s"http://localhost:3030/$dataset/query"
+  def mkConnection(datasetName: String): RDFConnection = {
+    val endpoint = s"$host/$datasetName/query"
     val builder = RDFConnectionFuseki
         .create()
-        .destination(host)
+        .destination(endpoint)
 
     builder.build
   }
 
-  def mkQuery(dataset: String): String =
+  def mkQuery(datasetName: String, ontologyName: String): String =
     s"""
+      |PREFIX data-prov: <http://ontology.causeex.com/ontology/odps/DataProvenance#>
+      |PREFIX event: <http://ontology.causeex.com/ontology/odps/Event#>
+      |PREFIX gc: <http://ontology.causeex.com/ontology/odps/GeneralConcepts#>
+      |PREFIX icm: <http://ontology.causeex.com/ontology/odps/ICM#>
       |PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      |PREFIX lcc: <http://www.languagecomputer.com/lcc#>
+      |PREFIX src: <http://graph.causeex.com/documents/sources#>
       |
-      |SELECT ?subject ?predicate ?object
-      |FROM <http://localhost:3030/$dataset/data/$dataset>
+      |SELECT ?text
+      |FROM <$host/$datasetName/data/$datasetName>
       |WHERE {
-      |  ?subject ?predicate ?object
+      |    ?doc a data-prov:Document;                         # Find a document.
+      |        data-prov:contains* ?component.                # It must have some components.
+      |    ?event data-prov:sourced_from* ?component;         # Some events are sourced_from these components.
+      |        a ?eventType.                                  # They are of a certain event type.
+      |    ?eventType rdfs:subClassOf* event:Event.           # That event type is a subClassOf Event.
+      |    ?event icm:has_factor ?factor.                     # The event has_factor factor
+      |    ?factor icm:has_factor_type $ontologyName.         # And that factor has_factor_type of a certain value
+      |    ?component data-prov:text_value ?text.             # In that case, get the text of the component.
       |}
-      |LIMIT 25
+      |
+      |LIMIT 100
       |""".stripMargin
 
   def mkFile(ontologyName: String): File = {
-    new File(StringUtils.afterFirst(ontologyName, ':') + ".txt")
+    new File("./texts/" + StringUtils.afterFirst(ontologyName, ':') + ".txt")
   }
 
   // Make sure the text stays on one line and can be used in tsv file.
@@ -46,27 +60,39 @@ object DumpOntology extends App {
       .replace("\n", "\\n")
       .replace("\r", "\\r")
 
-  def run(ontologyName: String): Unit = {
+  def run(countPrintWriter: PrintWriter, ontologyName: String): Unit = {
     var count = 0
 
-    Sinker.newBufferedOutputStream(mkFile(ontologyName)).autoClose { outputStream =>
+    Sinker.printWriterFromFile(mkFile(ontologyName)).autoClose { printWriter =>
       Dataset.names.foreach { datasetName =>
-        val connection = mkConnection(datasetName)
-        val query = mkQuery(datasetName)
+        // There seem to be multiple events of the same kind in the same sentence.
+        // The query delivers them in order, so this is essentially implements DISTINCT.
+        var prevText = ""
 
-        connection.queryResultSet(query, { resultSet =>
-          ResultSetFormatter.outputAsTSV(outputStream, resultSet)
-//          val text = ResultSetFormatter.asText(subject)
-//          val text = subject.toString
-          count += 1
-//          printWriter.println(escape(text))
-        })
+        mkConnection(datasetName).autoClose { connection =>
+          val query = mkQuery(datasetName, ontologyName)
+
+          connection.queryResultSet(query, { resultSet =>
+            while (resultSet.hasNext()) {
+              val querySolution = resultSet.next
+              val text = querySolution.getLiteral("text").getString
+
+              if (text != prevText) {
+                printWriter.println(escape(text))
+                count += 1
+              }
+              prevText = text
+            }
+          })
+        }
       }
     }
-    println(s"For ontology $ontologyName there were $count entries.")
+    countPrintWriter.println(s"$ontologyName\t$count")
   }
 
-  Ontology.names.foreach { ontologyName =>
-    run(ontologyName)
+  Sinker.printWriterFromFile("counts.txt").autoClose { printWriter =>
+    Ontology.names.foreach { ontologyName =>
+      run(printWriter, ontologyName)
+    }
   }
 }
