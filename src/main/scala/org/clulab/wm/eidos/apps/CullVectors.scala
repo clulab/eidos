2d63d92682265f4ad30180315a215067a88c6918
Keith Alcock <github@keithalcock.com>
2020-03-17 22:35:51 -0700

    Use frequencies when culling

--- src/main/scala/org/clulab/wm/eidos/apps/CullVectors.scala
+++ src/main/scala/org/clulab/wm/eidos/apps/CullVectors.scala
@@ -18,8 +18,15 @@ object CullVectors extends App {
   def normalize(values: Array[Float]): Array[Float] = {
     val length = l2(values)
 
+    require(length > 0)
     values.indices.foreach { index => values(index) /= length }
     values
+  }
+
+  def addWeightedVec(dest: Array[Float], freq: Int, src: Array[Float]): Unit = {
+    dest.indices.foreach { index =>
+      dest(index) += src(index) * freq
+    }
   }
 
   val inVectorFile = new File(args(0))
@@ -28,82 +35,73 @@ object CullVectors extends App {
   val outputFile = new File(args(3))
   val limit = args(4).toInt
 
+  // The words in gigaword have been lowercased and include these substitutions.
   val substitutions = Seq(
     ("-lrb-", "("), ("-rrb-", ")"), // round
     ("-lsb-", "["), ("-rsb-", "]"), // square
     ("-lcb-", "{"), ("-rcb-", "}")  // curvy
   )
-  val freqentWords = Sourcer.sourceFromFile(inFrequencyFile).autoClose { source =>
+  // This is Map[word, (index, freq)].  The index is used for separating frequent from infrequent words.
+  // The freq is used to eventually weight the vectors for each word when
+  val wordFrequencies: Map[String, (Int, Int)] = Sourcer.sourceFromFile(inFrequencyFile).autoClose { source =>
+    val counter = Counter(-1)
     val frequentWords = source
         .getLines
-        // In this case, the others are simply ignored.  If their frequencies were to
-        // be taken into account, that would have to change.
-        .take(limit)
-        .map(StringUtils.beforeFirst(_, '\t'))
-        .map(_.toLowerCase) // We're punting here.  Lowercase will be compared to lowercase.
-        .map { token =>
-          substitutions.foldLeft(token) { case (token, (remove, insert)) =>
-            token.replace(remove, insert)
+        .map { line =>
+          val Array(rawWord, freq) = line.split('\t')
+          val cookedWord = substitutions.foldLeft(rawWord) { case (word, (remove, insert)) =>
+            word.replace(remove, insert)
           }
-        }
-        .toSet
+
+          cookedWord -> (counter.inc(), freq.toInt)
+        }.toMap
 
     frequentWords
   }
   val reservedWords = {
     val tableDomainOntology = new TableDomainOntology
         .TableDomainOntologyBuilder(null, null, false)
-        .build("two_six", "../two_six")
+        .build("two_six", inOntologyDir)
     val values = tableDomainOntology
         .indices
         .flatMap(tableDomainOntology.getValues(_))
+        // These are lowercased to match gigaword.
         .map(_.toLowerCase)
         .toSet
 
     values
   }
-  val (goodLines, badLines) = Sourcer.sourceFromFile(inVectorFile).autoClose { source =>
-    val goodAndBadLines = source
-        .getLines()
-        .drop(1)
-        .toSeq
-        .groupBy { line =>
-          val word = StringUtils
-              .beforeFirst(line, ' ')
-              .toLowerCase
+  // There must be a better way than to open the file twice.
+  val columns = Sourcer.sourceFromFile(inVectorFile).autoClose { source =>
+    val line = source.getLines.take(1).toSeq.head
+    val Array(_, columns) =line.split(' ')
 
-          reservedWords.contains(word) || freqentWords.contains(word)
-        }
+    columns.toInt
+  }
+  val badFloats = new Array[Float](columns)
+  val goodLines = Sourcer.sourceFromFile(inVectorFile).autoClose { source =>
+    source.getLines.drop(1).filter { line =>
+      val word = StringUtils
+          .beforeFirst(line, ' ')
+          .toLowerCase
+      val (index, freq) = wordFrequencies.getOrElse(word, (-1, 0))
+      // 0 <= index implies wordFrequencies.contains(word).
+      val good = reservedWords.contains(word) || (0 <= index && index < limit)
 
-    (goodAndBadLines(true), goodAndBadLines(false))
+      if (!good) { // Need to add to bad line
+        val floats = StringUtils.afterFirst(line, ' ').split(' ').map(_.toFloat)
+
+        addWeightedVec(badFloats, freq, floats)
+      }
+      good
+    }.toVector
   }
   val count = goodLines.size + 1
-  val columns = goodLines.head.count(_ == ' ')
-  val badLine = {
-    val badFloats = new Array[Float](columns)
-
-    badLines.foreach { line =>
-      val values = line
-          .split(' ')
-          .drop(1)
-          .map(_.toFloat)
-      val normalizedValues = normalize(values)
-
-      assert(normalizedValues.length == badFloats.length)
-      // Normalize them each before adding.
-      normalizedValues
-          .indices
-          .foreach(index => badFloats(index) += normalizedValues(index))
-    }
-
-    // Although the vectors are not normalized to begin with, we'll normalize it now.
-    // Word2Vec normalizes all incoming vectors.  Doing it twice will not hurt.
-    val normalizedBadFloats = normalize(badFloats)
-    val badStrings = normalizedBadFloats.map(_.toString)
-    val badLine = " " + badStrings.mkString(" ")
-
-    badLine
-  }
+  // Although the vectors are not normalized to begin with, we'll normalize it now.
+  // Word2Vec normalizes all incoming vectors.  Doing it twice will not hurt.
+  val normalizedBadFloats = normalize(badFloats)
+  val badStrings = normalizedBadFloats.map(_.toString)
+  val badLine = " " + badStrings.mkString(" ")
 
   // The \n is to force LF as eol even on Windows.
   FileUtils.printWriterFromFile(outputFile).autoClose { printWriter =>
