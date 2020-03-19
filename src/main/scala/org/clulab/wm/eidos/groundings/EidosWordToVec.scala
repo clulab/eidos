4d4814547c4f7fb67c5544083edeaa84e648ff0a
Keith Alcock <github@keithalcock.com>
2020-03-19 00:03:27 -0700

    Implement initial version with backoff

--- src/main/scala/org/clulab/wm/eidos/groundings/EidosWordToVec.scala
+++ src/main/scala/org/clulab/wm/eidos/groundings/EidosWordToVec.scala
@@ -1,55 +1,53 @@
 package org.clulab.wm.eidos.groundings
 
-import org.clulab.wm.eidos.groundings.CompactWord2Vec
-import org.clulab.wm.eidos.groundings.Word2VecUtils
 import org.clulab.wm.eidos.utils.Namer
 import org.slf4j.{Logger, LoggerFactory}
 
 trait EidosWordToVec {
+  // This returns, for compatability reasons, the vector that would traditionally result from
+  // makeCompositeVector() if all the words are out of vocabulary (OOV).
+  def unknownCompositeVector: Array[Float]
+
   // This makes vectors for both arrays
   def calculateSimilarities(canonicalNameParts: Array[String], conceptEmbeddings: Seq[ConceptEmbedding]): EidosWordToVec.Similarities
 
   // This is used to make the vector/array for a single bunch of strings so that they can be compared to another vector.
-  def makeCompositeVector(texts: Iterable[String]): Array[Float]
+  def makeCompositeVector(texts: Seq[String]): Option[Array[Float]]
 }
 
 class FakeWordToVec extends EidosWordToVec {
 
+  def unknownCompositeVector: Array[Float] = Array.emptyFloatArray
+
   def calculateSimilarities(canonicalNameParts: Array[String], conceptEmbeddings: Seq[ConceptEmbedding]): EidosWordToVec.Similarities = Seq.empty
 //  def calculateSimilarities(canonicalNameParts: Array[String], conceptEmbeddings: ConceptEmbeddings): Seq[(String, Float)] = Seq(("hello", 5.0f))
 
-  def makeCompositeVector(texts: Iterable[String]): Array[Float] = Array.emptyFloatArray
+  def makeCompositeVector(texts: Seq[String]): Option[Array[Float]] = None
 }
 
-class RealWordToVec(val w2v: CompactWord2Vec, topKNodeGroundings: Int) extends EidosWordToVec {
+class RealWordToVec(val w2v: SanitizedWord2Vec, topKNodeGroundings: Int) extends EidosWordToVec {
 
-  // Maybe need to pass in list of transformations that might be used to find it?
+  def unknownCompositeVector: Array[Float] = w2v.unknownVector
 
-  def calculateSimilarities(canonicalNameParts: Array[String], conceptEmbeddings: Seq[ConceptEmbedding]): EidosWordToVec.Similarities = {
-    val sanitizedNameParts = canonicalNameParts.map(Word2VecUtils.sanitizeWord(_))
-    val outOfVocabulary = sanitizedNameParts.forall { sanitizedNamePart =>
-      w2v.isOutOfVocabulary(sanitizedNamePart) && w2v.isOutOfVocabulary(sanitizedNamePart.toLowerCase)
-    }
+  def calculateSimilarities(words: Array[String], conceptEmbeddings: Seq[ConceptEmbedding]): EidosWordToVec.Similarities = {
+    val nodeEmbeddingOpt = makeCompositeVector(words)
 
-    if (outOfVocabulary)
-      Seq.empty
-    else {
-      val nodeEmbedding = makeCompositeVector(sanitizedNameParts)
+    nodeEmbeddingOpt.map { nodeEmbedding =>
       val similarities = conceptEmbeddings.map { conceptEmbedding =>
         (conceptEmbedding.namer, w2v.dotProduct(conceptEmbedding.embedding, nodeEmbedding))
       }
 
       similarities.sortBy(-_._2).take(topKNodeGroundings)
-    }
+    }.getOrElse(Seq.empty)
   }
 
-  // These texts are expected to be sanitized in advance.  For instance, their cases should match
-  // that used in the vector file.
-  def makeCompositeVector(texts: Iterable[String]): Array[Float] = w2v.makeCompositeVector(texts)
+  def makeCompositeVector(words: Seq[String]): Option[Array[Float]] = w2v.makeCompositeVector(words)
 }
 
 object EidosWordToVec {
   type Similarities = Seq[(Namer, Float)]
+
+  protected val builder = new SanitizedWord2VecBuilder()
 
   protected lazy val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
@@ -61,8 +59,8 @@ object EidosWordToVec {
       logger.info(s"Loading w2v from $wordToVecPath...")
 
       val w2v =
-        if (cached) CompactWord2Vec(makeCachedFilename(cachedPath, wordToVecPath), resource = false, cached)
-        else CompactWord2Vec(wordToVecPath, resource = true, cached)
+        if (cached) builder.build(makeCachedFilename(cachedPath, wordToVecPath), resource = false, cached)
+        else builder.build(wordToVecPath, resource = true, cached)
 
       new RealWordToVec(w2v, topKNodeGroundings)
     }
--- src/main/scala/org/clulab/wm/eidos/groundings/SanitizedWord2Vec.scala
+++ src/main/scala/org/clulab/wm/eidos/groundings/SanitizedWord2Vec.scala
@@ -1,54 +1,51 @@
 package org.clulab.wm.eidos.groundings
 
-import java.io.BufferedInputStream
-import java.io.BufferedOutputStream
-import java.io.{FileInputStream, FileOutputStream, ObjectOutputStream}
-
 import org.clulab.embeddings.word2vec.CompactWord2Vec
-import org.clulab.embeddings.word2vec.CompactWord2Vec.MapType
-import org.clulab.utils.Closer.AutoCloser
-import org.clulab.utils.{ClassLoaderObjectInputStream, Sourcer}
-import org.slf4j.{Logger, LoggerFactory}
 
-import scala.collection.immutable.HashMap
-import scala.collection.mutable.{HashMap => MutableHashMap, Map => MutableMap}
-
-class SanitizedWord2Vec(buildType: CompactWord2Vec.BuildType, sanitizerOpt: Option[Sanitizer] = None) extends CompactWord2Vec(buildType){
-  protected val unsanitizedMap: CompactWord2Vec.MapType = map
-  protected val sanitizedMap: Option[CompactWord2Vec.MapType] = sanitizerOpt.map { sanitizer =>
-    map // .map { case (key, value) => sanitizer.sanitize(key) -> value }
-  }
+class SanitizedWord2Vec(buildType: SanitizedWord2VecBuilder.BuildType, sanitizer: Sanitizer) extends CompactWord2Vec(buildType){
   protected val unknownRowOpt: Option[Int] = map.get("")
 
-  protected val cachedUnknownVector: CompactWord2Vec.ArrayType = {
-    val result = new CompactWord2Vec.ArrayType(columns)
+  protected val (sanitizedMap, unsanitizedMap) = {
+    val groupedMaps = map.groupBy[Boolean] { case (key, value) =>
+      key.startsWith("\t") // Sanitized words begin with tab
+    }
+    val sanitizedMap = groupedMaps.get(true).map { map =>
+      map.map { case (key, value) =>
+        key.substring(1) -> value // Remove the tab.
+      }
+    }.getOrElse(SanitizedWord2VecBuilder.emptyMap)
+    val unsanitizedMap = groupedMaps.getOrElse(false, SanitizedWord2VecBuilder.emptyMap)
+
+    map.clear // It is no longer necessary to hold the original map.
+    (sanitizedMap, unsanitizedMap)
+  }
+
+  protected val cachedUnknownVector: SanitizedWord2VecBuilder.ArrayType = {
+    val result = new SanitizedWord2VecBuilder.ArrayType(columns)
 
     if (unknownRowOpt.isDefined)
       add(result, unknownRowOpt.get)
     result
   }
 
-  def unknownVector: CompactWord2Vec.ArrayType = cachedUnknownVector
+  def unknownVector: SanitizedWord2VecBuilder.ArrayType = cachedUnknownVector
 
-  // TODO: switch getRow to getSanitizedRow getUnsanitizedRow so don't have to make choice all the time.
   protected def getRow(word: String): Option[Int] = {
     // Prefer the word when possible.
     val bestRowOpt = unsanitizedMap.get(word)
     // Make do with the sanitized word if not.
     val betterRowOpt = bestRowOpt.orElse {
-      sanitizerOpt.flatMap { sanitizer =>
-        val sanitizedWordOpt = sanitizer.sanitize(word)
+      val sanitizedWordOpt = sanitizer.sanitize(word)
 
-        // Keep in mind that sanitization may erase the entire word
-        // and tht the sanitized map may not contain the word anyway.
-        sanitizedWordOpt.map(sanitizedMap.get(_))
-      }
+      // Keep in mind that sanitization may erase the entire word
+      // and tht the sanitized map may not contain the word anyway.
+      sanitizedWordOpt.flatMap(sanitizedMap.get(_))
     }
 
     betterRowOpt
   }
 
-  protected def add(dest: CompactWord2Vec.ArrayType, freq: Int, srcRow: Int): Unit = {
+  protected def add(dest: SanitizedWord2VecBuilder.ArrayType, freq: Int, srcRow: Int): Unit = {
     val srcOffset = srcRow * columns
     var i = 0 // optimization
 
@@ -77,32 +74,23 @@ class SanitizedWord2Vec(buildType: CompactWord2Vec.BuildType, sanitizerOpt: Opti
   // whatever grounding happens to match the vectors for unknown words.  If any grounding should
   // be associated with that at all, it will be up to the caller to find it.  Returning None
   // also does away with immediate need for the previously used isOutOfVocabulary() method.
-  def makeCompositeVector(words: Seq[String]): Option[CompactWord2Vec.ArrayType] = {
+  def makeCompositeVector(words: Seq[String]): Option[SanitizedWord2VecBuilder.ArrayType] = {
     val rows = words.flatMap { word => getRow(word) }
 
     if (rows.nonEmpty) {
-      val total = new CompactWord2Vec.ArrayType(columns) // Start with this.
+      val total = new SanitizedWord2VecBuilder.ArrayType(columns) // Start with this.
       val count = // This is how many vectors will be added.
-          if (unknownRowOpt.isDefined) rows.size
-          else words.size
+        if (unknownRowOpt.isDefined) rows.size
+        else words.size
 
       rows.foreach(add(total, _))
       if (unknownRowOpt.isDefined) // Account for misses.
-        add(total, count - rows.size, unknownRowOpt.get)
+      add(total, count - rows.size, unknownRowOpt.get)
       if (count > 1) // Only normalize when required.
-        norm(total)
+      norm(total)
       Some(total)
     }
     else
       None // It is completely OOV!
   }
-}
-
-object SanitizedWord2Vec {
-  type MapType = MutableMap[String, Int]
-  type ValueType = Float
-  type ArrayType = Array[ValueType]
-
-  protected type BuildType = (MapType, ArrayType)
-  protected type StoreType = (String, ArrayType)
 }
