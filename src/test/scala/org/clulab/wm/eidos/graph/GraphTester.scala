bff09603d761a2c182ffd95c2abd3d6aa0318b50
Keith Alcock <github@keithalcock.com>
2020-04-07 13:55:12 -0700

    Make output more helpful

--- src/test/scala/org/clulab/wm/eidos/graph/GraphTester.scala
+++ src/test/scala/org/clulab/wm/eidos/graph/GraphTester.scala
@@ -7,6 +7,7 @@ import org.clulab.wm.eidos.graph.TestResult.TestResults
 import org.clulab.wm.eidos.mentions.EidosMention
 import org.clulab.wm.eidos.test.TestUtils
 
+import scala.collection.JavaConverters._
 import scala.collection.Seq
 
 class GraphTester(ieSystem: EidosSystem, text: String) {
@@ -35,33 +36,53 @@ class GraphTester(ieSystem: EidosSystem, text: String) {
 
   protected def toString(mentions: Seq[Mention]): String = {
     mentions.zipWithIndex.map { case (mention, index) =>
-      s"$index: ${mention.text} ${mention.attachments.mkString(", ")}"
-    }.mkString("\n")
+      val id = mention.getClass.getSimpleName + "@" + System.identityHashCode(mention)
+
+      s"#$index: ($id) = ${mention.text} ${mention.attachments.mkString(", ") }"
+    }.mkString("\n\t")
   }
 
-  protected def annotateTest(result: Seq[String]): Seq[String] =
+  protected def annotateTest(graphSpec: GraphSpec, result: Seq[String]): Seq[String] = {
     if (result == TestUtils.successful)
       result
-    else
-      result ++ Seq("Mentions:\n" + toString(mentions))
+    else Seq(
+      Seq("\nErrors:\n"),
+      Seq(result.mkString("\n\t")),
+      Seq("\nMentions:\n"),
+      Seq(toString(mentions)),
+      Seq("\nFound:\n"),
+      Seq(testResults.keySet.asScala.toSeq.map { graphSpec =>
+        val testResult = testResults.get(graphSpec)
+        val mentionOpt = testResult.mention
+        val string = mentionOpt.map { mention =>
+          val id = mention.getClass.getSimpleName + "@" + System.identityHashCode(mention)
+          val index = mentions.indexOf(mention)
+
+          s"$graphSpec: ($id) = [#$index], ${mention.text} ${mention.attachments.mkString(", ") }"
+        }.getOrElse(s"$graphSpec: None")
+
+        string
+      }.mkString("\n\t"))
+    ).flatten
+  }
 
   def test(nodeSpec: NodeSpec): Seq[String] = {
     val testResult = nodeSpec.test(mentions, useTimeNorm, useGeoNorm, testResults)
 
-    annotateTest(testResult.complaints)
+    annotateTest(nodeSpec, testResult.complaints)
   }
 
   def test(edgeSpec: EdgeSpec): Seq[String] = {
     val testResult = edgeSpec.test(mentions, useTimeNorm, useGeoNorm, testResults)
 
-    annotateTest(testResult.complaints)
+    annotateTest(edgeSpec, testResult.complaints)
   }
 
 
   def test(humanMigrationEdgeSpec: HumanMigrationEdgeSpec): Seq[String] = {
     val testResult = humanMigrationEdgeSpec.test(mentions, useTimeNorm, useGeoNorm, testResults)
 
-    annotateTest(testResult.complaints)
+    annotateTest(humanMigrationEdgeSpec, testResult.complaints)
   }
 
   def useTimeNorm: Boolean = ieSystem.components.useTimeNorm
