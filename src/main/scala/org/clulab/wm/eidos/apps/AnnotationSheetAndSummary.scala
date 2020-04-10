275daba0620e7da01ce1735d7828eaa4f7d5bd1d
bsharpataz <bsharp@email.arizona.edu>
2020-04-09 17:10:55 -0700

    app for generating the summary as well as the annotation sheet

--- src/main/scala/org/clulab/wm/eidos/apps/AnnotationSheetAndSummary.scala
+++ src/main/scala/org/clulab/wm/eidos/apps/AnnotationSheetAndSummary.scala
@@ -2,20 +2,28 @@ package org.clulab.wm.eidos.apps
 
 
 import java.io.File
+import java.util.Calendar
 
 import com.typesafe.config.{Config, ConfigFactory}
+import org.clulab.struct.Counter
 import org.clulab.utils.Configured
 import org.clulab.wm.eidos.EidosSystem
-import org.clulab.wm.eidos.apps.ReconstituteAndExport.getArgString
 import org.clulab.wm.eidos.document.AnnotatedDocument
+import org.clulab.wm.eidos.exporters.GroundingAnnotationExporter
 import org.clulab.wm.eidos.serialization.json.JLDDeserializer
-import org.clulab.wm.eidos.utils.{FileUtils, ThreadUtils}
+import org.clulab.wm.eidos.utils.{CsvWriter, FileUtils, FoundBy, ThreadUtils}
+import org.clulab.wm.eidos.utils.Closer.AutoCloser
+
 
 object AnnotationSheetAndSummary extends App with Configured {
 
   collection.parallel.ForkJoinTasks.defaultForkJoinPool
   lazy val reader = new EidosSystem()
   lazy val deserializer = new JLDDeserializer()
+  val groundAs = getArgStrings("apps.groundAs", None)
+  val topN = getArgInt("apps.groundTopN", Some(5))
+  val exporter = GroundingAnnotationExporter("", reader, groundAs, topN)
+
   val inputDir = getArgString("apps.inputDirectory", None)
   val outputDir = getArgString("apps.outputDirectory", None)
   val inputExtension = getArgString("apps.inputFileExtension", None)
@@ -40,8 +48,34 @@ object AnnotationSheetAndSummary extends App with Configured {
   val parFiles = ThreadUtils.parallelize(files, nCores)
   val annotatedDocuments = parFiles.map(getInput)
 
-  
+  // For each file in the input directory:
+  val relevantMentions = parFiles.seq
+    // extract the annotated documents from the files
+    .map(f => getInput(f))
+    // filter the eidos mentions in each to be only relevant ones
+    .flatMap(ad => reader.components.stopwordManager.relevantMentions(ad))
 
+  // tally the rules in the found mentions
+  val ruleCounter = new Counter[String]
+  relevantMentions.foreach(m => ruleCounter.incrementCount(m.odinMention.foundBy))
 
+  // make a row for the sheet for each EidosMention
+  val rows = exporter.getRows(relevantMentions)
+
+  val timestamp = Calendar.getInstance.getTime
+
+  new CsvWriter(FileUtils.printWriterFromFile(s"$outputDir/rule_annotation.tsv")).autoClose { csvWriter1 =>
+    new CsvWriter(FileUtils.printWriterFromFile(s"$outputDir/rule_summary.tsv")).autoClose { csvWriter2 =>
+
+      // Sheet 1 -- Extraction Data
+      csvWriter1.println(s"EIDOS Rule Annotation -- generated $timestamp")
+      exporter.printHeader(csvWriter1)
+      rows.foreach(csvWriter1.println)
+      // Sheet 2 -- Summary Statistics
+      csvWriter2.println(AnnotationTSV.headers2)
+      val summaryRows = AnnotationTSV.counterToRows(ruleCounter, ruleCounter = "Q", correctColumn = "L")
+      summaryRows.foreach(csvWriter2.println)
+    }
+  }
 
 }
