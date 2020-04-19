640d2e2f7a6dd124fa0486730056d9b9dfcac378
bsharpataz <bsharp@email.arizona.edu>
2020-04-19 11:12:54 -0700

    modified filtering to make more robust, test added for 50 %

--- src/test/scala/org/clulab/wm/eidos/system/TestFiltering.scala
+++ src/test/scala/org/clulab/wm/eidos/system/TestFiltering.scala
@@ -23,4 +23,13 @@ class TestFiltering extends ExtractionTest {
     val valid = ieSystem.components.stopwordManager.keepCAGRelevant(causal)
     valid should be(empty)
   }
+
+  it should "not find entities that have numbers and stops only" in {
+    val text1 = "The 50% causes flooding."
+    val mentions1 = extractMentions(text1)
+    val causal = mentions1.filter(_ matches EidosSystem.CAUSAL_LABEL)
+    causal should have size(1)
+    val valid = ieSystem.components.stopwordManager.keepCAGRelevant(causal)
+    valid should be(empty)
+  }
 }
