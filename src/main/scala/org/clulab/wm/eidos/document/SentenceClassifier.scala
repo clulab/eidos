2995cef0085f6322f36fe7e74f564f1660d0cf5e
Zhengzhong Liang <zhengzhongliang@Zhengzhongs-MacBook-Pro.local>
2020-06-16 07:48:58 -0700

    adding a little code to sentence classifier

--- src/main/scala/org/clulab/wm/eidos/document/SentenceClassifier.scala
+++ src/main/scala/org/clulab/wm/eidos/document/SentenceClassifier.scala
@@ -2,11 +2,22 @@ package org.clulab.wm.eidos.document
 
 import com.typesafe.config.Config
 import org.clulab.processors.Sentence
-import org.clulab.wm.eidos.utils.Language
+import org.clulab.wm.eidos.groundings.FullTreeDomainOntology.FullTreeDomainOntologyBuilder
+import org.clulab.wm.eidos.utils.{Canonicalizer, Language}
 
 class SentenceClassifier {
 
+  // Load ontology embeddings and idf
+  val a = ontologyHandlerOpt
+  val canonicalizer = new Canonicalizer(stopwordManager, tagSet)
+  val filter = true
+  val ontologyYaml = "wm_compositional"
+  val fullTreeDomainOntology = new FullTreeDomainOntologyBuilder(sentenceExtractor, canonicalizer, filter).buildFromYaml(ontologyYaml)
+
   def classify(sentence: Sentence): Float = {
+    for (word <- sentence.words){
+
+    }
     1.0f
   }
 }
