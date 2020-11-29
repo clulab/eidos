04cb66b720b0d96af3b0ac6e71aeca96b18b976b
Keith Alcock <github@keithalcock.com>
2020-11-28 21:19:56 -0700

    Backport after Lum merge

--- src/main/scala/org/clulab/wm/eidos/groundings/grounders/EidosOntologyGrounder.scala
+++ src/main/scala/org/clulab/wm/eidos/groundings/grounders/EidosOntologyGrounder.scala
@@ -7,11 +7,11 @@ import org.clulab.wm.eidos.groundings.DomainOntology
 import org.clulab.wm.eidos.groundings.EidosWordToVec
 import org.clulab.wm.eidos.groundings.IndividualGrounding
 import org.clulab.wm.eidos.groundings.OntologyAliases
+import org.clulab.wm.eidos.groundings.OntologyAliases.MultipleOntologyGrounding
+import org.clulab.wm.eidos.groundings.OntologyAliases.OntologyGroundings
 import org.clulab.wm.eidos.groundings.OntologyGrounder
 import org.clulab.wm.eidos.groundings.OntologyGrounding
 import org.clulab.wm.eidos.groundings.SingleOntologyNodeGrounding
-import org.clulab.wm.eidos.groundings.OntologyAliases.MultipleOntologyGrounding
-import org.clulab.wm.eidos.groundings.OntologyAliases.OntologyGroundings
 import org.clulab.wm.eidos.mentions.EidosMention
 import org.clulab.wm.eidos.utils.Canonicalizer
 import org.clulab.wm.eidos.utils.StringUtils
