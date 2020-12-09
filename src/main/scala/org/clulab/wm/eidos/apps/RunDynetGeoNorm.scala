d5d29bf1edfcfc8518aad1104d2d007ab2c4e850
Keith Alcock <github@keithalcock.com>
2020-12-09 12:04:23 -0700

    Cherry pick geo test program

---
+++ src/main/scala/org/clulab/wm/eidos/apps/RunDynetGeoNorm.scala
@@ -0,0 +1,39 @@
+package org.clulab.wm.eidos.apps
+
+import ai.lum.common.ConfigUtils._
+import com.typesafe.config.Config
+import org.clulab.odin.Mention
+import org.clulab.odin.State
+import org.clulab.processors.Document
+import org.clulab.wm.eidos.EidosSystem
+import org.clulab.wm.eidos.context.GeoNormFinder
+
+object RunDynetGeoNorm extends App {
+
+  def fromConfig(config: Config): GeoNormFinder = {
+    val model = config[String]("model")
+    // Do metal stuff.
+
+    // Eventually the code should be just
+    // dynetGeoNorm.fromConfig(config)
+    // but for now, it is this:
+    null
+  }
+
+  def find(doc: Document, initialState: State): Seq[Mention] = {
+    // Eventually the code should be just
+    // dynetGeoNorm.find(doc, initialState)
+    // but for now, it is this:
+    Seq.empty
+  }
+
+  val text = "Early rainfall in Ethiopia improves harvests tremendously." // args(0)
+  val config = EidosSystem.defaultConfig
+  val dynetGeoNormFinder = fromConfig(config[Config]("geonorm"))
+  val eidosSystem = new EidosSystem(config)
+  val doc = eidosSystem.annotate(text)
+  val mentions = eidosSystem.extractMentionsFrom(doc)
+  val state = new State()
+
+  find(doc, state)
+}
