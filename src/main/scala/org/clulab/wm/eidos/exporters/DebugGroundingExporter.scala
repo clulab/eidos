731c613238e6d9ce6497b34b4a1525c09bfb96f2
zupon <zupon@email.arizona.edu>
2021-01-11 10:51:48 -0700

    remove unused / commented out code and comments

--- src/main/scala/org/clulab/wm/eidos/exporters/DebugGroundingExporter.scala
+++ src/main/scala/org/clulab/wm/eidos/exporters/DebugGroundingExporter.scala
@@ -24,7 +24,6 @@ class DebugGroundingExporter(filename: String, reader: EidosSystem, reground: Bo
     // top 5 tuples
     FileUtils.printWriterFromFile(filename + ".debug.txt").autoClose { pw =>
 
-//      pw.println("********************************************")
       val doc = annotatedDocument.document
       for (i <- doc.sentences.indices) {
         val clusent = reader.components.procOpt.get.asInstanceOf[EidosCluProcessor].annotate(doc.sentences(i)
@@ -33,7 +32,6 @@ class DebugGroundingExporter(filename: String, reader: EidosSystem, reground: Bo
         pw.println(s"Sentence $i: ${clusent.getSentenceText}.\n")
         pw.println("SRLS:")
         pw.println(clusent.enhancedSemanticRoles.getOrElse(None))
-//        pw.println()
         pw.println("DEPS:")
         pw.println(clusent.dependencies.get)
         val mentions = annotatedDocument.eidosMentions.filter(_.odinMention.sentence == i)
@@ -71,105 +69,20 @@ class DebugGroundingExporter(filename: String, reader: EidosSystem, reground: Bo
                     val tuple = gr.asInstanceOf[PredicateGrounding].predicateTuple
                     val labels = Seq("Theme", "Theme Prop", "Process", "Process Prop")
                     val slots = Seq(tuple.theme, tuple.themeProperties, tuple.themeProcess, tuple.themeProcessProperties)
-//                    val theme_name = if (tuple.theme.nonEmpty) slots.head.grounding.head.name else None
                     for (j <- labels.indices) {
                       pw.println(s"\t${labels(j)}")
                       for (g <- slots(j).grounding) {
-//                        if (g.name == theme_name && j != 0) {
-//                          //do nothing
-//                        }
-//                        else {
-//                          pw.println(s"    --> grounding ${j}:   ${g.name}")
-//                          pw.println(s"        score:        ${g.score}")
-//                        }
                         pw.println(s"    --> grounding ${j}:   ${g.name}")
                         pw.println(s"        score:        ${g.score}")
-                        //                      pw.println()
                       }
                       pw.println()
-
                     }
                   }
               }
             }
-
-            }
-
           }
-
+        }
       }
     }
-
-
-
-
-    /**
-     * ********************************************
-     * Sentence: The flooding caused sadness.
-     *
-     * SRLS:
-     *  [directed graph]
-     *
-     * Deps:
-     *  [directed graph]
-     *
-     * ---------------------------------
-     * flat grounding:
-     * ---------------------------------
-     *  0: ddd, 0.8
-     *  ...
-     *
-     * ---------------------------------
-     * compositional grounding:
-     * ---------------------------------
-     * Tuple 0:
-     *    concept:
-     *      abc, 0.9
-     *      def, 0.8
-     *      ..
-     *     concept property:
-     *      None
-     *     process:
-     *      kdslkd, 0.9
-     *      ...
-     *     process property:
-     *       xxx, 1.0
-     *
-     * [Tuple 1 - 4]
-     *
-     *
-     */
-
-
-//    FileUtils.printWriterFromFile(filename + ".debug.txt").autoClose { pw =>
-//      annotatedDocument.eidosMentions.filter(_.odinMention matches "Causal").foreach { em =>
-//        val args = em.eidosArguments("cause") ++ em.eidosArguments("effect")
-//        val info = args.map(m => (m.odinMention.text, m.grounding.get("wm_compositional"), m.grounding.get("wm_flattened")))
-//        for ((text, groundingComp, groundingFlat) <- info) {
-//          pw.println(s"text: ${text}")
-//          pw.println(s"Compositional Grounding:")
-//          groundingComp match {
-//            case None => pw.println("no grounding...")
-//            case Some(grounding) =>
-//              grounding.grounding.foreach { gr =>
-//                pw.println(s"  --> grounding: ${gr.name}")
-//                pw.println(s"      score: ${gr.score}")
-//              }
-//          }
-//          pw.println(s"Flat Grounding:")
-//          groundingFlat match {
-//            case None => pw.println("no grounding...")
-//            case Some(grounding) =>
-//              grounding.grounding.foreach { gr =>
-//                pw.println(s"  --> grounding: ${gr.name}")
-//                pw.println(s"      score: ${gr.score}")
-//              }
-//          }
-//          pw.println()
-//        }
-//
-//      }
-//    }
   }
-
 }
