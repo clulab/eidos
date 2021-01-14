07bcdc765bf639f096f7c2b0b2580400223c0bc8
Keith Alcock <github@keithalcock.com>
2021-01-13 17:27:39 -0700

    Move Menu to eidoscommon

--- src/main/scala/org/clulab/wm/eidos/apps/EidosShell.scala
+++ src/main/scala/org/clulab/wm/eidos/apps/EidosShell.scala
@@ -4,54 +4,23 @@ import com.typesafe.config.Config
 import org.clulab.wm.eidos.EidosSystem
 import org.clulab.wm.eidos.serialization.web.WebSerializer
 import org.clulab.wm.eidos.utils.DisplayUtils.displayMentions
+import org.clulab.wm.eidoscommon.utils.DefaultMenuItem
+import org.clulab.wm.eidoscommon.utils.ExitMenuItem
+import org.clulab.wm.eidoscommon.utils.HelpMenuItem
+import org.clulab.wm.eidoscommon.utils.MainMenuItem
+import org.clulab.wm.eidoscommon.utils.Menu
 import org.clulab.wm.eidoscommon.utils.{CliReader, IdeReader}
-
-import scala.collection.immutable.ListMap
 
 /**
   * Interactive shell for demonstrating Eidos
   */
 
 object EidosShell extends App {
-  val reader = {
-    val prompt = "(Eidos)>>> "
-
-    if (args.length == 0) new CliReader(prompt, "user.home", ".eidosshellhistory")
-    else new IdeReader(prompt)
-  }
-  val commands = ListMap(
-    ":help" -> "show commands",
-    ":reload" -> "reload grammar",
-    ":exit" -> "exit system"
-  )
   val eidosConfig: Config = EidosSystem.defaultConfig
   var ieSystem = new EidosSystem(eidosConfig)
   val webSerializer = new WebSerializer(ieSystem, eidosConfig)
 
-  def printCommands(): Unit = {
-    println("\nCOMMANDS:")
-    for ((cmd, msg) <- commands)
-      println(s"\t$cmd\t=> $msg")
-    println()
-  }
-
-  def processMenu: Boolean = {
-    val continue = reader.readLineOpt().map { line =>
-      if (line == ":exit") false
-      else {
-        line match {
-          case ":help" => printCommands()
-          case ":reload" => ieSystem = new EidosSystem(EidosSystem.defaultConfig, Some(ieSystem))
-          case text => extractFrom(text)
-        }
-        true
-      }
-    }.getOrElse(false)
-
-    continue
-  }
-
-  def extractFrom(text: String): Unit = {
+  def extractFromText(text: String): Unit = {
     val annotatedDocument = ieSystem.extractFromText(text)
     val doc = annotatedDocument.document
     val mentions = annotatedDocument.odinMentions
@@ -61,7 +30,30 @@ object EidosShell extends App {
     displayMentions(sortedMentions, doc, true)
   }
 
-  println("\nWelcome to the Eidos Shell!")
-  printCommands()
-  while (processMenu) { }
+  def extractFromMenu(menu: Menu, text: String): Boolean = {
+    extractFromText(text)
+    true
+  }
+
+  def reloadFromMenu(menu: Menu, key: String): Boolean = {
+    ieSystem = new EidosSystem(EidosSystem.defaultConfig, Some(ieSystem))
+    true
+  }
+
+  val lineReader = {
+    val prompt = "(Eidos)>>> "
+    // The CliReader does not work in IntelliJ on Windows, so this is a hack.
+    // Include a command line parameter and the lineReader will be switched.
+    if (args.length == 0) new CliReader(prompt, "user.home", ".eidosshellhistory")
+    else new IdeReader(prompt)
+  }
+  val mainMenuItems = Seq(
+    new HelpMenuItem(":help", "show commands"),
+    new MainMenuItem(":reload", "reload grammar", reloadFromMenu),
+    new ExitMenuItem(":exit", "exit system")
+  )
+  val defaultMenuItem = new DefaultMenuItem(extractFromMenu)
+  val menu = new Menu("Welcome to the Eidos Shell!", lineReader, mainMenuItems, defaultMenuItem)
+
+  menu.run()
 }
--- src/main/scala/org/clulab/wm/eidos/apps/EmptyEidosShell.scala
+++ src/main/scala/org/clulab/wm/eidos/apps/EmptyEidosShell.scala
@@ -1,57 +1,12 @@
 package org.clulab.wm.eidos.apps
 
 import org.clulab.wm.eidoscommon.utils.CliReader
+import org.clulab.wm.eidoscommon.utils.DefaultMenuItem
+import org.clulab.wm.eidoscommon.utils.ExitMenuItem
+import org.clulab.wm.eidoscommon.utils.HelpMenuItem
 import org.clulab.wm.eidoscommon.utils.IdeReader
-import org.clulab.wm.eidoscommon.utils.LineReader
-
-class MenuItem(val command: MenuItem.Command) {
-
-  def run(menu: Menu, input: String): Boolean = command(menu, input)
-}
-
-object MenuItem {
-  type Command = (Menu, String) => Boolean
-  val helpCommand: Command = (menu: Menu, _: String) => { menu.printCommands(); true }
-  val exitCommand: Command = (_: Menu, _: String) => false
-  val defaultCommand: Command = (_: Menu, input: String) => { println(s"Unknown command: $input"); true }
-  val defaultMenuItem: DefaultMenuItem = new DefaultMenuItem(defaultCommand)
-}
-
-class KeyedMenuItem(val key: String, val hint: String, command: MenuItem.Command) extends MenuItem(command)
-
-class HelpMenuItem(key: String, hint: String, command: MenuItem.Command = MenuItem.helpCommand) extends KeyedMenuItem(key, hint, command)
-
-class ExitMenuItem(key: String, hint: String, command: MenuItem.Command = MenuItem.exitCommand) extends KeyedMenuItem(key, hint, command)
-
-class DefaultMenuItem(command: MenuItem.Command = MenuItem.defaultCommand) extends MenuItem(command)
-
-class Menu(greeting: String, lineReader: LineReader, keyedMenuItems: Seq[KeyedMenuItem], defaultMenuItem: DefaultMenuItem = MenuItem.defaultMenuItem) {
-
-  def printCommands(): Unit = {
-    println("COMMANDS:")
-    keyedMenuItems.foreach { keyedMenuItem =>
-      println(s"\t${keyedMenuItem.key}\t=> ${keyedMenuItem.hint}")
-    }
-  }
-
-  def processMenu: Boolean = {
-    println()
-    lineReader.readLineOpt().map { line =>
-      println()
-      keyedMenuItems
-          .find(_.key == line)
-          .map(_.command(this, line))
-          .getOrElse(defaultMenuItem.command(this, line))
-    }.getOrElse(false)
-  }
-
-  def run(): Unit = {
-    println(s"\n$greeting\n")
-    printCommands()
-    while (processMenu) { }
-    println(s"Bye!\n")
-  }
-}
+import org.clulab.wm.eidoscommon.utils.MainMenuItem
+import org.clulab.wm.eidoscommon.utils.Menu
 
 object EmptyEidosShell extends App {
   val lineReader = {
@@ -61,13 +16,13 @@ object EmptyEidosShell extends App {
     if (args.length == 0) new CliReader(prompt, "user.home", ".eidosshellhistory")
     else new IdeReader(prompt)
   }
-  val keyedMenuItems = Seq(
+  val mainMenuItems = Seq(
     new HelpMenuItem(":help", "show commands"),
-    new KeyedMenuItem(":reload", "reload grammar", (_: Menu, _: String) => { println("Reloading..."); true }),
+    new MainMenuItem(":reload", "reload grammar", (_: Menu, _: String) => { println("Reloading..."); true }),
     new ExitMenuItem(":exit", "exit system")
   )
   val defaultMenuItem = new DefaultMenuItem((_: Menu, text: String) => { println(s"""Processing "$text"..."""); true })
-  val menu = new Menu("Welcome to the Eidos Shell!", lineReader, keyedMenuItems, defaultMenuItem)
+  val menu = new Menu("Welcome to the Eidos Shell!", lineReader, mainMenuItems, defaultMenuItem)
 
   menu.run()
 }
