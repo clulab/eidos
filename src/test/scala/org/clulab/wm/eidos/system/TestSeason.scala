be418fc2668546ffc762a8d7a89c65e15cd2b42e
Keith Alcock <github@keithalcock.com>
2021-03-02 18:50:16 -0700

    Configure tests differently

--- src/test/scala/org/clulab/wm/eidos/system/TestSeason.scala
+++ src/test/scala/org/clulab/wm/eidos/system/TestSeason.scala
@@ -8,17 +8,22 @@ import org.clulab.wm.eidos.test.ExtractionTest
 
 class TestSeason extends ExtractionTest {
 
-  def seasons(text: String): Seq[Time] = extractMentions(text).filter(_.label == "Time").flatMap(m => m.attachments.collect{
-    case time: Time => time
-  })
+  def seasons(text: String): Seq[Time] = extractMentions(text)
+      .filter(_.label == "Time")
+      .flatMap { m =>
+        m.attachments.collect {
+          case time: Time => time
+        }
+      }
 
   behavior of "season finder"
-  
-  it should "identify seasons" in {
+
+  longTimeNormTest should "identify a bi-gram (lean season) and a tri-gram (short rainy season) in one paragraph" in {
     // This should identify a bi-gram (lean season) and a tri-gram (short rainy season)
     // season expressions with date and location references in the same sentence.
     // It should also identify a modifier (early) for the bi-gram (lean season).
-    val text = """
+    val text =
+    """
       In many areas of Afar, household cereal stocks
       are likely to be depleted by March 2018, prompting
       an early lean season after the annual short rainy season.
@@ -33,30 +38,11 @@ class TestSeason extends ExtractionTest {
     )
     seasons(text).map(_.text) should contain allElementsOf Set("early lean season", "short rainy season")
     seasons(text).flatMap(_.interval.intervals) should contain allElementsOf Set(earlyLeanSeason, shortRainySeason)
+  }
 
-  if (true) { // This is sometimes disabled.
-    it should "identify seasons" in {
-      // This should identify bi-grams (lean season) and tri-grams (short rainy season)
-      // season expressions with date and location references in the same sentence.
-      val text = """
-        In many areas of Afar, household cereal stocks
-        are likely to be depleted by March 2018, prompting
-        an early lean season after the annual short rainy season.
-      """
-      val leanSeason = TimeStep(
-        LocalDateTime.of(2018, 1, 1, 0, 0),
-        LocalDateTime.of(2018, 4, 1, 0, 0)
-      )
-      val shortRainySeason = TimeStep(
-        LocalDateTime.of(2018, 4, 1, 0, 0),
-        LocalDateTime.of(2018, 6, 1, 0, 0)
-      )
-      seasons(text).map(_.text) should contain allElementsOf Set("lean season", "short rainy season")
-      seasons(text).flatMap(_.interval.intervals) should contain allElementsOf Set(leanSeason, shortRainySeason)
-
-    // This should identify a uni-gram season expression (meher)
-    // with date and location references in the previous sentence.
-    val text2 ="""
+  longTimeNormTest should "identify a uni-gram season expression (meher) with date and location references in the previous sentence" in {
+    val text =
+    """
       In 2011, regions of Tigray have experienced a
       significant increase in the frequency of below-normal
       rains compared to previous years. In particular,
@@ -66,17 +52,17 @@ class TestSeason extends ExtractionTest {
       LocalDateTime.of(2011, 9, 1, 0, 0),
       LocalDateTime.of(2012, 3, 1, 0, 0)
     )
-    seasons(text2).map(_.text) should contain ("meher")
-    seasons(text2).flatMap(_.interval.intervals) should contain (meher)
+    seasons(text).map(_.text) should contain("meher")
+    seasons(text).flatMap(_.interval.intervals) should contain(meher)
+  }
 
-      // This should not identify anything.
-      val text3 = """
-          In agricultural regions of developing countries,
-          it's known as the lean season that dangerous period
-          between planting and harvesting when job opportunities
-          are scarce and incomes plummet.
-      """
-      seasons(text3) shouldBe empty
-    }
+  longTimeNormTest should "not identify anything in a last paragraph" in {
+    val text = """
+        In agricultural regions of developing countries,
+        it's known as the lean season that dangerous period
+        between planting and harvesting when job opportunities
+        are scarce and incomes plummet.
+    """
+    seasons(text) shouldBe empty
   }
 }
