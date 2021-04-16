1f5bb3893d4dd1a2c6840861f8ccacc4d5e1d09f
Keith Alcock <github@keithalcock.com>
2021-04-15 18:36:53 -0700

    Work on batch processing

--- wmexchanger/loop_java.sh
+++ wmexchanger/loop_java.sh
@@ -6,4 +6,5 @@ set assembly=eidos-assembly-1.1.0-SNAPSHOT.jar:eidos-wmexchanger_2.12-1.1.0-SNAP
 java -cp ${assembly} org.clulab.wm.wmexchanger.wmconsumer.KafkaConsumerLoopApp app.topic=dart.cdr.streaming.updates app.outputDir=${baseDir}/input/kafka &
 java -cp ${assembly} org.clulab.wm.wmexchanger.wmconsumer.RestConsumerLoopApp ${baseDir}/input/kafka ${baseDir}/input ${baseDir}/input/kafka/done &
 java -cp ${assembly} org.clulab.wm.wmexchanger.wmproducer.RestProducerLoopApp ${baseDir}/output ${baseDir}/output/done &
+# This doesn't work because wmexchanger can't be assembled properly.
 java -cp ${assembly} org.clulab.wm.wmexchanger.wmeidos.EidosLoopAppJavaLauncher 4 ${baseDir}/input ${baseDir}/output ${baseDir}/input/done ${threads} ${memory}" &
--- wmexchanger/loop_sbt.sh
+++ wmexchanger/loop_sbt.sh
@@ -5,4 +5,13 @@ set memory=-Xmx16g
 sbt "wmexchanger/runMain org.clulab.wm.wmexchanger.wmconsumer.KafkaConsumerLoopApp app.topic=dart.cdr.streaming.updates app.outputDir=${baseDir}/input/kafka" &
 sbt "wmexchanger/runMain org.clulab.wm.wmexchanger.wmconsumer.RestConsumerLoopApp ${baseDir}/input/kafka ${baseDir}/input ${baseDir}/input/kafka/done" &
 sbt "wmexchanger/runMain org.clulab.wm.wmexchanger.wmproducer.RestProducerLoopApp ${baseDir}/output ${baseDir}/output/done" &
+# This doesn't work because sbt is not re-entrant, at least not so far.
 sbt "wmexchanger/runMain org.clulab.wm.wmexchanger.wmeidos.EidosLoopAppLauncher wmexchanger 4 ${baseDir}/input ${baseDir}/output ${baseDir}/input/done ${threads} ${memory}" &
+
+
+while ! sbt "wmexchanger/runMain org.clulab.wm.wmexchanger.wmeidos.EidosLoopApp ${baseDir}/input ${baseDir}/output ${baseDir}/input/done ${threads}"
+do
+  echo "It failed, so I'm trying again in 10 seconds."
+  sleep 10
+done
+
