bf524eed12cef1a4ac25d26de45c7028d1056a3b
Keith Alcock <github@keithalcock.com>
2021-04-15 17:18:27 -0700

    Update loop programs

---
+++ wmexchanger/loop_java.sh
@@ -0,0 +1,9 @@
+set baseDir=../corpora/loop
+set threads=4
+set memory=-Xmx16g
+set assembly=eidos-assembly-1.1.0-SNAPSHOT.jar:eidos-wmexchanger_2.12-1.1.0-SNAPSHOT.jar
+
+java -cp ${assembly} org.clulab.wm.wmexchanger.wmconsumer.KafkaConsumerLoopApp app.topic=dart.cdr.streaming.updates app.outputDir=${baseDir}/input/kafka &
+java -cp ${assembly} org.clulab.wm.wmexchanger.wmconsumer.RestConsumerLoopApp ${baseDir}/input/kafka ${baseDir}/input ${baseDir}/input/kafka/done &
+java -cp ${assembly} org.clulab.wm.wmexchanger.wmproducer.RestProducerLoopApp ${baseDir}/output ${baseDir}/output/done &
+java -cp ${assembly} org.clulab.wm.wmexchanger.wmeidos.EidosLoopAppJavaLauncher 4 ${baseDir}/input ${baseDir}/output ${baseDir}/input/done ${threads} ${memory}" &
