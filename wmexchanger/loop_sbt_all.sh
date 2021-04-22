82a69b602363e13ff687df977b06ef475180c8e4
Keith Alcock <github@keithalcock.com>
2021-04-22 16:11:57 -0700

    Sleep a little

--- wmexchanger/loop_sbt_all.sh
+++ wmexchanger/loop_sbt_all.sh
@@ -1,7 +1,10 @@
 # Copy this file to the main eidos directory so that the paths match up.
 # These are started in reverse order so that the pipeline is mostly ready when the first document is retrieved.
 ./wmexchanger/loop_sbt_restProducer.sh &
+sleep 10
 ./wmexchanger/loop_sbt_eidos.sh &
+sleep 5
 ./wmexchanger/loop_sbt_restConsumer.sh &
+sleep 5
 ./wmexchanger/loop_sbt_kafkaConsumer.sh &
 read -p "Press enter to stop the container. "
