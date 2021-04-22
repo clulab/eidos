11898e1f50dfdcf3f74bda25ce5d354255d50e45
Keith Alcock <github@keithalcock.com>
2021-04-22 10:31:36 -0700

    Test more on server

--- wmexchanger/loop_sbt_eidos.sh
+++ wmexchanger/loop_sbt_eidos.sh
@@ -1,17 +1,6 @@
-baseDir=../corpora/loop
 sleep=30
 
-default_threads=4
-default_memory=-Xmx20g
-
-threads="${eidos_threads:-$default_threads}"
-memory="${eidos_memory:-$default_memory}"
-
-echo threads is ${threads}
-echo memory is ${memory}
-
-export _JAVA_OPTIONS=${memory}
-while ! sbt ";set ThisBuild/offline := true ;wmexchanger/runMain org.clulab.wm.wmexchanger.wmeidos.EidosLoopApp ${baseDir}/input ${baseDir}/output ${baseDir}/input/done ${threads}"
+while ! ./wmexchanger/loop_sbt_eidos_once.sh
 do
   echo "It failed, so I am trying again in ${sleep} seconds."
   sleep ${sleep}
