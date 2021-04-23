9e00396ba1dae09e0ba3540954f4e418dfe2996d
Keith Alcock <github@keithalcock.com>
2021-04-22 17:33:42 -0700

    Fiddle with scripts

--- wmexchanger/loop_sbt_restConsumer.sh
+++ wmexchanger/loop_sbt_restConsumer.sh
@@ -1,3 +1,4 @@
 baseDir=../corpora/loop
 
+export _JAVA_OPTIONS=-Xmx2g
 sbt ";set ThisBuild/offline := true ;wmexchanger/runMain org.clulab.wm.wmexchanger.wmconsumer.RestConsumerLoopApp ${baseDir}/input/kafka ${baseDir}/input ${baseDir}/input/kafka/done"
--- wmexchanger/loop_sbt_restProducer.sh
+++ wmexchanger/loop_sbt_restProducer.sh
@@ -1,4 +1,5 @@
 baseDir=../corpora/loop
 
+export _JAVA_OPTIONS=-Xmx2g
 sbt ";set ThisBuild/offline := true ;wmexchanger/runMain org.clulab.wm.wmexchanger.wmproducer.RestProducerLoopApp ${baseDir}/output ${baseDir}/output/done"
 
