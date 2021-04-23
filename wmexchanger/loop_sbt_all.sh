5f11313a78b29ee2d905a496f9390dab26e2690e
Keith Alcock <github@keithalcock.com>
2021-04-22 17:34:06 -0700

    Fiddle more

--- wmexchanger/loop_sbt_all.sh
+++ wmexchanger/loop_sbt_all.sh
@@ -1,10 +1,11 @@
-# Copy this file to the main eidos directory so that the paths match up.
 # These are started in reverse order so that the pipeline is mostly ready when the first document is retrieved.
+# sbt ";set ThisBuild/offline := true ;wmexchanger/runMain org.clulab.wm.wmexchanger.utils.HelloWorld"
 ./wmexchanger/loop_sbt_restProducer.sh &
 sleep 10
-./wmexchanger/loop_sbt_eidos.sh &
-sleep 5
 ./wmexchanger/loop_sbt_restConsumer.sh &
 sleep 5
 ./wmexchanger/loop_sbt_kafkaConsumer.sh &
-read -p "Press enter to stop the container. "
+sleep 5
+# The container ends when eidos stops running.
+./wmexchanger/loop_sbt_eidos.sh
+
