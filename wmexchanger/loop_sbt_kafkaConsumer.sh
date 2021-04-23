ebd2ba3be12fb36d60a130cfffe617f450182349
Keith Alcock <github@keithalcock.com>
2021-04-23 08:38:07 -0700

    Reduce memory requirment, increase log level

--- wmexchanger/loop_sbt_kafkaConsumer.sh
+++ wmexchanger/loop_sbt_kafkaConsumer.sh
@@ -1,5 +1,5 @@
 baseDir=../corpora/loop
 
-export _JAVA_OPTIONS=-Xmx2g
+export _JAVA_OPTIONS=-Xmx512m
 sbt ";set ThisBuild/offline := true ;wmexchanger/runMain org.clulab.wm.wmexchanger.wmconsumer.KafkaConsumerLoopApp app.topic=dart.cdr.streaming.updates app.outputDir=${baseDir}/input/kafka"
 
--- wmexchanger/loop_sbt_restConsumer.sh
+++ wmexchanger/loop_sbt_restConsumer.sh
@@ -1,4 +1,4 @@
 baseDir=../corpora/loop
 
-export _JAVA_OPTIONS=-Xmx2g
+export _JAVA_OPTIONS=-Xmx1g
 sbt ";set ThisBuild/offline := true ;wmexchanger/runMain org.clulab.wm.wmexchanger.wmconsumer.RestConsumerLoopApp ${baseDir}/input/kafka ${baseDir}/input ${baseDir}/input/kafka/done"
