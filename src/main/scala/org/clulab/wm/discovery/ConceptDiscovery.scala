b70e7dd70d9d0853f5bfb58a1400d5b43967a52b
Steven Bethard <steven.bethard@gmail.com>
2021-05-05 11:56:40 -0700

    First draft of concept discovery and ranking APIs

--- src/main/scala/org/clulab/wm/discovery/ConceptDiscovery.scala
+++ src/main/scala/org/clulab/wm/discovery/ConceptDiscovery.scala
@@ -1,4 +1,15 @@
 package org.clulab.wm.discovery
 
+import java.nio.file.Path
+
+case class Concept(phrase: String, frequency: Int, documentLocations: Set[String])
+
+case class RankedConcept(concept: Concept, saliency: Double)
+
 class ConceptDiscovery {
+
+  def discoverConcepts(documents: Seq[Path]): Set[Concept] = ???
+
+  def rankConcepts(concepts: Set[Concept]): Seq[RankedConcept] = ???
+
 }
