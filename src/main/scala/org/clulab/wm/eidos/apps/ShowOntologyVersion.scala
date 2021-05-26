38c470bcbd0607c4f38059983c8be7403a57895d
Keith Alcock <github@keithalcock.com>
2021-05-26 10:07:53 -0700

    Clean up

--- src/main/scala/org/clulab/wm/eidos/apps/ShowOntologyVersion.scala
+++ src/main/scala/org/clulab/wm/eidos/apps/ShowOntologyVersion.scala
@@ -1,14 +1,14 @@
 package org.clulab.wm.eidos.apps
 
-import org.clulab.wm.eidos.EidosSystem
 import ai.lum.common.ConfigUtils._
+import org.clulab.wm.eidos.EidosSystem
 import org.clulab.wm.eidos.groundings.DomainHandler
 
 object ShowOntologyVersion extends App {
   val config = EidosSystem.defaultConfig
   val wmCompositional = config[String]("ontologies.wm_compositional")
   val (versionOpt, _) = DomainHandler.getVersionOpt(wmCompositional)
-  val version: String = versionOpt.getOrElse("unavailable")
+  val version = versionOpt.getOrElse("unavailable")
 
   println(s"Version: $version")
 }
