0d2132998e1e147414c905225e83ff921cfab78a
Keith Alcock <github@keithalcock.com>
2021-07-12 12:10:45 -0700

    Fix one test

--- src/test/scala/org/clulab/wm/eidos/text/englishGrounding/TestSRLGrounder.scala
+++ src/test/scala/org/clulab/wm/eidos/text/englishGrounding/TestSRLGrounder.scala
@@ -8,9 +8,13 @@ class TestSRLGrounder extends EnglishGroundingTest {
 
   it should "not recurse infinitely when there are two properties that point at each other" in {
     val text = "BOOST INCOME TO SUPPORT FOOD SECURITY AND NUTRITION ."
-    val mentions = ieSystem.extractFromText(text).eidosMentions
+    val annotatedDocument = ieSystem.extractFromText(text)
+    val mentions = annotatedDocument.eidosMentions
     mentions.size should be (2)
-    val groundings  = mentions.map(_.grounding("wm_compositional").grounding).map(_.head.name)
+    // The tested grounding was no longer at the head, so they are all collected.
+    val groundings  = mentions.map(_.grounding("wm_compositional").grounding).flatMap {
+      multipleOntologyGrounding => multipleOntologyGrounding.map(_.name)
+    }
     groundings.exists(gr => gr.contains("THEME: wm/concept/health/nutrition/")) shouldBe (true)
     groundings.exists(gr =>
       gr.contains("THEME: wm/property/security") &&
