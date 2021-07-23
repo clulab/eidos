cfb64ca220d199c5b8c1bed1fd01791fde98eb7e
Keith Alcock <github@keithalcock.com>
2021-07-23 09:26:32 -0700

    Configure wmexchanger better

--- wmexchanger/export.sh
+++ wmexchanger/export.sh
@@ -1,16 +1,23 @@
-#export KAFKA_CONSUMER_BOOTSTRAP_SERVERS=uat-ingest-pipeline-streaming-1.prod.dart.worldmodelers.com:9093
-export KAFKA_CONSUMER_BOOTSTRAP_SERVERS=wm-ingest-pipeline-streaming-1.prod.dart.worldmodelers.com:9093
+# Run this before loop_sbt_configure.sh or docker-compose.
+
+# This one is for production.
+export KAFKA_HOSTNAME=wm-ingest-pipeline-streaming-1.prod.dart.worldmodelers.com
+# This one is for testing.
+# export KAFKA_HOSTNAME=uat-ingest-pipeline-streaming-1.prod.dart.worldmodelers.com
+
+export KAFKA_CONSUMER_BOOTSTRAP_SERVERS=${KAFKA_HOSTNAME:-localhost}:9093
 export KAFKA_APP_TOPIC=dart.cdr.streaming.updates
-#export REST_CONSUMER_SERVICE=https://uat-ingest-pipeline-rest-1.prod.dart.worldmodelers.com/dart/api/v1/cdrs
-export REST_CONSUMER_SERVICE=https://wm-ingest-pipeline-rest-1.prod.dart.worldmodelers.com/dart/api/v1/cdrs
-#export REST_PRODUCER_SERVICE=https://uat-ingest-pipeline-rest-1.prod.dart.worldmodelers.com/dart/api/v1/readers/upload
-export REST_PRODUCER_SERVICE=https://wm-ingest-pipeline-rest-1.prod.dart.worldmodelers.com/dart/api/v1/readers/upload
+
+# This one is for production.
+export REST_HOSTNAME=wm-ingest-pipeline-rest-1.prod.dart.worldmodelers.com
+# This one is for testing.
+# export REST_HOSTNAME=uat-ingest-pipeline-rest-1.prod.dart.worldmodelers.com
 
 export EIDOS_VERSION=julyEmbed1
 export ONTOLOGY_VERSION=2.3
 
 export EIDOS_USERNAME=eidos
+# Add this elsewhere.
 # export EIDOS_PASSWORD=
 
-#export EIDOS_BASE_DIR=../corpora/julyEmbed/61538
-export EIDOS_BASE_DIR=../corpora/julyEmbed/534
+export EIDOS_BASE_DIR=../corpora/julyEmbed
