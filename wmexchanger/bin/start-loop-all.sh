1fd6ecb979cd854604a451b3cb912ada14453aff
Keith Alcock <github@keithalcock.com>
2021-07-26 18:43:36 -0700

    Divide into base and eidos, separate from rest

--- wmexchanger/bin/start-loop-all.sh
+++ wmexchanger/bin/start-loop-all.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+# These came from ../loop_sbt_configure.sh.  Values should be exported
+# so that Java can access them except for values that are used directly.
+
 default_base_dir="../corpora/loop"
 
 # Coordinate servers
@@ -37,3 +42,29 @@ export EIDOS_DONE_DIR="${EIDOS_DONE_DIR:-$EIDOS_INPUT_DIR/$DONE_SUBDIR}"
 # For the rest producer, output goes to the network.
 export REST_PRODUCER_INPUT_DIR="${REST_PRODUCER_INPUT_DIR:-$EIDOS_OUTPUT_DIR}"
 export REST_PRODUCER_DONE_DIR="${REST_PRODUCER_DONE_DIR:-$REST_PRODUCER_INPUT_DIR/$DONE_SUBDIR}"
+
+echo "Starting Eidos"
+
+SLEEP=30
+DEFAULT_THREADS=4
+DEFAULT_EIDOS_MEMORY="-Xmx20g"
+
+THREADS="${EIDOS_THREADS:-$DEFAULT_THREADS}"
+EIDOS_MEMORY="${EIDOS_MEMORY:-DEFAULT_EIDOS_MEMORY}"
+
+export _JAVA_OPTIONS=-Xmx1g ; ./bin/rest-producer-loop-app "${REST_PRODUCER_INPUT_DIR}" "${REST_PRODUCER_DONE_DIR}" &
+sleep 10
+export _JAVA_OPTIONS=-Xmx1g ; ./bin/rest-consumer-loop-app "${REST_CONSUMER_INPUT_DIR}" "${REST_CONSUMER_OUTPUT_DIR}" "${REST_CONSUMER_DONE_DIR}" &
+sleep 5
+export _JAVA_OPTIONS=-Xmx512m ; ./bin/kafka-consumer-loop-app &
+sleep 5
+
+export _JAVA_OPTIONS=${DEFAULT_EIDOS_MEMORY}
+export JAVA_OPTS=${DEFAULT_EIDOS_MEMORY}
+
+# The container ends when eidos stops running.
+while ! ./bin/eidos-loop-app "${EIDOS_INPUT_DIR}" "${EIDOS_OUTPUT_DIR}" "${EIDOS_DONE_DIR}" "${THREADS}"
+do
+  echo "It failed, so I am trying again in ${SLEEP} seconds."
+  sleep ${SLEEP}
+done
