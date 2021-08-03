d15ff391988fb990cd6a5b666da72dec7a93e177
Keith Alcock <github@keithalcock.com>
2021-08-03 11:42:53 -0700

    Organize apps

---
+++ src/main/scala/org/clulab/wm/eidos/apps/filter/FilterJson.scala
@@ -0,0 +1,43 @@
+package org.clulab.wm.eidos.apps.filter
+
+import org.clulab.wm.eidoscommon.utils.FileUtils
+import org.json4s.DefaultFormats
+import org.json4s.JArray
+import org.json4s.JNothing
+import org.json4s.JValue
+import org.json4s.jackson.JsonMethods
+
+import java.io.File
+
+object FilterJson extends App {
+
+  class Filter(val outputDir: String) {
+    implicit val formats: DefaultFormats.type = org.json4s.DefaultFormats
+
+    def filter(inputFile: File, jValue: JValue): Unit = {
+      // println(s"Extracting from ${inputFile.getName}")
+      val geoLocations: JValue = (jValue \\ "geolocs" \ "text")
+
+      geoLocations match {
+        case JArray(geoLocations: List[_]) => // Type erasure removes the [JString]
+          geoLocations.foreach { geoLocation =>
+            println(geoLocation.extract[String])
+          }
+        case JNothing =>
+        case _ => throw new RuntimeException(s"Unexpected geoLocations value: $geoLocations")
+      }
+    }
+  }
+
+  val inputDir = args(0)
+  val extension = args(1)
+  val outputDir = args(2)
+  val inputFiles = FileUtils.findFiles(inputDir, extension)
+  val filter = new Filter(outputDir)
+
+  inputFiles.foreach { inputFile =>
+    val text = FileUtils.getTextFromFile(inputFile)
+    val json = JsonMethods.parse(text)
+    filter.filter(inputFile, json)
+  }
+}
