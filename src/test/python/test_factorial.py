4fc388ca85ce101d938483db4f51cd066cb90758
Keith Alcock <github@keithalcock.com>
2021-08-17 10:10:08 -0700

    Fail a test

--- src/test/python/test_factorial.py
+++ src/test/python/test_factorial.py
@@ -2,7 +2,7 @@ from factorial import factorial
 
 class TestFactorial:
     def test_low(self):
-        assert factorial(0) == 1
+        assert factorial(0) == 2
         assert factorial(5) == 120
 
     def test_high(self):
