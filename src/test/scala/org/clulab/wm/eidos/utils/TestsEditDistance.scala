88c519294cb932084607e3124a245d86cc72af5e
Keith Alcock <github@keithalcock.com>
2021-10-10 11:06:23 -0700

    Rearrange testing of MED

--- src/test/scala/org/clulab/wm/eidos/utils/TestsEditDistance.scala
+++ src/test/scala/org/clulab/wm/eidos/utils/TestsEditDistance.scala
@@ -1,6 +1,7 @@
 package org.clulab.wm.eidos.utils
 
-import org.clulab.wm.eidoscommon.utils.MED
+// This is the local implementation.
+import org.clulab.wm.eidoscommon.utils.{MED => ClulabEditDistance}
 import org.clulab.wm.eidoscommon.utils.Test
 
 // These implementations are already "included" in eidos.
@@ -11,119 +12,66 @@ import com.sun.xml.bind.v2.util.{EditDistance => SunEditDistance}
 import edu.stanford.nlp.util.{EditDistance => StanfordEditDistance}
 import org.apache.commons.text.similarity.{LevenshteinDistance => ApacheEditDistance}
 
+abstract class EditDist(source: String, target: String) {
+  def measure(): Int
+  def transposes: Boolean = false
+  def substitutes: Boolean = true
+}
+
+class ClulabEditDist(source: String, target: String) extends EditDist(source, target) {
+  def measure(): Int = ClulabEditDistance(source, target).getDistance
+}
+
+class SunEditDist(source: String, target: String) extends EditDist(source, target) {
+  def measure(): Int = SunEditDistance.editDistance(source, target)
+}
+
+class StanfordEditDist(source: String, target: String, transpose: Boolean = false) extends EditDist(source, target) {
+  override val transposes = transpose
+
+  def measure(): Int = new StanfordEditDistance(/*allowTranspose =*/ transpose).score(source, target).toInt
+}
+
+class ApacheEditDist(source: String, target: String) extends EditDist(source, target) {
+  def measure(): Int = new ApacheEditDistance().apply(source, target)
+}
+
 class TestsEditDistance extends Test {
 
-  behavior of "SunEditDistance"
+  def test(name: String, constructor: (String, String) => EditDist): Unit = {
+    behavior of name
 
-  it should "insert correctly" in {
-    val med = SunEditDistance.editDistance("ac", "abc")
+    it should "insert correctly" in {
+      val med = constructor("ac", "abc")
 
-    med should be (1)
+      // depends on if can substitute and transpose
+      med.measure should be (1)
+    }
+
+    it should "delete correctly" in {
+      val med = constructor("abc", "ac")
+
+      med.measure should be (1)
+    }
+
+    it should "substitute correctly" in {
+      val med = constructor("abc", "adc")
+
+      // then check if can do without substitution
+      med.measure should be (1)
+    }
+
+    it should "transpose correctly" in {
+      val med = constructor("ab", "ba")
+      val expected = if (med.transposes) 1 else 2
+
+      med.measure should be (expected)
+    }
   }
 
-  it should "delete correctly" in {
-    val med = SunEditDistance.editDistance("abc", "ac")
-
-    med should be (1)
-  }
-
-  it should "substitute correctly" in {
-    val med = SunEditDistance.editDistance("abc", "adc")
-
-    med should be (1)
-  }
-
-  it should "transpose correctly" in {
-    val med = SunEditDistance.editDistance("ab", "ba")
-
-    // This might not be optimal.
-    med should be (2)
-  }
-
-  behavior of "ApacheEditDistance"
-
-  it should "insert correctly" in {
-    val med = new ApacheEditDistance().apply("ac", "abc")
-
-    med should be (1)
-  }
-
-  it should "delete correctly" in {
-    val med = new ApacheEditDistance().apply("abc", "ac")
-
-    med should be (1)
-  }
-
-  it should "substitute correctly" in {
-    val med = new ApacheEditDistance().apply("abc", "adc")
-
-    med should be (1)
-  }
-
-  it should "transpose correctly" in {
-    val med = new ApacheEditDistance().apply("ab", "ba")
-
-    // This might not be optimal.
-    med should be (2)
-  }
-
-  behavior of "StanfordEditDistance"
-
-  it should "insert correctly" in {
-    val med = new StanfordEditDistance().score("ac", "abc")
-
-    med should be (1)
-  }
-
-  it should "delete correctly" in {
-    val med = new StanfordEditDistance().score("abc", "ac")
-
-    med should be (1)
-  }
-
-  it should "substitute correctly" in {
-    val med = new StanfordEditDistance().score("abc", "adc")
-
-    med should be (1)
-  }
-
-  it should "transpose correctly" in {
-    val med = new StanfordEditDistance(false).score("ab", "ba")
-
-    // This might not be optimal.
-    med should be (2)
-  }
-
-  it should "transpose (in)correctly" in {
-    val med = new StanfordEditDistance(true).score("ab", "ba")
-
-    med should be (1)
-  }
-
-  behavior of "MED"
-
-  it should "insert correctly" in {
-    val med = MED("ac", "abc").getDistance
-
-    med should be (1)
-  }
-
-  it should "delete correctly" in {
-    val med = MED("abc", "ac").getDistance
-
-    med should be (1)
-  }
-
-  it should "substitute correctly" in {
-    val med = MED("abc", "adc").getDistance
-
-    med should be (1)
-  }
-
-  it should "transpose correctly" in {
-    val med = MED("ab", "ba").getDistance
-
-    // This might not be optimal.
-    med should be (2)
-  }
+  test("SunEditDistance", (source: String, target: String) => new SunEditDist(source, target))
+  test("ApacheEditDistance", (source: String, target: String) => new ApacheEditDist(source, target))
+  test("StanfordEditDistance, non-transposing", (source: String, target: String) => new StanfordEditDist(source, target, transpose = false))
+  test("StanfordEditDistance, transposing", (source: String, target: String) => new StanfordEditDist(source, target, transpose = true))
+  test ("ClulabEditDistance", (source: String, target: String) => new ClulabEditDist(source, target))
 }
