bbcc8c9beec4cc2729d5f1fa5d512381751e13d1
Keith Alcock <github@keithalcock.com>
2021-10-14 14:04:24 -0700

    Update tests

--- eidoscommon/src/test/scala/org/clulab/wm/eidoscommon/utils/TestMED.scala
+++ eidoscommon/src/test/scala/org/clulab/wm/eidoscommon/utils/TestMED.scala
@@ -3,43 +3,115 @@ package org.clulab.wm.eidoscommon.utils
 class TestMED extends Test {
   behavior of "MED"
 
-  it should "work, substitute = false, transpose = false" in {
+  it should "work, substitute = false, transpose = false, capitalize = false" in {
     val allowSubstitute = false
     val allowTranspose = false
+    val allowCapitalize = false
 
-    MED("cat", "cars", allowSubstitute, allowTranspose).getDistance should be (3)
-    MED("kitten", "sitting", allowSubstitute, allowTranspose).getDistance should be (5)
-    MED("Sunday", "Saturday", allowSubstitute, allowTranspose).getDistance should be (4)
-    MED("meter", "litre", allowSubstitute, allowTranspose).getDistance should be (6)
+    def getDistance(source: String, target: String) = MED(source, target, allowSubstitute, allowTranspose, allowCapitalize).getDistance
+
+    getDistance("cat", "cars") should be (3)
+    getDistance("kitten", "sitting") should be (5)
+    getDistance("Sunday", "Saturday") should be (4)
+    getDistance("meter", "litre") should be (6)
+    getDistance("mom", "Mom") should be (2)
   }
 
-  it should "work, substitute = true, transpose = false" in {
+  it should "work, substitute = true, transpose = false, capitalize = false" in {
     val allowSubstitute = true
     val allowTranspose = false
+    val allowCapitalize = false
 
-    MED("cat", "cars", allowSubstitute, allowTranspose).getDistance should be (2)
-    MED("kitten", "sitting", allowSubstitute, allowTranspose).getDistance should be (3)
-    MED("Sunday", "Saturday", allowSubstitute, allowTranspose).getDistance should be (3)
-    MED("meter", "litre", allowSubstitute, allowTranspose).getDistance should be (4)
+    def getDistance(source: String, target: String) = MED(source, target, allowSubstitute, allowTranspose, allowCapitalize).getDistance
+
+    getDistance("cat", "cars") should be (2)
+    getDistance("kitten", "sitting") should be (3)
+    getDistance("Sunday", "Saturday") should be (3)
+    getDistance("meter", "litre") should be (4)
+    getDistance("mom", "Mom") should be (1)
   }
 
-  it should "work, substitute = false, transpose = true" in {
+  it should "work, substitute = false, transpose = true, capitalize = false" in {
     val allowSubstitute = false
     val allowTranspose = true
+    val allowCapitalize = false
 
-    MED("cat", "cars", allowSubstitute, allowTranspose).getDistance should be (3)
-    MED("kitten", "sitting", allowSubstitute, allowTranspose).getDistance should be (5)
-    MED("Sunday", "Saturday", allowSubstitute, allowTranspose).getDistance should be (4)
-    MED("meter", "litre", allowSubstitute, allowTranspose).getDistance should be (5)
+    def getDistance(source: String, target: String) = MED(source, target, allowSubstitute, allowTranspose, allowCapitalize).getDistance
+
+    getDistance("cat", "cars") should be (3)
+    getDistance("kitten", "sitting") should be (5)
+    getDistance("Sunday", "Saturday") should be (4)
+    getDistance("meter", "litre") should be (5)
+    getDistance("mom", "Mom") should be (2)
   }
 
-  it should "work, substitute = true, transpose = true" in {
+  it should "work, substitute = true, transpose = true, capitalize = false" in {
     val allowSubstitute = true
     val allowTranspose = true
+    val allowCapitalize = false
 
-    MED("cat", "cars", allowSubstitute, allowTranspose).getDistance should be (2)
-    MED("kitten", "sitting", allowSubstitute, allowTranspose).getDistance should be (3)
-    MED("Sunday", "Saturday", allowSubstitute, allowTranspose).getDistance should be (3)
-    MED("meter", "litre", allowSubstitute, allowTranspose).getDistance should be (3)
+    def getDistance(source: String, target: String) = MED(source, target, allowSubstitute, allowTranspose, allowCapitalize).getDistance
+
+    getDistance("cat", "cars") should be (2)
+    getDistance("kitten", "sitting") should be (3)
+    getDistance("Sunday", "Saturday") should be (3)
+    getDistance("meter", "litre") should be (3)
+    getDistance("mom", "Mom") should be (1)
+  }
+
+  it should "work, substitute = false, transpose = false, capitalize = true" in {
+    val allowSubstitute = false
+    val allowTranspose = false
+    val allowCapitalize = true
+
+    def getDistance(source: String, target: String) = MED(source, target, allowSubstitute, allowTranspose, allowCapitalize).getDistance
+
+    getDistance("cat", "cars") should be (3)
+    getDistance("kitten", "sitting") should be (5)
+    getDistance("Sunday", "Saturday") should be (4)
+    getDistance("meter", "litre") should be (6)
+    getDistance("mom", "Mom") should be (0)
+  }
+
+  it should "work, substitute = true, transpose = false, capitalize = true" in {
+    val allowSubstitute = true
+    val allowTranspose = false
+    val allowCapitalize = true
+
+    def getDistance(source: String, target: String) = MED(source, target, allowSubstitute, allowTranspose, allowCapitalize).getDistance
+
+    getDistance("cat", "cars") should be (2)
+    getDistance("kitten", "sitting") should be (3)
+    getDistance("Sunday", "Saturday") should be (3)
+    getDistance("meter", "litre") should be (4)
+    getDistance("mom", "Mom") should be (0)
+  }
+
+  it should "work, substitute = false, transpose = true, capitalize = true" in {
+    val allowSubstitute = false
+    val allowTranspose = true
+    val allowCapitalize = true
+
+    def getDistance(source: String, target: String) = MED(source, target, allowSubstitute, allowTranspose, allowCapitalize).getDistance
+
+    getDistance("cat", "cars") should be (3)
+    getDistance("kitten", "sitting") should be (5)
+    getDistance("Sunday", "Saturday") should be (4)
+    getDistance("meter", "litre") should be (5)
+    getDistance("mom", "Mom") should be (0)
+  }
+
+  it should "work, substitute = true, transpose = true, capitalize = true" in {
+    val allowSubstitute = true
+    val allowTranspose = true
+    val allowCapitalize = true
+
+    def getDistance(source: String, target: String) = MED(source, target, allowSubstitute, allowTranspose, allowCapitalize).getDistance
+
+    getDistance("cat", "cars") should be (2)
+    getDistance("kitten", "sitting") should be (3)
+    getDistance("Sunday", "Saturday") should be (3)
+    getDistance("meter", "litre") should be (3)
+    getDistance("mom", "Mom") should be (0)
   }
 }
