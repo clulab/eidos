1eaa59bcb3d232cb20ee5cf98e10bba7317f101e
Keith Alcock <github@keithalcock.com>
2021-10-25 12:10:33 -0700

    Convert mapper

--- eidoscommon/src/main/scala/org/clulab/wm/eidoscommon/utils/Mapper.scala
+++ eidoscommon/src/main/scala/org/clulab/wm/eidoscommon/utils/Mapper.scala
@@ -27,16 +27,16 @@ class EqualityMapper[K, V] extends MentionMapper[K, V] {
   def size: Int = map.size
 }
 
-class IdentityMapper[K, V] extends MentionMapper[K, V] {
-  protected val map = new util.IdentityHashMap[K, V]()
+class IdentityMapper[K <: AnyRef, V] extends MentionMapper[K, V] {
+  protected val map = new IdentityHashMap[K, V]()
 
-  def put(key: K, value: V): Unit = map.put(key, value)
+  def put(key: K, value: V): Unit = map(key) = value
 
-  def get(key: K): V = Option(map.get(key)).getOrElse(throw new Exception(s"Value for key $key not found."))
+  def get(key: K): V = map(key)
 
-  def getOrElse(key: K, default: => V): V = Option(map.get(key)).getOrElse(default)
+  def getOrElse(key: K, default: => V): V = map.getOrElse(key, default)
 
-  def getValues: Seq[V] = map.values.asScala.toSeq
+  def getValues: Seq[V] = map.values.toSeq
 
   def size: Int = map.size
 }
