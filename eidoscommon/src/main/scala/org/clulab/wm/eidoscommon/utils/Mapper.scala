6b1a12b20347aee5e8412974a65cc6794e843a48
Keith Alcock <github@keithalcock.com>
2021-10-25 17:49:08 -0700

    Change name of Mapper to match Bagger

--- eidoscommon/src/main/scala/org/clulab/wm/eidoscommon/utils/Mapper.scala
+++ eidoscommon/src/main/scala/org/clulab/wm/eidoscommon/utils/Mapper.scala
@@ -1,11 +1,8 @@
 package org.clulab.wm.eidoscommon.utils
 
-import java.util
-
-import scala.collection.JavaConverters._
 import scala.collection.mutable
 
-abstract class MentionMapper[K, V] {
+abstract class Mapper[K, V] {
   def put(key: K, value: V): Unit
   def get(key: K): V
   def getOrElse(key: K, default: => V): V
@@ -13,7 +10,7 @@ abstract class MentionMapper[K, V] {
   def size: Int
 }
 
-class EqualityMapper[K, V] extends MentionMapper[K, V] {
+class EqualityMapper[K, V] extends Mapper[K, V] {
   protected val map = new mutable.HashMap[K, V]()
 
   def put(key: K, value: V): Unit = map.put(key, value)
@@ -27,7 +24,7 @@ class EqualityMapper[K, V] extends MentionMapper[K, V] {
   def size: Int = map.size
 }
 
-class IdentityMapper[K <: AnyRef, V] extends MentionMapper[K, V] {
+class IdentityMapper[K <: AnyRef, V] extends Mapper[K, V] {
   protected val map = new IdentityHashMap[K, V]()
 
   def put(key: K, value: V): Unit = map(key) = value
