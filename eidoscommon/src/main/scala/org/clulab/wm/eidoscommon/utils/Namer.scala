5af41d09983aa36f501f6eb37ebae97138557e21
Keith Alcock <github@keithalcock.com>
2021-11-12 11:21:28 -0700

    Add canonicalName to Namer and make them sortable

--- eidoscommon/src/main/scala/org/clulab/wm/eidoscommon/utils/Namer.scala
+++ eidoscommon/src/main/scala/org/clulab/wm/eidoscommon/utils/Namer.scala
@@ -4,7 +4,23 @@ trait Namer {
   def name: String // gets the entire name with parts separated by /
   def branch: Option[String] // gets the branch in top/branch/[more/]leaf
 
+  def canonicalName: String = {
+    val longName = name
+    val shortName = StringUtils.afterLast(longName, '/', true)
+    // Use '' instead of "" to avoid regular expression replacement of strings.  We have simple characters here.
+    val separatedName = shortName.replace('_', ' ')
+    val lowerName = separatedName.toLowerCase
+
+    lowerName
+  }
+
   override def toString: String = name
+}
+
+object Namer {
+  implicit val NameOrdering: Ordering[Namer] = new Ordering[Namer] {
+    override def compare(x: Namer, y: Namer): Int = x.name.compare(y.name)
+  }
 }
 
 class PassThruNamer(val name: String) extends Namer {
