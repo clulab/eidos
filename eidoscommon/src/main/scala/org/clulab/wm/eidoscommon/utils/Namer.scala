5f2bb5fbcf4f2306cb3ab160454568055a857efe
Keith Alcock <github@keithalcock.com>
2021-11-15 10:31:40 -0700

    Streamline naming

--- eidoscommon/src/main/scala/org/clulab/wm/eidoscommon/utils/Namer.scala
+++ eidoscommon/src/main/scala/org/clulab/wm/eidoscommon/utils/Namer.scala
@@ -4,15 +4,7 @@ trait Namer {
   def name: String // gets the entire name with parts separated by /
   def branch: Option[String] // gets the branch in top/branch/[more/]leaf
 
-  def canonicalName: String = {
-    val longName = name
-    val shortName = StringUtils.afterLast(longName, '/', true)
-    // Use '' instead of "" to avoid regular expression replacement of strings.  We have simple characters here.
-    val separatedName = shortName.replace('_', ' ')
-    val lowerName = separatedName.toLowerCase
-
-    lowerName
-  }
+  def canonicalName: String = Namer.canonicalize(name)
 
   override def toString: String = name
 }
@@ -20,6 +12,16 @@ trait Namer {
 object Namer {
   implicit val NameOrdering: Ordering[Namer] = new Ordering[Namer] {
     override def compare(x: Namer, y: Namer): Int = x.name.compare(y.name)
+  }
+
+  def canonicalize(name: String): String = {
+    val longName = name
+    val shortName = StringUtils.afterLast(longName, '/', true)
+    // Use '' instead of "" to avoid regular expression replacement of strings.  We have simple characters here.
+    val separatedName = shortName.replace('_', ' ')
+    val lowerName = separatedName.toLowerCase
+
+    lowerName
   }
 }
 
