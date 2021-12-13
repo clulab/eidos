57b34a2721fc235e16c61262ea574f15c70b51e3
Keith Alcock <github@keithalcock.com>
2021-12-13 13:15:02 -0700

    Delete files before making geo cache

--- src/main/scala/org/clulab/wm/eidos/apps/cache/CacheGeonames.scala
+++ src/main/scala/org/clulab/wm/eidos/apps/cache/CacheGeonames.scala
@@ -4,6 +4,7 @@ import ai.lum.common.ConfigUtils._
 import org.clulab.geonorm.GeoNamesIndex
 import org.clulab.wm.eidos.EidosSystem
 
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
@@ -11,5 +12,19 @@ object CacheGeonames extends App {
   val config = EidosSystem.defaultConfig
   val geoNamesDir: Path = Paths.get(config[String]("geonorm.geoNamesDir")).toAbsolutePath.normalize
 
+  def deleteFiles(path: Path): Unit = {
+    if (Files.exists(path)) {
+      // This is written for Scala 1.11 compatibility.
+      Files.list(path).toArray.foreach { path =>
+        Files.deleteIfExists(path.asInstanceOf[Path])
+      }
+      // At least on Windows, the directory doesn't disappear until the application exists.
+      // In the meantime, it will be locked and unavailable for use by GeoNamesIndex.
+      // However, it is is left around but empty, it works OK.  So, skip this.
+      // Files.deleteIfExists(path)
+    }
+  }
+
+  deleteFiles(geoNamesDir)
   GeoNamesIndex.fromClasspathJar(geoNamesDir)
 }
