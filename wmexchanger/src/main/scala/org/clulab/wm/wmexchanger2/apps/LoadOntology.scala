d018dcf5e599942d491d06e61e1359857668fa73
Keith Alcock <github@keithalcock.com>
2022-02-03 11:46:38 -0700

    Read an ontology file

---
+++ wmexchanger/src/main/scala/org/clulab/wm/wmexchanger2/apps/LoadOntology.scala
@@ -0,0 +1,32 @@
+package org.clulab.wm.wmexchanger2.apps
+
+import org.clulab.wm.eidos.EidosSystem
+import org.clulab.wm.eidoscommon.utils.Closer.AutoCloser
+import org.clulab.wm.eidoscommon.utils.StringUtils
+import org.clulab.wm.ontologies.NodeTreeDomainOntology
+import org.clulab.wm.ontologies.NodeTreeDomainOntologyBuilder
+
+import java.io.File
+import java.io.FileInputStream
+
+object LoadOntology extends App {
+  val ontologyFile = new File(args(0))
+
+  def newDomainOntology(eidosSystem: EidosSystem, file: File, version: String): NodeTreeDomainOntology = {
+    val oldComponents = eidosSystem.components
+    val oldOntologyHandler = oldComponents.ontologyHandlerOpt.get
+    val oldSentencesExtractor = oldOntologyHandler.sentencesExtractor
+    val oldCanonicalizer = oldOntologyHandler.canonicalizer
+
+    val newDomainOntologyBuilder = new NodeTreeDomainOntologyBuilder(oldSentencesExtractor, oldCanonicalizer, filtered = true)
+    val newDomainOntology = (new FileInputStream(file)).autoClose { inputStream =>
+      newDomainOntologyBuilder.buildFromStream(inputStream, Some(version), None)
+    }
+
+    newDomainOntology
+  }
+
+  val eidosSystem = new EidosSystem() // Maybe remove ontology handler? and not perform grounding?
+  val version = StringUtils.beforeFirst(ontologyFile.getName, '.')
+  newDomainOntology(eidosSystem, ontologyFile, version)
+}
